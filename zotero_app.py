17cf82a9ab679fa4868175314b07308054415eef
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 13:27:31 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -66,7 +66,7 @@ bbb = zot.collections()
 data3=[]
 columns3 = ['Key','Name', 'Number', 'Link']
 for item in bbb:
-    data2.append((item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href']))
+    data3.append((item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href']))
 pd.set_option('display.max_colwidth', None)
 df_collections_2 = pd.DataFrame(data3, columns=columns3)
 
