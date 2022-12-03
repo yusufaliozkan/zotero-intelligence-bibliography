7144ad97887fe917ee80ec6faae6a5035102eb99
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 14:04:58 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -26,7 +26,7 @@ columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zoter
 for item in items:
     data.append((item['data']['title'], item['data']['itemType'], item['data']['url'], item['data']['abstractNote'], item['links']['alternate']['href'], item['data']['dateAdded'],))
 
-st.set_page_config(layout = "wide") 
+st.set_page_config(layout = "centered") 
 
 df = pd.DataFrame(data, columns=columns)
 
