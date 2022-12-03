c5660fc2f91f5c0ca9f03919777343f3f2f6911e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 13:49:18 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -73,14 +73,12 @@ pd.set_option('display.max_colwidth', None)
 df_collections_2 = pd.DataFrame(data3, columns=columns3)
 df_collections_2
 
-def make_clickable(link):
-    # target _blank to open new window
-    # extract clickable text to display for your link
-    text = link.split('=')[1]
-    return f'<a target="_blank" href="{link}">{text}</a>'
-df_collections_2['Link'] = df_collections_2['Link'].apply(make_clickable)
-df_collections_2 = df_collections_2.to_html(escape=False)
-st.write(df_collections_2, unsafe_allow_html=True)
+df = pd.DataFrame({
+    'name':['Softhints', 'DataScientyst'],
+    'url':['https://www.softhints.com', 'https://datascientyst.com']
+})
+df
+
 
 # Zotero library collections
 st.header('Items by collection: ')
