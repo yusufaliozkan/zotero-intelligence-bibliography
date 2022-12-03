32a08331da506b633e9b2d5a349785b46c8a0350
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 14:18:34 +0000

    COMMIT

--- zotero_app.py
+++ zotero_app.py
@@ -113,57 +113,57 @@ df_collections = df_collections.sort_values(by='Name')
 # df_collections['Name2'] = df_collections['Name2'].replace(['98. Special collections'],'Special collections')
 # df_collections['Name2'] = df_collections['Name2'].replace(['99. Archival sources and reports'],'Archival sources and reports')
 
-    clist = df_collections['Name'].unique()
-    collection_name = st.selectbox('Select a collection:', clist)
-    collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
+clist = df_collections['Name'].unique()
+collection_name = st.selectbox('Select a collection:', clist)
+collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
-    df_collections=df_collections['Name'].reset_index()
-    pd.set_option('display.max_colwidth', None)
+df_collections=df_collections['Name'].reset_index()
+pd.set_option('display.max_colwidth', None)
 
-    # st.dataframe(df_collections['Name'], height=1500)
+# st.dataframe(df_collections['Name'], height=1500)
 
-    # Collection items
+# Collection items
 
-    items = zot.everything(zot.collection_items_top(collection_code))
+items = zot.everything(zot.collection_items_top(collection_code))
 
-    data3=[]
-    columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link']
+data3=[]
+columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link']
 
-    for item in items:
-        data3.append((item['data']['title'], item['data']['itemType'], item['data']['url'], item['data']['abstractNote'], item['links']['alternate']['href'])) 
-    pd.set_option('display.max_colwidth', None)
+for item in items:
+    data3.append((item['data']['title'], item['data']['itemType'], item['data']['url'], item['data']['abstractNote'], item['links']['alternate']['href'])) 
+pd.set_option('display.max_colwidth', None)
 
-    df = pd.DataFrame(data3, columns=columns3)
+df = pd.DataFrame(data3, columns=columns3)
 
-    df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
-    df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
-    df['Publication type'] = df['Publication type'].replace(['book'], 'Book')
-    df['Publication type'] = df['Publication type'].replace(['bookSection'], 'Book chapter')
-    df['Publication type'] = df['Publication type'].replace(['blogPost'], 'Blog post')
-    df['Publication type'] = df['Publication type'].replace(['videoRecording'], 'Video')
-    df['Publication type'] = df['Publication type'].replace(['podcast'], 'Podcast')
-    df['Publication type'] = df['Publication type'].replace(['newspaperArticle'], 'Newspaper article')
-    df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
-    df['Publication type'] = df['Publication type'].replace(['webpage'], 'Webpage')
+df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
+df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
+df['Publication type'] = df['Publication type'].replace(['book'], 'Book')
+df['Publication type'] = df['Publication type'].replace(['bookSection'], 'Book chapter')
+df['Publication type'] = df['Publication type'].replace(['blogPost'], 'Blog post')
+df['Publication type'] = df['Publication type'].replace(['videoRecording'], 'Video')
+df['Publication type'] = df['Publication type'].replace(['podcast'], 'Podcast')
+df['Publication type'] = df['Publication type'].replace(['newspaperArticle'], 'Newspaper article')
+df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
+df['Publication type'] = df['Publication type'].replace(['webpage'], 'Webpage')
 
-    df_items = '**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] + ' '+ "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+df_items = '**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] + ' '+ "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
 
-    row_nu_1= len(df_items.index)
-    if row_nu_1<15:
-        row_nu_1=row_nu_1
-    else:
-        row_nu_1=15
+row_nu_1= len(df_items.index)
+if row_nu_1<15:
+    row_nu_1=row_nu_1
+else:
+    row_nu_1=15
 
-    st.markdown('#### Collection theme: ' + collection_name)
+st.markdown('#### Collection theme: ' + collection_name)
 
-    with st.expander("Expand to see the list", expanded=False):
-        st.write('This list shows the last 15 added items. To see the full collection list click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
-        # display2 = st.checkbox('Display abstracts')
-        for i in range(row_nu_1):
-            st.write(''+str(i+1)+') ' +df_items.iloc[i])
-            df_items.fillna("nan") 
-            # if display2:
-            #     st.caption(df['Abstract'].iloc[i])
+with st.expander("Expand to see the list", expanded=False):
+    st.write('This list shows the last 15 added items. To see the full collection list click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
+    # display2 = st.checkbox('Display abstracts')
+    for i in range(row_nu_1):
+        st.write(''+str(i+1)+') ' +df_items.iloc[i])
+        df_items.fillna("nan") 
+        # if display2:
+        #     st.caption(df['Abstract'].iloc[i])
 
 
 with col2:
