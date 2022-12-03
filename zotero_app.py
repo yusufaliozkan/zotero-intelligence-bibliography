f01d1b396f87629d29b81c59b6e1832c40599032
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 12:39:43 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -67,95 +67,93 @@ st.header('Items by collection: ')
 col1, col2 =st.columns([3,5])
 
 aaa = zot.collections()
-with col1:
-    data2=[]
-    columns2 = ['Code','Name', 'Number', 'Link', 'Parent collection']
-    for item in aaa:
-        data2.append((item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href'], item['data']['parentCollection']))
+data2=[]
+columns2 = ['Code','Name', 'Number', 'Link', 'Parent collection']
+for item in aaa:
+    data2.append((item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href'], item['data']['parentCollection']))
 
-    pd.set_option('display.max_colwidth', None)
-    df_collections = pd.DataFrame(data2, columns=columns2)
+pd.set_option('display.max_colwidth', None)
+df_collections = pd.DataFrame(data2, columns=columns2)
 
-    df_collections['Name2'] = df_collections['Name'].replace(['01. Intelligence history'],'Intelligence history')
-    df_collections['Name2'] = df_collections['Name2'].replace(['02. Intelligence studies'],'Intelligence studies')
-    df_collections['Name2'] = df_collections['Name2'].replace(['03. Intelligence analysis'],'Intelligence analysis')
-    df_collections['Name2'] = df_collections['Name2'].replace(['04. Intelligence organisations'],'Intelligence organisations')
-    df_collections['Name2'] = df_collections['Name2'].replace(['05. Intelligence failures'],'Intelligence failures')
-    df_collections['Name2'] = df_collections['Name2'].replace(['06. Intelligence, oversight, and ethics'],'Intelligence, oversight, and ethics')
-    df_collections['Name2'] = df_collections['Name2'].replace(['07. a. HUMINT'],'HUMINT')
-    df_collections['Name2'] = df_collections['Name2'].replace(['07. b. IMINT'],'IMINT')
-    df_collections['Name2'] = df_collections['Name2'].replace(['07. c. OSINT - SOCMINT'],'OSINT - SOCMINT')
-    df_collections['Name2'] = df_collections['Name2'].replace(['07. d. SIGINT'],'SIGINT')
-    df_collections['Name2'] = df_collections['Name2'].replace(['07. e. Medical Intelligence'],'Medical Intelligence')
-    df_collections['Name2'] = df_collections['Name2'].replace(['08. Counterintelligence'],'Counterintelligence')
-    df_collections['Name2'] = df_collections['Name2'].replace(['09. Covert action'],'Covert action')
-    df_collections['Name2'] = df_collections['Name2'].replace(['10. Cybersphere'],'Cybersphere')
-    df_collections['Name2'] = df_collections['Name2'].replace(['11. Intelligence in literature and popular culture'],'Intelligence in literature and popular culture')
-    df_collections['Name2'] = df_collections['Name2'].replace(['12. Documentaries and movies'],'Documentaries and movies')
-    df_collections['Name2'] = df_collections['Name2'].replace(['13. Podcasts and videos'],'Podcasts and videos')
-    df_collections['Name2'] = df_collections['Name2'].replace(['14. Miscellaneous'],'Miscellaneous')
-    df_collections['Name2'] = df_collections['Name2'].replace(['15. Methodology'],'Methodology')
-    df_collections['Name2'] = df_collections['Name2'].replace(['98. Special collections'],'Special collections')
-    df_collections['Name2'] = df_collections['Name2'].replace(['99. Archival sources and reports'],'Archival sources and reports')
+df_collections['Name2'] = df_collections['Name'].replace(['01. Intelligence history'],'Intelligence history')
+df_collections['Name2'] = df_collections['Name2'].replace(['02. Intelligence studies'],'Intelligence studies')
+df_collections['Name2'] = df_collections['Name2'].replace(['03. Intelligence analysis'],'Intelligence analysis')
+df_collections['Name2'] = df_collections['Name2'].replace(['04. Intelligence organisations'],'Intelligence organisations')
+df_collections['Name2'] = df_collections['Name2'].replace(['05. Intelligence failures'],'Intelligence failures')
+df_collections['Name2'] = df_collections['Name2'].replace(['06. Intelligence, oversight, and ethics'],'Intelligence, oversight, and ethics')
+df_collections['Name2'] = df_collections['Name2'].replace(['07. a. HUMINT'],'HUMINT')
+df_collections['Name2'] = df_collections['Name2'].replace(['07. b. IMINT'],'IMINT')
+df_collections['Name2'] = df_collections['Name2'].replace(['07. c. OSINT - SOCMINT'],'OSINT - SOCMINT')
+df_collections['Name2'] = df_collections['Name2'].replace(['07. d. SIGINT'],'SIGINT')
+df_collections['Name2'] = df_collections['Name2'].replace(['07. e. Medical Intelligence'],'Medical Intelligence')
+df_collections['Name2'] = df_collections['Name2'].replace(['08. Counterintelligence'],'Counterintelligence')
+df_collections['Name2'] = df_collections['Name2'].replace(['09. Covert action'],'Covert action')
+df_collections['Name2'] = df_collections['Name2'].replace(['10. Cybersphere'],'Cybersphere')
+df_collections['Name2'] = df_collections['Name2'].replace(['11. Intelligence in literature and popular culture'],'Intelligence in literature and popular culture')
+df_collections['Name2'] = df_collections['Name2'].replace(['12. Documentaries and movies'],'Documentaries and movies')
+df_collections['Name2'] = df_collections['Name2'].replace(['13. Podcasts and videos'],'Podcasts and videos')
+df_collections['Name2'] = df_collections['Name2'].replace(['14. Miscellaneous'],'Miscellaneous')
+df_collections['Name2'] = df_collections['Name2'].replace(['15. Methodology'],'Methodology')
+df_collections['Name2'] = df_collections['Name2'].replace(['98. Special collections'],'Special collections')
+df_collections['Name2'] = df_collections['Name2'].replace(['99. Archival sources and reports'],'Archival sources and reports')
 
-    df_collections = df_collections.sort_values(by='Name')
+df_collections = df_collections.sort_values(by='Name')
 
-    clist = df_collections['Name'].unique()
-    collection_name = st.selectbox('Select a collection:', clist)
-    collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Code'].values[0]
+clist = df_collections['Name'].unique()
+collection_name = st.selectbox('Select a collection:', clist)
+collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Code'].values[0]
 
-    df_collections=df_collections['Name'].reset_index()
-    pd.set_option('display.max_colwidth', None)
+df_collections=df_collections['Name'].reset_index()
+pd.set_option('display.max_colwidth', None)
 
-    st.dataframe(df_collections['Name'], height=1500)
-    # Collection items
+st.dataframe(df_collections['Name'], height=1500)
+# Collection items
 
-    items = zot.everything(zot.collection_items_top(collection_code))
+items = zot.everything(zot.collection_items_top(collection_code))
 
-    data3=[]
-    columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link']
+data3=[]
+columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link']
 
-    for item in items:
-        data3.append((item['data']['title'], item['data']['itemType'], item['data']['url'], item['data']['abstractNote'], item['links']['alternate']['href'])) 
-    pd.set_option('display.max_colwidth', None)
+for item in items:
+    data3.append((item['data']['title'], item['data']['itemType'], item['data']['url'], item['data']['abstractNote'], item['links']['alternate']['href'])) 
+pd.set_option('display.max_colwidth', None)
 
-    df = pd.DataFrame(data3, columns=columns3)
+df = pd.DataFrame(data3, columns=columns3)
 
-    df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
-    df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
-    df['Publication type'] = df['Publication type'].replace(['book'], 'Book')
-    df['Publication type'] = df['Publication type'].replace(['bookSection'], 'Book chapter')
-    df['Publication type'] = df['Publication type'].replace(['blogPost'], 'Blog post')
-    df['Publication type'] = df['Publication type'].replace(['videoRecording'], 'Video')
-    df['Publication type'] = df['Publication type'].replace(['podcast'], 'Podcast')
-    df['Publication type'] = df['Publication type'].replace(['newspaperArticle'], 'Newspaper article')
-    df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
-    df['Publication type'] = df['Publication type'].replace(['webpage'], 'Webpage')
+df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
+df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
+df['Publication type'] = df['Publication type'].replace(['book'], 'Book')
+df['Publication type'] = df['Publication type'].replace(['bookSection'], 'Book chapter')
+df['Publication type'] = df['Publication type'].replace(['blogPost'], 'Blog post')
+df['Publication type'] = df['Publication type'].replace(['videoRecording'], 'Video')
+df['Publication type'] = df['Publication type'].replace(['podcast'], 'Podcast')
+df['Publication type'] = df['Publication type'].replace(['newspaperArticle'], 'Newspaper article')
+df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
+df['Publication type'] = df['Publication type'].replace(['webpage'], 'Webpage')
 
-    df_items = '**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] + ' '+ "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
-    row_nu_1= 30 #len(df_items.index)
+df_items = '**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] + ' '+ "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+row_nu_1= 30 #len(df_items.index)
 
-with col2:
-    st.markdown('#### Collection theme: ' + collection_name)
+st.markdown('#### Collection theme: ' + collection_name)
 
-    with st.expander("Expand to see the list", expanded=True):
-        st.write('This list shows the last 30 added items. To see the full collection list click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
-        display2 = st.checkbox('Display abstracts')
-        for i in range(row_nu_1):
-            st.write(''+str(i+1)+') ' +df_items.iloc[i])
-            if display2:
-                st.caption(df['Abstract'].iloc[i])
+with st.expander("Expand to see the list", expanded=True):
+    st.write('This list shows the last 30 added items. To see the full collection list click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
+    display2 = st.checkbox('Display abstracts')
+    for i in range(row_nu_1):
+        st.write(''+str(i+1)+') ' +df_items.iloc[i])
+        if display2:
+            st.caption(df['Abstract'].iloc[i])
 
-    # text_to_be_copied = st.write(''+str(i+1)+') ' +df_items.iloc[i])
-    # copy_dict = {"content": text_to_be_copied}
-    # copy_button = Button(label="Copy HTML template to clipboard")
-    # copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-    #         navigator.clipboard.writeText(content);
-    #         """))
-    # no_event = streamlit_bokeh_events(
-    #         copy_button,
-    #         events="GET_TEXT",
-    #         key="get_text",
-    #         refresh_on_update=True,
-    #         override_height=75,
-    #         debounce_time=0)
+# text_to_be_copied = st.write(''+str(i+1)+') ' +df_items.iloc[i])
+# copy_dict = {"content": text_to_be_copied}
+# copy_button = Button(label="Copy HTML template to clipboard")
+# copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
+#         navigator.clipboard.writeText(content);
+#         """))
+# no_event = streamlit_bokeh_events(
+#         copy_button,
+#         events="GET_TEXT",
+#         key="get_text",
+#         refresh_on_update=True,
+#         override_height=75,
+#         debounce_time=0)
