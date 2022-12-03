e42486f93e36be999d39fea44448517d0dfd8200
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-03 13:29:50 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -69,8 +69,10 @@ for item in bbb:
     data3.append((item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href']))
 pd.set_option('display.max_colwidth', None)
 df_collections_2 = pd.DataFrame(data3, columns=columns3)
-df_collections_2
 
+
+df_collections_2['hyperlinked'] = [df_collections_2['Name']](df_collections_2['Link'])
+df_collections_2
 # Zotero library collections
 st.header('Items by collection: ')
 
