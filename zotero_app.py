e90b1209bc8c8a559f2e6d175b7c32634dde975a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-04 22:29:11 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -60,6 +60,11 @@ with st.sidebar:
         To see more details about the sources, please visit the group library [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/library). 
         If you need more information about Zotero, visit [this page](https://www.intelligencenetwork.org/zotero).
         ''')
+        components.html(
+        """
+        <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />This tool is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+        """
+        )
     with st.expander('Source code'):
         st.info('''
         Source code of this app is available [here](https://github.com/YusufAliOzkan/zotero-intelligence-bibliography).
