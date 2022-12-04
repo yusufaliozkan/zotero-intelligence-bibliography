b8376985fe410ebc9e9d8b9b26cf1a2509e151e5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-04 22:10:35 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -57,7 +57,7 @@ with st.sidebar:
         st.write('''This website lists secondary sources on intelligence studies and intelligence history.
         The sources are originally listed in the [Intelligence bibliography Zotero library](https://www.zotero.org/groups/2514686/intelligence_bibliography).
         This website uses [Zotero API](https://github.com/urschrei/pyzotero) to connect the *Intelligence bibliography Zotero group library*.
-        To see more details about the sources, please visit the Zotero group library. 
+        To see more details about the sources, please visit the [group library](https://www.zotero.org/groups/2514686/intelligence_bibliography/library). 
         If you need more information about Zotero, visit [this page](https://www.intelligencenetwork.org/zotero).
         ''')
     # with st.expander('Source code'):
