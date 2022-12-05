395c72481ccaaa1169d35a544280c8d3918921ad
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 11:02:08 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -22,7 +22,7 @@ items = zot.top(limit=10)
 pd.set_option('display.max_colwidth', None)
 
 data=[]
-columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Collection key', 'First name', 'Last name']
+columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Collection key', 'First name']
 
 for item in items:
     data.append((item['data']['title'], 
@@ -32,7 +32,6 @@ for item in items:
     item['links']['alternate']['href'], 
     item['data']['dateAdded'], 
     item['data']['collections'],
-    item['data']['creators'],
     item['data']['creators'])
     )
 
