b6ee881026f211aaf8d23a480ad0a660fda96f8b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 21:00:40 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -150,15 +150,13 @@ with col1:
     with st.expander('Click to hide the list', expanded=True):
         display = st.checkbox('Display theme/abstract')
 
-        df_last = ('**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] 
-        )
-        df_last_2 = ("[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ 
+        df_last = ('**'+ df['Publication type']+ '**'+ ': ' +  df['Title'] + ' '+ 
+        "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ 
         "[[Zotero link]]" +'('+ df['Zotero link'] + ')' + ' (Added on: ' + df['Date added']+')'
-        )        
+        )
         row_nu_1= len(df_last.index)
         for i in range(row_nu_1):
             st.write(''+str(i+1)+') ' +df_last.iloc[i])
-            st.write(df_last_2.iloc[i])
             if display:
                 st.caption('Themes: ' + '['+df['Name_x'].iloc[i]+']' +'('+ df['Link_x'].iloc[i] + ')' + '   ' +
                 '['+df['Name_y'].iloc[i]+']' +'('+ df['Link_y'].iloc[i] + ')'+'   ' +
