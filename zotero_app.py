a94c5e4969faa3de4051a8e6acb5d02a46b8ddd3
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 23:56:44 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -235,8 +235,6 @@ src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
 © 2022 All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
 """
 )
-<<<<<<< HEAD
-
 
 # Legacy
 
