f89f04af36d1c588b65d1ba57176b0ac2a1e5c91
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 20:16:20 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -92,7 +92,15 @@ merged_df2 = pd.merge(
     right_on='Key',
     how='left'
 )
-merged_df2
+merged_df3 = pd.merge(
+    left=merged_df2,
+    right=df_collections,
+    left_on='col2',
+    right_on='Key',
+    how='left'
+)
+
+merged_df3
 # Streamlit app
 
 st.title("Intelligence bibliography")
