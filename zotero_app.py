a32a02dce328c12b168281bdee7735ffee86aec7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 11:35:08 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -41,7 +41,7 @@ st.set_page_config(layout = "wide",
 
 df = pd.DataFrame(data, columns=columns)
 
-items
+df
 # Change type name
 df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
 df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
