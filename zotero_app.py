2ad7a957ac52ef91c027bd82f974d7a1ad987a6a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-05 20:27:19 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -157,7 +157,7 @@ with col1:
         for i in range(row_nu_1):
             st.write(''+str(i+1)+') ' +df_last.iloc[i])
             if display:
-                st.caption(' Themes: ' + '['+df['Name_x'].iloc[i]+']' +'('+ df['Link_x'].iloc[i] + ')' +', ' + '['+df['Name_y'].iloc[i]+']' +'('+ df['Link_y'].iloc[i] + ')')
+                st.caption(' Themes: ' + '['+df['Name_x'].iloc[i]+']' +'('+ df['Link_x'].iloc[i] + ')' + '['+df['Name_y'].iloc[i]+']' +'('+ df['Link_y'].iloc[i] + ')')
                 st.caption('Abstract:'+'\n '+ df['Abstract'].iloc[i])
 
 # Collection list
