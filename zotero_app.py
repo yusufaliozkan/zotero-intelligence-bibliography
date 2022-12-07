bf6f68812d66369c27cf48804df9325b47cd21e2
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:57:36 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -41,7 +41,6 @@ split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
 split_df2 = split_df2[0]
-split_df2[0].fillna('{}', inplace=True)
 for df in split_df2.values():
     df.fillna('{}', inplace = True)
 
