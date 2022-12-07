a05c617b78a507458a079691e6463a2790a3ea10
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 23:52:30 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -236,7 +236,7 @@ with col2:
 
     # Zotero library collections
 df_collections_2
-st.bar_chart(df_collections_2['Number'])
+st.bar_chart(df_collections_2['Number'], x='Name')
 
 
 components.html(
