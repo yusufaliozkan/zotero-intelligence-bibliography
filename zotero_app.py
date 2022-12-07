fadfe5041ef77ac38eadc4fac55142a6a7273294
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 23:50:15 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -230,11 +230,13 @@ with col2:
         row_nu_collections = len(df_collections_2.index)
         
         for i in range(row_nu_collections):
-            st.caption('[' + df_collections_2.sort_values(by='Name')['Name'].iloc[i]+ ']'+ '('+ df_collections_2.sort_values(by='Name')['Link'].iloc[i] + ')' + ' [' +
-            str(df_collections_2.sort_values(by='Name')['Number'].iloc[i]) + ' items]'
+            st.caption('[' + df_collections_2.sort_values(by='Name')['Name'].iloc[i]+ ']'+ '('+ df_collections_2.sort_values(by='Name')['Link'].iloc[i] + ')' + 
+            ' [' + str(df_collections_2.sort_values(by='Name')['Number'].iloc[i]) + ' items]'
             )
 
     # Zotero library collections
+df_collections_2
+
 
 components.html(
 """
