4c1c0399cf5602744fe6c974bd8981346481e0c0
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:45:13 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -40,7 +40,7 @@ df = pd.DataFrame(data, columns=columns)
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
-split_df2
+split_df2[0] = split_df2[0].fillna('{}')
 pd.json_normalize(split_df2[0])
 
 
