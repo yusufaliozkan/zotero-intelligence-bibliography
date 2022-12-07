bf69a5440e3e30228c956b09b0251ec3a3acc5c7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:47:18 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -42,7 +42,7 @@ df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
 # split_df2[0] = split_df2[0].fillna('{}')
 pd.json_normalize(split_df2[0].head(3))
-
+split_df2[0]
 
     # Change type name
 df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
