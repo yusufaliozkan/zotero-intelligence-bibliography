a9cbf19b4697e0f659a4c2b5b5d4dc7fa0acb052
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:31:57 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -39,8 +39,8 @@ pd.set_option('display.max_colwidth', None)
 df = pd.DataFrame(data, columns=columns)
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
-split_df2 = pd.DataFrame(df['Author'].tolist())
-split_df2[0]
+df['Author'].astype(str)
+df['Author']
 
     # Change type name
 df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
