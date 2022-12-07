ee4d87e24ecea0ba42a7f9bda35514850098cffe
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:46:59 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -40,8 +40,8 @@ df = pd.DataFrame(data, columns=columns)
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
-split_df2[0] = split_df2[0].fillna('{}')
-pd.json_normalize(split_df2[0])
+# split_df2[0] = split_df2[0].fillna('{}')
+pd.json_normalize(split_df2[0].head(3))
 
 
     # Change type name
