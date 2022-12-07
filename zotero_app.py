b6d7470ac855825d346de6e529d7bd363a07397a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 23:07:12 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -42,7 +42,7 @@ df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
 split_df2 = split_df2[0]
 split_df2
-split_df2.rename(columns={ split_df2.columns[0]: "whatever" })
+split_df2.rename(columns={ split_df2.columns[1]: "whatever" })
 split_df2
 
     # Change type name
