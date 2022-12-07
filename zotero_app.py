48414c0e57435957dc9e3228b4c052ce1010a924
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-07 22:59:41 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -42,6 +42,7 @@ df = pd.concat([df, split_df], axis=1)
 split_df2= pd.DataFrame(df['Author'].tolist())
 split_df2 = split_df2[0]
 split_df2
+split_df2["0"].fillna("[]", inplace = True)
 
 
     # Change type name
