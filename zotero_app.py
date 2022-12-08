7f518de7258f917d6c3c07affcdc95188e88198a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 13:51:18 +0000

    d

--- zotero_app.py
+++ zotero_app.py
@@ -253,7 +253,7 @@ src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
 """
 )
 
-types = zot.top(limit=10)
+types = zot.collections()
 
 data_t=[]
 column_t = ['Title', 'Publication type']
