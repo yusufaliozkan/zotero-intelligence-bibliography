37b9f07376338a6d1cb349ecb14cd9f172cb6181
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 00:00:33 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -239,7 +239,7 @@ df_collections_2
 
 df_collections_2.set_index('Name', inplace=True)
 
-st.bar_chart(df_collections_2['Number'])
+st.bar_chart(df_collections_2['Number'].head())
 
 
 components.html(
