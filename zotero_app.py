b617ad6c434ee037c2a092ef9227f766d3b92909
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 22:13:08 +0000

    e

--- zotero_app.py
+++ zotero_app.py
@@ -41,7 +41,8 @@ df = pd.DataFrame(data, columns=columns)
 df['FirstName'] = pd.DataFrame(df['FirstName'].tolist())
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
-pd.json_normalize(df['FirstName'], max_level=0)
+df['FirstName'] = df['FirstName'].fillna(value='s') 
+# pd.json_normalize(df['FirstName'], max_level=0)
 
 df
 
