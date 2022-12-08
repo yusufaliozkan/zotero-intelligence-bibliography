dc6109b8ccf3947de4e1dbd46a0c5f4106498e3f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 00:15:59 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -240,7 +240,7 @@ st.header('Top 10 collections in the library: ')
 df_collections_2.set_index('Name', inplace=True)
 df_collections_2 = df_collections_2.sort_values(['Number'], ascending=[False])
 plot= df_collections_2.head(10)
-st.bar_chart(plot['Number'], height=600, use_container_width=False)
+st.bar_chart(plot['Number'], height=600, width=600, use_container_width=True)
 
 
 components.html(
