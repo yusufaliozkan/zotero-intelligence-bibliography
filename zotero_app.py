fd6a49d8cab213edda777b0c7cb898aafffddccf
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 22:08:16 +0000

    e

--- zotero_app.py
+++ zotero_app.py
@@ -40,7 +40,7 @@ st.set_page_config(layout = "wide",
 pd.set_option('display.max_colwidth', None)
 df = pd.DataFrame(data, columns=columns)
 df_q = pd.DataFrame(df['FirstName'].tolist())
-df_q
+df_q = df_q[0].copy
 split_df= pd.DataFrame(df['Col key'].tolist())
 split_df
 df = pd.concat([df, split_df], axis=1)
