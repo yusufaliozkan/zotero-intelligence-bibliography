0f75c64d3ed1b29f21837a3d4f836d97e80a89cd
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 00:05:22 +0000

    commit

--- zotero_app.py
+++ zotero_app.py
@@ -239,7 +239,7 @@ with col2:
 df_collections_2.set_index('Name', inplace=True)
 df_collections_2 = df_collections_2.sort_values(['Number'], ascending=[False])
 
-st.bar_chart(df_collections_2['Number'].head(15), height=300)
+st.bar_chart(df_collections_2['Number'].head(15), height=1300)
 
 
 components.html(
