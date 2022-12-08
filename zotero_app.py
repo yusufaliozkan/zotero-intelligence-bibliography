2e71dd4a1e634dd38ba920ae1c5720e3cc7a558f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-08 19:29:07 +0000

    s

--- zotero_app.py
+++ zotero_app.py
@@ -19,7 +19,7 @@ zot = zotero.Zotero(library_id, library_type)
 items = zot.top(limit=10)
 
 data=[]
-columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Col key']
+columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Col key', 'Author']
 
 for item in items:
     data.append((item['data']['title'], 
@@ -28,7 +28,8 @@ for item in items:
     item['data']['abstractNote'], 
     item['links']['alternate']['href'], 
     item['data']['dateAdded'], 
-    item['data']['collections']
+    item['data']['collections'],
+    item['data']['creators'][0]
     ))
 
 st.set_page_config(layout = "wide", 
