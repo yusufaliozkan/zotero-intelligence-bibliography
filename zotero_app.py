66a62fa69434124fec7c4a4f4ba053b411fe477f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-09 09:27:01 +0000

    s

--- zotero_app.py
+++ zotero_app.py
@@ -162,8 +162,8 @@ with col1:
     with st.expander('Click to hide the list', expanded=True):
         display = st.checkbox('Display theme and abstract')
 
-        df_last = ('**'+ df['Publication type']+ '**'+ ': ' + '*' + df['Title'] + '*'+' '+
-        ' (by ' + df['firstName'] + ' ' + df['lastName'] + ') ' +
+        df_last = ('**'+ df['Publication type']+ '**'+ ': ' + df['Title'] + ' '+
+        ' (by ' + '*' + df['firstName'] + ' ' + '*' + df['lastName'] + '*' + ') ' +
         "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ 
         "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
         ' (Added on: ' + df['Date added']+')'
