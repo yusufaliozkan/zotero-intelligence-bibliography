3e850bbde9f21e04379e2ad3535d1f572b142cfa
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-09 08:27:51 +0000

    d

--- zotero_app.py
+++ zotero_app.py
@@ -39,12 +39,12 @@ st.set_page_config(layout = "wide",
                     initial_sidebar_state="auto") 
 pd.set_option('display.max_colwidth', None)
 df = pd.DataFrame(data, columns=columns)
-df['FirstName'] = pd.DataFrame(df['FirstName'].tolist())
+# df['FirstName'] = pd.DataFrame(df['FirstName'].tolist())
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
 df
 pd.json_normalize(df, record_path=['FirstName'])
-df
+
 
     # Change type name
 df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
