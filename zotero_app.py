d6afbd13d1fd3d25a79df3fa9a7601e148958bc3
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-09 08:22:47 +0000

    d

--- zotero_app.py
+++ zotero_app.py
@@ -39,10 +39,10 @@ st.set_page_config(layout = "wide",
                     initial_sidebar_state="auto") 
 pd.set_option('display.max_colwidth', None)
 df = pd.DataFrame(data, columns=columns)
-df['FirstName'] = pd.DataFrame(df['FirstName'].tolist())
+# df['FirstName'] = pd.DataFrame(df['FirstName'].tolist())
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
-
+df
 # df
 # pd.json_normalize(df, record_path=['FirstName'])
 # df
