18a70fab17fb1249ccb11795c985968b5c2b60f5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-10 00:13:42 +0000

    d

--- pages/10_Intelligence and cybersphere.py
+++ pages/10_Intelligence and cybersphere.py
@@ -69,7 +69,7 @@ df_collections=df_collections[df_collections['Name'].str.contains("10")]
 
 # clist = df_collections['Name'].unique()
 
-col1, col2, col3 = st.columns([1.4,4,1.6])
+col1, col2, col3 = st.columns([1,4,1.6])
 
 with col1:
     radio = st.radio('Select a collection', df_collections['Name'])
