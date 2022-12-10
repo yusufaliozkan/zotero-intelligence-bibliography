b3d11c1289017c3df281c5dd858bc26160a58d8b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-10 13:17:22 +0000

    e

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -129,15 +129,15 @@ with col2:
     else:
         row_nu_1=15
     df_download = df.drop(['Abstract', 'FirstName2'], axis=1)
-    
+    df_download
     st.markdown('#### Collection theme: ' + collection_name)
     st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).')
     # def convert_df(df):
     #     return df.to_csv(index=False).encode('cp1252') # not utf-8 because of the weird character,  Â
     # csv = convert_df(df_download)
-    csv = df_download
-    # st.caption(collection_name)
-    st.download_button('Press to download the collection', csv, 'collection.csv', "text/csv", key='download-csv')
+    # csv = df_download
+    # # st.caption(collection_name)
+    # st.download_button('Press to download the collection', csv, 'collection.csv', "text/csv", key='download-csv')
     with st.expander("Expand to see the list", expanded=True):
         st.write('To see the collection in Zotero click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
         # display2 = st.checkbox('Display abstracts')
