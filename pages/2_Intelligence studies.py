c04c80f29bdd749f0b99bb6a4065da2ce262be8d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-10 13:29:56 +0000

    s

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -134,7 +134,7 @@ with col2:
 
     df_download = df.drop(['Abstract', 'FirstName2'], axis=1)
     def convert_df(df):
-        return df.to_csv(index=False).encode('cp1252') # not utf-8 because of the weird character,  Â
+        return df.to_csv(index=False).encode('utf-8') # not utf-8 because of the weird character,  Â cp1252
     csv = convert_df(df_download)
     # csv = df_download
     # # st.caption(collection_name)
