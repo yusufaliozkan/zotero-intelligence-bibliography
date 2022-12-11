889f72a8c360a425f3f45caa51007a6de78cc23d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-11 18:59:28 +0000

    d

--- Home.py
+++ Home.py
@@ -167,7 +167,7 @@ with st.sidebar:
 col1, col2 = st.columns([4,1]) 
 with col1:
     st.header('Recently added items: ')
-
+    df
     df_download = df.iloc[:, [0,1,2,4]] 
     def convert_df(df):
         return df.to_csv(index=False).encode('utf-8') # not utf-8 because of the weird character,  Â
