82c656b8eef61ea4e3a15a8e04aadbf4cfd2c40b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-11 13:19:01 +0000

    d

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ api_key = '' # api_key is only needed for private groups and libraries
 
 # Bringing recently changed items
 zot = zotero.Zotero(library_id, library_type)
-items = zot.top(limit=10)
+items = zot.top(limit=15)
 
 data=[]
 columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date modified', 'Col key', 'FirstName']
