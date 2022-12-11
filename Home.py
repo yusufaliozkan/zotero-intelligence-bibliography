3dea14ee7a344b9fe37b765e82a84167e05ae083
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-11 13:12:00 +0000

    d

--- Home.py
+++ Home.py
@@ -168,7 +168,7 @@ with col1:
     st.header('Recently added items: ')
     with st.expander('Click to hide the list', expanded=True):
         display = st.checkbox('Display theme and abstract')
-        df_last
+        df
         df_last = ('**'+ df['Publication type']+ '**'+ ': ' + 
         '['+ df['Title'] + ']'+ '('+ df['Link to publication'] + ')' +
         ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' +
