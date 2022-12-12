d53f56ab84691f972303779710c5dd553660c757
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-12 00:18:20 +0000

    d

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -170,4 +170,11 @@ with col3:
             st.caption('[' + df_collections_2.sort_values(by='Name')['Name'].iloc[i]+ ']'+ '('+ df_collections_2.sort_values(by='Name')['Link'].iloc[i] + ')' + 
             ' [' + str(df_collections_2.sort_values(by='Name')['Number'].iloc[i]) + ' items]'
             )
-    
+
+components.html(
+"""
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
+src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
+© 2022 All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+"""
+)
