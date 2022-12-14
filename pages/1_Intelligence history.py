927069163c01b2ab2bde690856f250d46aef8e2c
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-14 21:46:17 +0000

    s

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -153,7 +153,6 @@ with col2:
 
     df_download = df.drop(['Abstract', 'FirstName2', 'creatorType'], axis=1)
     df_download['First author'] = df_download['firstName'] + ' ' + df_download['lastName']
-    df_download = df.drop(['firstName', 'lastName'], axis=1)
     df_download
 
     st.markdown('#### Collection theme: ' + collection_name)
