5f4fc27da586b045e88a40b4ce50220cba50f15b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-15 15:37:51 +0000

    s

--- Home.py
+++ Home.py
@@ -170,6 +170,7 @@ with col1:
     st.caption('This list also shows recently updated items.')
         
     df_download = df.iloc[:, [0,1,2,4,5,14,15]] 
+    df_download['First author'] = df['firstName'] + ' ' + df['lastName']
     df_download
     def convert_df(df):
         return df.to_csv(index=False).encode('utf-8-sig') # not utf-8 because of the weird character,  Â cp1252
