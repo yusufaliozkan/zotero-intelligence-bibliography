ac397083d33f2585d7691482086d7a14bee897e1
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-15 08:48:20 +0000

    d

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -139,8 +139,8 @@ with col2:
         df_fa = df_fa.apply(lambda x: {} if pd.isna(x) else x) # https://stackoverflow.com/questions/44050853/pandas-json-normalize-and-null-values-in-json
         df_new = pd.json_normalize(df_fa, errors='ignore') 
         df = pd.concat([df, df_new], axis=1)
-        df['firstName'] = df['firstName'].fillna('n.')
-        df['lastName'] = df['lastName'].fillna('a.')
+        df['firstName'] = df['firstName'].fillna('null')
+        df['lastName'] = df['lastName'].fillna('null')
         
         df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
                     df['Title'] + ' '+ 
