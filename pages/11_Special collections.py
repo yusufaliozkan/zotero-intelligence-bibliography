82168bcc15742da0bfbae941d4a31363289b809c
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-20 23:57:36 +0000

    d

--- pages/11_Special collections.py
+++ pages/11_Special collections.py
@@ -65,7 +65,7 @@ pd.set_option('display.max_colwidth', None)
 df_collections = pd.DataFrame(data2, columns=columns2)
 
 df_collections = df_collections.sort_values(by='Name')
-df_collections=df_collections[df_collections['Name'].str.contains("15")]
+df_collections=df_collections[df_collections['Name'].str.contains("98.")]
 df_collections = df_collections.iloc[2: , :]
 
 # clist = df_collections['Name'].unique()
