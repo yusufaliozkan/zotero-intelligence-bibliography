cdf80775582c61d0c26a93b694744fc007e7d9d2
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-20 00:15:05 +0000

    s

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -7,7 +7,7 @@ import numpy as np
 import altair as alt
 from pandas.io.json import json_normalize
 import datetime
-
+from streamlit_extras.stoggle import stoggle
 
 st.set_page_config(layout = "wide", 
                     page_title='Intelligence bibliography',
