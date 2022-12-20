6a1c12b8a5928c66a437a1001cebe6a5377fc395
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-20 00:18:46 +0000

    d

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -123,7 +123,6 @@ with col2:
     st.markdown('#### Collection theme: ' + collection_name)
     st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
 
-
     types = st.multiselect('Publication type', df['Publication type'].unique(), df['Publication type'].unique())
 
 
