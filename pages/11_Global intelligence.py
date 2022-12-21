1da8a24a2c0b0e444a9ada569f2736939fcb880f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-21 19:53:18 +0000

    d

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -6,8 +6,14 @@ import streamlit.components.v1 as components
 import numpy as np
 import altair as alt
 from pandas.io.json import json_normalize
+import datetime
 
-st.title("Special collection")
+st.set_page_config(layout = "wide", 
+                    page_title='Intelligence bibliography',
+                    page_icon="https://images.pexels.com/photos/315918/pexels-photo-315918.png",
+                    initial_sidebar_state="auto") 
+
+st.title("Special collections")
 
 # Connecting Zotero with API
 library_id = '2514686' # intel 2514686
@@ -57,15 +63,14 @@ for item in collections:
 
 pd.set_option('display.max_colwidth', None)
 df_collections = pd.DataFrame(data2, columns=columns2)
-df_collections
 
 df_collections = df_collections.sort_values(by='Name')
-df_collections=df_collections[df_collections['Name'].str.contains("98.")]
+df_collections=df_collections[df_collections['Name'].str.contains("16.")]
 df_collections = df_collections.iloc[2: , :]
 
 # clist = df_collections['Name'].unique()
 
-col1, col2, col3 = st.columns([1,4,1])
+col1, col2, col3 = st.columns([1.4,4,1.6])
 
 with col1:
     radio = st.radio('Select a collection', df_collections['Name'])
@@ -79,7 +84,6 @@ with col1:
 
 with col2:
 # Collection items
-
     count_collection = zot.num_collectionitems(collection_code)
 
     items = zot.everything(zot.collection_items_top(collection_code))
@@ -111,22 +115,62 @@ with col2:
     df['Publication type'] = df['Publication type'].replace(['webpage'], 'Webpage')
     df['Publication type'] = df['Publication type'].replace(['newspaperArticle'], 'Newspaper article')
     df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
+    df['Publication type'] = df['Publication type'].replace(['forumPost'], 'Forum post')
 
-    df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
-                df['Title'] + ' '+ 
-                # ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + # IT CANNOT READ THE NAN VALUES
-                "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
-                "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
-                )
+    st.markdown('#### Collection theme: ' + collection_name)
+    st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
 
+    types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
+
+    df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
+    df = df.reset_index()
+
+    if df['FirstName2'].any() in ("", [], None, 0, False):
+        # st.write('no author')
+        df['firstName'] = 'null'
+        df['lastName'] = 'null'
+
+        df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
+            df['Title'] + ' '+ 
+            ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + 
+            "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
+            "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+            )
+    else:
+        # st.write('author entered')
+        ## This section is for displaying the first author details but it doesn't work for now because of json normalization error.
+        df_fa = df['FirstName2']
+        df_fa = pd.DataFrame(df_fa.tolist())
+        df_fa = df_fa[0]
+        df_fa = df_fa.apply(lambda x: {} if pd.isna(x) else x) # https://stackoverflow.com/questions/44050853/pandas-json-normalize-and-null-values-in-json
+        df_new = pd.json_normalize(df_fa, errors='ignore') 
+        df = pd.concat([df, df_new], axis=1)
+        df['firstName'] = df['firstName'].fillna('null')
+        df['lastName'] = df['lastName'].fillna('null')
+        
+        df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
+                    df['Title'] + ' '+ 
+                    ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + # IT CANNOT READ THE NAN VALUES
+                    "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
+                    "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+                    )
     row_nu_1= len(df.index)
     # if row_nu_1<15:
     #     row_nu_1=row_nu_1
     # else:
     #     row_nu_1=15
 
-    st.markdown('#### Collection theme: ' + collection_name)
-    st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).')
+    df['First author'] = df['firstName'] + ' ' + df['lastName']
+    df_download = df[['Title', 'Publication type', 'First author', 'Link to publication', 'Zotero link']]
+
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8-sig') # not utf-8 because of the weird character,  Â cp1252
+    today = datetime.date.today().isoformat()
+    csv = convert_df(df_download)
+    # csv = df_download
+    # # st.caption(collection_name)
+    st.download_button('Download the collection', csv, collection_name+ '-'+today +'.csv', mime="text/csv", key='download-csv')
+
     with st.expander("Expand to see the list", expanded=True):
         st.write('To see the collection in Zotero click [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/collections/' + collection_code + ')')
         # display2 = st.checkbox('Display abstracts')
@@ -151,3 +195,29 @@ with col3:
             st.caption('[' + df_collections_2.sort_values(by='Name')['Name'].iloc[i]+ ']'+ '('+ df_collections_2.sort_values(by='Name')['Link'].iloc[i] + ')' + 
             ' [' + str(df_collections_2.sort_values(by='Name')['Number'].iloc[i]) + ' items]'
             )
+
+    with st.expander('Collections in this site', expanded=False):
+        st.caption('[Intelligence history](https://intelligence.streamlit.app/Intelligence_history)')
+        st.caption('[Intelligence studies](https://intelligence.streamlit.app/Intelligence_studies)')
+        st.caption('[Intelligence analysis](https://intelligence.streamlit.app/Intelligence_analysis)')
+        st.caption('[Intelligence organisations](https://intelligence.streamlit.app/Intelligence_organisations)')
+        st.caption('[Intelligence oversight and ethics](https://intelligence.streamlit.app/Intelligence_oversight_and_ethics)')
+        st.caption('[Intelligence collection](https://intelligence.streamlit.app/Intelligence_collection)')
+        st.caption('[Counterintelligence](https://intelligence.streamlit.app/Counterintelligence)')
+        st.caption('[Covert action](https://intelligence.streamlit.app/Covert_action)')
+        st.caption('[Intelligence and cybersphere](https://intelligence.streamlit.app/Intelligence_and_cybersphere)')
+        st.caption('[Special collections](https://intelligence.streamlit.app/Special_collections)')
+
+st.markdown('#### Item types of '+ collection_name)
+df_plot= df['Publication type'].value_counts()
+plot= df_plot
+st.bar_chart(plot.sort_values(ascending=False), height=600, width=600, use_container_width=True)
+
+
+components.html(
+"""
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
+src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
+© 2022 All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+"""
+)
