7f50194d7ccd429d237a52e5321b9830264afb0e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-21 20:30:06 +0000

    d

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -74,8 +74,7 @@ col1, col2, col3 = st.columns([1.4,4,1.6])
 with col1:
     radio = st.radio('Select a collection', df_collections['Name'])
     radio2=df_collections['Name']
-    radio2
-    
+        
     # collection_name = st.selectbox('Select a collection:', clist)
     collection_name = radio
     
