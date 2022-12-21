1af41530a3ca3446c67ce519e6eedd79bdac4cc5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-21 20:27:22 +0000

    d

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -66,21 +66,20 @@ df_collections = pd.DataFrame(data2, columns=columns2)
 
 df_collections = df_collections.sort_values(by='Name')
 df_collections=df_collections[df_collections['Name'].str.contains("Global")].reset_index()
-df_collections['Key'].loc[0]
 
 # clist = df_collections['Name'].unique()
 
 col1, col2, col3 = st.columns([1.4,4,1.6])
 
 with col1:
-    radio = st.radio('Select a collection', df_collections['Name'])
-    radio2=df_collections['Name']
-    radio2
+    # radio = st.radio('Select a collection', df_collections['Name'])
+    # radio2=df_collections['Name']
+    # radio2
     
-    # collection_name = st.selectbox('Select a collection:', clist)
-    collection_name = radio
+    # # collection_name = st.selectbox('Select a collection:', clist)
+    # collection_name = radio
     
-    collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
+    collection_code = df_collections['Key'].loc[0]
     
     df_collections=df_collections['Name'].reset_index()
     pd.set_option('display.max_colwidth', None)
