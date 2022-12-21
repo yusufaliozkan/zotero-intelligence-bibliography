3405ecd056eb3e1c013efa2d64f65acfb3d1ea5d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-21 20:01:41 +0000

    f

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -66,6 +66,7 @@ df_collections = pd.DataFrame(data2, columns=columns2)
 
 df_collections = df_collections.sort_values(by='Name')
 df_collections=df_collections[df_collections['Name'].str.contains("16.")]
+df_collections
 
 # clist = df_collections['Name'].unique()
 
