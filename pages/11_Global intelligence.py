6db81dd00362cb0e55ed1733ae3403f0f8347021
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-21 20:26:48 +0000

    d

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -66,7 +66,7 @@ df_collections = pd.DataFrame(data2, columns=columns2)
 
 df_collections = df_collections.sort_values(by='Name')
 df_collections=df_collections[df_collections['Name'].str.contains("Global")].reset_index()
-df_collections.loc['Key']
+df_collections['Key'].loc[0]
 
 # clist = df_collections['Name'].unique()
 
