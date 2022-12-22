69796e888352057f913e54281c97aba770951495
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-22 09:16:26 +0000

    \

--- Home.py
+++ Home.py
@@ -324,7 +324,6 @@ with col1:
 
 with col2:
     with st.expander("Collections in Zotero library", expanded=False):
-        st.markdown('''[Visuals](#Visuals)''', unsafe_allow_html=True)
         row_nu_collections = len(df_collections_2.index)        
         for i in range(row_nu_collections):
             st.caption('[' + df_collections_2.sort_values(by='Name')['Name'].iloc[i]+ ']'+ '('+ df_collections_2.sort_values(by='Name')['Link'].iloc[i] + ')' + 
