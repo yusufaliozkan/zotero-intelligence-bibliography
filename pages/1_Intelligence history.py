a835a342fceb8f650b50c272ad4c3046c871cac2
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-22 00:01:45 +0000

    e

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -222,7 +222,7 @@ st.bar_chart(plot.sort_values(ascending=False), height=600, width=600, use_conta
 
 fig = px.pie(plot, values='Publication type')
 fig.show()
-st.plotly_chart()
+st.plotly_chart(fig)
 
 components.html(
 """
