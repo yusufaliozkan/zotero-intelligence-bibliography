ff112d021082af7a8db64938b962fc5e6c9a89ec
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-23 10:08:55 +0000

    s

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -121,7 +121,7 @@ with col2:
     df['Publication type'] = df['Publication type'].replace(['forumPost'], 'Forum post')
 
     df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
-    df['Date published'] = df['Date published'].map(lambda x: x.strftime('%Y') if x else 'No date')
+    # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
     df
 
     st.markdown('#### Collection theme: ' + collection_name)
