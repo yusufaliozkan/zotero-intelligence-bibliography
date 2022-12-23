4d939a13e4edac90003b14c5fda475b85af3b7ee
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-23 10:15:58 +0000

    d

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -108,7 +108,6 @@ with col2:
 
     df = pd.DataFrame(data3, columns=columns3)
 
-    df['Date published'] = df['Date published'].astype('float')
     df['Date published'] = df['Date published'].astype('Int32')
 
     # df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
