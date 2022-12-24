d7201a56be5ce9d2bde78ff583d5bc3bd918a492
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-24 01:06:13 +0000

    s

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -261,7 +261,7 @@ fig.update_layout(
     width=400,
     height=400,)
 fig.update_layout(title={'text':'Publications: '+collection_name, 'y':0.95, 'x':0.3, 'yanchor':'top'})
-col2.plotly_chart(fig, use_container_width = True)
+st.plotly_chart(fig, use_container_width = True)
 
 
 components.html(
