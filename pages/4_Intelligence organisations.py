9ab07ceb411ccabd91eee6e97eac4d1ffb0f2326
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-24 00:37:22 +0000

    d

--- pages/4_Intelligence organisations.py
+++ pages/4_Intelligence organisations.py
@@ -93,7 +93,7 @@ with col2:
     items = zot.everything(zot.collection_items_top(collection_code))
 
     data3=[]
-    columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'FirstName2']
+    columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date published', 'FirstName2']
 
     for item in items:
         data3.append((
@@ -102,11 +102,16 @@ with col2:
             item['data']['url'], 
             item['data']['abstractNote'], 
             item['links']['alternate']['href'],
+            item['data'].get('date'),
             item['data']['creators']
             )) 
     pd.set_option('display.max_colwidth', None)
 
     df = pd.DataFrame(data3, columns=columns3)
+
+    # df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
+    # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
+    # df
 
     df['Publication type'] = df['Publication type'].replace(['thesis'], 'Thesis')
     df['Publication type'] = df['Publication type'].replace(['journalArticle'], 'Journal article')
@@ -121,11 +126,14 @@ with col2:
     df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
     df['Publication type'] = df['Publication type'].replace(['forumPost'], 'Forum post')
 
+    df['Date published'] = pd.to_datetime(df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
+    df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
+    df['Date published'] = df['Date published'].fillna('No date')
+
     st.markdown('#### Collection theme: ' + collection_name)
     st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
 
-    types = st.multiselect('Publication type', df['Publication type'].unique(), df['Publication type'].unique())
-
+    types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
 
     df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
     df = df.reset_index()
@@ -139,7 +147,8 @@ with col2:
             df['Title'] + ' '+ 
             ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + 
             "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
-            "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+            "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
+            ' (Published on: ' +df['Date published'] + ')'
             )
     else:
         # st.write('author entered')
@@ -157,7 +166,8 @@ with col2:
                     df['Title'] + ' '+ 
                     ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + # IT CANNOT READ THE NAN VALUES
                     "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
-                    "[[Zotero link]]" +'('+ df['Zotero link'] + ')'
+                    "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
+                    ' (Published on: ' +df['Date published'] + ')'
                     )
 
     row_nu_1= len(df.index)
@@ -167,7 +177,7 @@ with col2:
     #     row_nu_1=15
 
     df['First author'] = df['firstName'] + ' ' + df['lastName']
-    df_download = df[['Title', 'Publication type', 'First author', 'Link to publication', 'Zotero link']]
+    df_download = df[['Title', 'Publication type', 'First author', 'Link to publication', 'Zotero link', 'Date published']]
 
     def convert_df(df):
         return df.to_csv(index=False).encode('utf-8-sig') # not utf-8 because of the weird character,  Â cp1252
