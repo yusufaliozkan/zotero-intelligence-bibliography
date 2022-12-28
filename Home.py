c41969806399605a223d77710ad05ad9cfa0af25
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-12-28 12:55:07 +0000

    s

--- Home.py
+++ Home.py
@@ -71,7 +71,11 @@ df['Publication type'] = df['Publication type'].replace(['report'], 'Report')
 df['Publication type'] = df['Publication type'].replace(['forumPost'], 'Forum post')
 
 df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
-df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
+df['Date published'] = pd.to_datetime(df['Date published'],utc=True).dt.tz_convert('Europe/London')
+df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
+df['Date published'] = df['Date published'].fillna('No date')
+
+# df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
 df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce')
 df['Date added'] = df['Date added'].dt.strftime('%d/%m/%Y')
 df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
