081f3dce96e47cf9c9f78c19cdd3b21f0cceb35f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-10 17:33:57 +0000

    s

--- Home.py
+++ Home.py
@@ -493,7 +493,8 @@ with col2:
     fig.update_layout(
         autosize=False,
         width=1200,
-        height=600,)
+        height=600,
+        showlegend=False)
     fig.update_xaxes(tickangle=-70)
     fig.update_layout(title={'text':'Top 15 journals that publish intelligence articles', 'y':0.95, 'x':0.4, 'yanchor':'top'})
     col2.plotly_chart(fig, use_container_width = True)
