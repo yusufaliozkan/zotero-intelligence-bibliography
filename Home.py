9372adab404f1bf740a35de55ca6c64197d8c3ea
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-10 17:54:46 +0000

    s

--- Home.py
+++ Home.py
@@ -493,7 +493,7 @@ with col2:
     fig.update_layout(
         autosize=False,
         width=1200,
-        height=750,
+        height=700,
         showlegend=False)
     fig.update_xaxes(tickangle=-70)
     fig.update_layout(title={'text':'Top 15 journals that publish intelligence articles', 'y':0.95, 'x':0.4, 'yanchor':'top'})
