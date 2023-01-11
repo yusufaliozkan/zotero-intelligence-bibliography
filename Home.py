c1d55572caf9ef6cf260039e2a8840d1ee7f8f63
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-11 23:54:12 +0000

    t

--- Home.py
+++ Home.py
@@ -581,7 +581,7 @@ with col2:
                 height=700,
                 showlegend=False)
             fig.update_xaxes(tickangle=-70)
-            fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles (in log scale)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
+            fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles', 'y':0.95, 'x':0.4, 'yanchor':'top'})
             col2.plotly_chart(fig, use_container_width = True)
         else:
             fig = px.bar(df_journal, x='Journal', y='Count', color='Journal', log_y=False)
@@ -591,7 +591,7 @@ with col2:
                 height=700,
                 showlegend=True)
             fig.update_xaxes(tickangle=-70)
-            fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles (in log scale)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
+            fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
             col2.plotly_chart(fig, use_container_width = True)
     with st.expander('See journals'):
         row_nu_collections = len(df_journal.index)        
