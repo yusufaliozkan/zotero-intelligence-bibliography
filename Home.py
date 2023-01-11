bd1c2295eef279731962edb487154dffaf3df42a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-11 23:27:29 +0000

    s

--- Home.py
+++ Home.py
@@ -479,16 +479,14 @@ with col1:
     leg1 = st.checkbox('Display the plot legend', key='leg1')
 
     if log1:
-        if leg1:
-            fig = px.bar(df_publisher, x='Publisher', y='Count', color='Publisher', log_y=True)
-            fig.update_layout(
-                autosize=False,
-                width=1200,
-                height=700,
-                showlegend=False)
-            fig.update_xaxes(tickangle=-70)
-            fig.update_layout(title={'text':'Top ' + str(number) + ' publishers (in log scale)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
-            col1.plotly_chart(fig, use_container_width = True)
+        fig = px.bar(df_publisher, x='Publisher', y='Count', color='Publisher', log_y=True)
+        fig.update_layout(
+            autosize=False,
+            width=1200,
+            height=700,)
+        fig.update_xaxes(tickangle=-70)
+        fig.update_layout(title={'text':'Top ' + str(number) + ' publishers (in log scale)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
+        col1.plotly_chart(fig, use_container_width = True)
     else:
         fig = px.bar(df_publisher, x='Publisher', y='Count', color='Publisher', log_y=False)
         fig.update_layout(
