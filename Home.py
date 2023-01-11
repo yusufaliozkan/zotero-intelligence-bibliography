280af18990d7ff131f7d79d80a8e1582e9150992
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-11 23:30:48 +0000

    s

--- Home.py
+++ Home.py
@@ -431,7 +431,7 @@ with col1:
             width=1200,
             height=600,)
         fig.update_xaxes(tickangle=-70)
-        fig.update_layout(title={'text':'All items in the library (by item type)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
+        fig.update_layout(title={'text':'All items in the library (by item type) in log scale', 'y':0.95, 'x':0.4, 'yanchor':'top'})
         col1.plotly_chart(fig, use_container_width = True)
     else:
         fig = px.bar(df_types, x='Publication type', y='Count', color='Publication type')
