c151b429ff9f246013174869a592ee29b381e895
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-12 23:49:19 +0000

    r

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -446,7 +446,15 @@ df['token_title']=df['clean_title'].apply(tokenization)
 df['token_abstract']=df['clean_abstract'].apply(tokenization)
 stopword = nltk.corpus.stopwords.words('english')
 
-SW = ['york', 'intelligence', 'security', 'pp', 'war','world', 'article', 'twitter',
+if collection_name=='98.1 War in Ukraine':
+    SW = ['york', 'intelligence', 'security', 'pp', 'war','world', 'article', 'twitter',
+        'ukraine', 'russian', 'ukrainian', 'russia', 
+        'new', 'isbn', 'book', 'also', 'yet', 'matter', 'erratum', 'commentary', 'studies',
+        'volume', 'paper', 'study', 'question', 'editorial', 'welcome', 'introduction', 'editorial', 'reader',
+        'university', 'followed', 'particular', 'based', 'press', 'examine', 'show', 'may', 'result', 'explore',
+        'examines', 'become', 'used', 'journal', 'london', 'review']
+else:
+    SW = ['york', 'intelligence', 'security', 'pp', 'war','world', 'article', 'twitter',
     'new', 'isbn', 'book', 'also', 'yet', 'matter', 'erratum', 'commentary', 'studies',
     'volume', 'paper', 'study', 'question', 'editorial', 'welcome', 'introduction', 'editorial', 'reader',
     'university', 'followed', 'particular', 'based', 'press', 'examine', 'show', 'may', 'result', 'explore',
