8bccb65e83111e400380389351994b2f2e3f5eb0
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-12 20:56:40 +0000

    e

--- Home.py
+++ Home.py
@@ -668,7 +668,7 @@ if wordcloud_opt=='Titles':
     wordcloud = WordCloud(stopwords=stopword, width=1500, height=750, background_color='white', collocations=False, colormap='magma').generate(wordcloud_texts_str)
     plt.figure(figsize=(20,8))
     plt.axis('off')
-    plt.title('Top words in title (collection: ' +collection_name+')')
+    plt.title('Top words in title (Intelligence bibliography collection)')
     plt.imshow(wordcloud)
     plt.axis("off")
     plt.show()
