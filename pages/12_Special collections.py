b2fbc76b6330f95ac7e1932f8d5f33427a09cc56
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-13 10:34:07 +0000

    d

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -275,11 +275,11 @@ with col2:
 df['Date published'] = pd.to_datetime(df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
 df['month'] = df['Date published'].dt.month
 df['year'] = df['Date published'].dt.year
-df
 # df['year'] = df['year'].astype(int)
 # df
 df['Date year'] = df['Date published'].dt.strftime('%Y-%m')
 df['Date year'] = df['Date year'].fillna('No date')
+df
 df_year=df['Date year'].value_counts()
 df_year=df_year.reset_index()
 
