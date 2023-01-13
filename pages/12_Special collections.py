81882c932b0da91676be8c3b7a22319ced93a768
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-13 15:05:35 +0000

    s

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -272,8 +272,8 @@ with col2:
     col2.plotly_chart(fig, use_container_width = True)
 
 df['Date published'] = pd.to_datetime(df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
-df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
-df['Date published'] = df['Date published'].fillna('No date')
+df['Date year'] = df['Date published'].dt.strftime('%Y')
+df['Date month'] = df['Date published'].dt.strftime('%m')
 
 df['Date year'] = df['Date year'].fillna('No date')
 df_year=df['Date year'].value_counts()
