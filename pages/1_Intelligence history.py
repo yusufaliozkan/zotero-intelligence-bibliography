a256a59e98ee1d63718f0a86dd87fbfe4508fb87
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-14 23:19:02 +0000

    s

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -78,20 +78,19 @@ df_collections=df_collections[df_collections['Name'].str.contains("01.")]
 df_collections = df_collections.iloc[1: , :]
 
 # clist = df_collections['Name'].unique()
-
-col1, col2, col3 = st.columns([1.5,4,1.6])
-
-with col1:
-    radio = st.radio('Select a collection', df_collections['Name'])
-    # collection_name = st.selectbox('Select a collection:', clist)
-    collection_name = radio
-    collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
-
-    df_collections=df_collections['Name'].reset_index()
-    pd.set_option('display.max_colwidth', None)
-
 tab1, tab2 = st.tabs(['Publications', 'Dashboard'])
 with tab1:
+    col1, col2, col3 = st.columns([1.5,4,1.6])
+
+    with col1:
+        radio = st.radio('Select a collection', df_collections['Name'])
+        # collection_name = st.selectbox('Select a collection:', clist)
+        collection_name = radio
+        collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
+
+        df_collections=df_collections['Name'].reset_index()
+        pd.set_option('display.max_colwidth', None)
+
     with col2:
     # Collection items
 
