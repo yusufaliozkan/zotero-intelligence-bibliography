a18e079ad23d370d98e6e61c3e0716add94906b8
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 20:02:12 +0000

    x

--- Home.py
+++ Home.py
@@ -423,8 +423,8 @@ with tab2:
     df_csv = pd.read_csv('all_items.csv')
     with st.expander('Select publication type'):
         container = st.container()
-        types = st.multiselect('Publication type', df_csv['Publication type'].unique())
-        df_csv = df_csv[df_csv['Publication type'].isin(types)]
+        # types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
+        # df_csv = df_csv[df_csv['Publication type'].isin(types)]
         all = container.checkbox('Select all')
         if all:
             types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
