5082f9f5a6447c4e583bd05d4750c10b4f37627b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 21:48:37 +0000

    f

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -147,7 +147,7 @@ with tab1:
         st.markdown('#### Collection theme: ' + collection_name)
         st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
 
-        types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
+        types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique(), key='original')
         df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
         df = df.reset_index()
 
