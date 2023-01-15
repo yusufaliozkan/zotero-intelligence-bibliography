b1b8dbed2bdaf599f2bc3a911b8d5713ffd6547e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 22:38:39 +0000

    s

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -80,17 +80,8 @@ df_collections = df_collections.iloc[1: , :]
 # clist = df_collections['Name'].unique()
 tab1, tab2 = st.tabs(['Publications', 'Dashboard'])
 with tab1:
-    col1, col2, col3 = st.columns([1.5,4,1.6])
+    col1, col2 = st.columns([5,1.6])
     with col1:
-        radio = st.radio('Select a collection', df_collections['Name'])
-        # collection_name = st.selectbox('Select a collection:', clist)
-        collection_name = radio
-        collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
-
-        df_collections=df_collections['Name'].reset_index()
-        pd.set_option('display.max_colwidth', None)
-
-    with col2:
     # Collection items
         count_collection = zot.num_collectionitems(collection_code)
 
@@ -210,7 +201,14 @@ with tab1:
                 # if display2:
                 #     st.caption(df['Abstract'].iloc[i])
 
-    with col3:
+    with col2:
+        radio = st.radio('Select a collection', df_collections['Name'])
+        # collection_name = st.selectbox('Select a collection:', clist)
+        collection_name = radio
+        collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
+
+        df_collections=df_collections['Name'].reset_index()
+        pd.set_option('display.max_colwidth', None)
         with st.expander("Collections in Zotero library", expanded=False):
             bbb = zot.collections()
             data3=[]
