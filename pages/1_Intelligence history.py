900322e17a145203bfbfaae0f5fe7a7f0ca89c34
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 22:30:31 +0000

    f

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -80,6 +80,7 @@ df_collections = df_collections.iloc[1: , :]
 # clist = df_collections['Name'].unique()
 tab1, tab2 = st.tabs(['Publications', 'Dashboard'])
 with tab1:
+    st.write('222')
     col1, col2, col3 = st.columns([1.5,4,1.6])
 
     with col1:
