0c78f31191189cd743ed8c57bdf2ced082f924e4
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 17:57:55 +0000

    f

--- Home.py
+++ Home.py
@@ -427,7 +427,8 @@ with tab2:
             all = st.checkbox("Select all")
             if all:
                 types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
-            
+            else:
+                types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
         df_csv = df_csv[df_csv['Publication type'].isin(types)]
     df_types = pd.DataFrame(df_csv['Publication type'].value_counts())
     df_types = df_types.sort_values(['Publication type'], ascending=[False])
