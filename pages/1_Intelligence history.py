ca4dcd217f9a097ee66f6680b702b62b3ec6ff43
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-15 22:47:37 +0000

    j

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -241,6 +241,14 @@ with tab1:
 with tab2:
     st.header('Dashboard')
     st.markdown('#### Collection theme: ' + collection_name)
+
+    types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique(), key='original')
+    df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
+    df = df.reset_index()
+    all = st.checkbox('Show all types')
+    if all:
+        types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique(), key='original')
+        df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]        
     
     if df['Title'].any() in ("", [], None, 0, False):
         st.write('No data to visualise')
