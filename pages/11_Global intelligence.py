9233e2bf3769883b13ac5daccb5b4e79c42c0330
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 08:09:08 +0000

    s

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -149,7 +149,7 @@ with col2:
         st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
         st.info('This collection lists academic sources that are non-UK/US on intelligence.')
 
-        types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
+        types = container.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
         df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
         df = df.reset_index()
 
