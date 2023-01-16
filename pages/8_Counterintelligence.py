94d890b24c7605562749c38dc8be9c546b9d965b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 09:56:25 +0000

    s

--- pages/8_Counterintelligence.py
+++ pages/8_Counterintelligence.py
@@ -87,7 +87,7 @@ with tab1:
     col1, col2 = st.columns([5,1.6])
 
     with col1:
-        collection_name = df_collections[0]['Title']
+        collection_name = df_collections[0]['Name']
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
