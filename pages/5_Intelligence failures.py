d9b448926518c3f821328f88e10b9a72cf70d499
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 09:02:50 +0000

    s

--- pages/5_Intelligence failures.py
+++ pages/5_Intelligence failures.py
@@ -76,6 +76,7 @@ df_collections = pd.DataFrame(data2, columns=columns2)
 
 df_collections = df_collections.sort_values(by='Name')
 df_collections=df_collections[df_collections['Name'].str.contains("05")]
+df_collections = df_collections.reset_index(drop=True)
 # df_collections = df_collections.iloc[1: , :]
 
 # clist = df_collections['Name'].unique()
@@ -84,9 +85,7 @@ with tab1:
     col1, col2, col3 = st.columns([1.4,4,1.6])
 
     with col1:
-        radio = st.radio('Select a collection', df_collections['Name'])
-        # collection_name = st.selectbox('Select a collection:', clist)
-        collection_name = radio
+        collection_name = df_collections.loc[0]['Name']
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
