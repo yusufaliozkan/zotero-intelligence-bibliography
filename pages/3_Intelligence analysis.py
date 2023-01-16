c6c2bec85639937a162beaf843aad8f314febd95
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 08:38:35 +0000

    d

--- pages/3_Intelligence analysis.py
+++ pages/3_Intelligence analysis.py
@@ -84,9 +84,7 @@ with tab1:
     col1, col2, col3 = st.columns([1.2,4,1.6])
 
     with col1:
-        radio = st.radio('Select a collection', df_collections['Name'])
-        # collection_name = st.selectbox('Select a collection:', clist)
-        collection_name = radio
+        collection_name = df_collections.loc[0]['Name']
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
