bcf4bd8edca87b9066d46a1906988f691c5e6410
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 09:07:27 +0000

    d

--- pages/5_Intelligence failures.py
+++ pages/5_Intelligence failures.py
@@ -82,14 +82,18 @@ df_collections = df_collections.reset_index(drop=True)
 # clist = df_collections['Name'].unique()
 tab1, tab2 = st.tabs(['Publications', 'Dashboard'])
 with tab1:
-    col1, col2 = st.columns([5,1.6])
+    col1, col2, col3 = st.columns([1.2,4,1.6])
 
     with col1:
-        collection_name = df_collections.loc[0]['Name']
+        radio = st.radio('Select a collection', df_collections['Name'])
+        # collection_name = st.selectbox('Select a collection:', clist)
+        collection_name = radio
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
         pd.set_option('display.max_colwidth', None)
+
+    with col2:    
 
         count_collection = zot.num_collectionitems(collection_code)
 
@@ -210,7 +214,7 @@ with tab1:
                 # if display2:
                 #     st.caption(df['Abstract'].iloc[i])
 
-    with col2:
+    with col3:
         with st.expander("Collections in Zotero library", expanded=False):
             bbb = zot.collections()
             data3=[]
