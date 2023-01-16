a2e99746ea3bd77382528303185e0fc50cf3eb61
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 08:40:10 +0000

    s

--- pages/3_Intelligence analysis.py
+++ pages/3_Intelligence analysis.py
@@ -85,7 +85,7 @@ with tab1:
     col1, col2, col3 = st.columns([1.2,4,1.6])
 
     with col1:
-        collection_name = df_collections.loc[0]['Name']
+        collection_name = df_collections.loc['03 Intelligence analysis']
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
