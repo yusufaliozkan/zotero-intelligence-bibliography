04afe555904899d12eb86fdd719248bf6427e9c0
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 10:01:01 +0000

    s

--- pages/9_Covert action.py
+++ pages/9_Covert action.py
@@ -146,6 +146,7 @@ with tab1:
         st.markdown('#### Collection theme: ' + collection_name)
         st.caption('This collection has ' + str(count_collection) + ' items (this number may include reviews attached to sources).') # count_collection
 
+        df2 = df.copy()
         types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique())
 
         df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
@@ -240,6 +241,14 @@ with tab1:
 with tab2:
     st.header('Dashboard')
     st.markdown('#### Collection theme: ' + collection_name)
+
+    if df['Title'].any() in ("", [], None, 0, False):
+        all = st.checkbox('Show all types')
+        if all:
+            df=df2.copy()
+    types = st.multiselect('Publication type', df['Publication type'].unique(),df['Publication type'].unique(), key='original2')
+    df = df[df['Publication type'].isin(types)]  #filtered_df = df[df["app"].isin(selected_options)]
+    df = df.reset_index()  
     if df['Title'].any() in ("", [], None, 0, False):
         st.write('No data to visualise')
         st.stop()
