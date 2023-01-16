508aa2a499606e02557b8c1e47efea001e75777e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-16 09:38:02 +0000

    f

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -77,22 +77,22 @@ df_collections = df_collections.sort_values(by='Name')
 df_collections=df_collections[df_collections['Name'].str.contains("02")]
 # df_collections = df_collections.iloc[1: , :]
 
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+container = st.container()
+
 # clist = df_collections['Name'].unique()
 tab1, tab2 = st.tabs(['Publications', 'Dashboard'])
 with tab1:
-    col1, col2, col3 = st.columns([1.2,4,1.6])
+    col1, col2 = st.columns([5,1.6])
 
     with col1:
-        radio = st.radio('Select a collection', df_collections['Name'])
+        radio = container.radio('Select a collection', df_collections['Name'])
         # collection_name = st.selectbox('Select a collection:', clist)
         collection_name = radio
         collection_code = df_collections.loc[df_collections['Name']==collection_name, 'Key'].values[0]
 
         df_collections=df_collections['Name'].reset_index()
         pd.set_option('display.max_colwidth', None)
-
-    with col2:
-    # Collection items
 
         count_collection = zot.num_collectionitems(collection_code)
 
@@ -212,7 +212,7 @@ with tab1:
                 # if display2:
                 #     st.caption(df['Abstract'].iloc[i])
 
-    with col3:
+    with col2:
         with st.expander("Collections in Zotero library", expanded=False):
             bbb = zot.collections()
             data3=[]
