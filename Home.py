d96f84e5c68f638ff2dba1dc81c78551cf462631
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 11:24:29 +0000

    s

--- Home.py
+++ Home.py
@@ -472,8 +472,9 @@ with tab2:
     df_year=df_year.rename(columns={'index':'Publication year','Date year':'Count'})
     df_year.drop(df_year[df_year['Publication year']== 'No date'].index, inplace = True)
     df_year=df_year.sort_values(by='Publication year', ascending=True)
+    df_year=df_year.reset_index(drop=True)
     df_year
-
+    
     col1, col2 = st.columns(2)
     with col1:
         fig = px.bar(df_year, x='Publication year', y='Count')
