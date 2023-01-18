cc98e91c12a5b1c8d6b62a8df51a6ea4d480ee19
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 11:46:09 +0000

    d

--- Home.py
+++ Home.py
@@ -435,6 +435,8 @@ with tab2:
     df_year=df_year.reset_index(drop=True)
     max = df_year['Publication year'].max()
     min = df_year['Publication year'].min()
+    int(max)
+    int(min)
 
     with st.expander('Select publication type', expanded=False):
         types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
