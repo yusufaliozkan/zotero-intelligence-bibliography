92903a46565269501a5333e1b71b70a33b8c9954
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 11:49:51 +0000

    s

--- Home.py
+++ Home.py
@@ -433,9 +433,10 @@ with tab2:
     df_year.drop(df_year[df_year['Publication year']== 'No date'].index, inplace = True)
     df_year=df_year.sort_values(by='Publication year', ascending=True)
     df_year=df_year.reset_index(drop=True)
-    int(df_year['Publication year'])
     max = df_year['Publication year'].max()
     min = df_year['Publication year'].min()
+    max.astype(int)
+    min.astype(int)
 
     with st.expander('Select publication type', expanded=False):
         types = st.multiselect('Publication type', df_csv['Publication type'].unique(),df_csv['Publication type'].unique())
