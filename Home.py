588604c02b3800682922e1f2a831807d5ccda2d8
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 17:16:36 +0000

    ;

--- Home.py
+++ Home.py
@@ -769,7 +769,3 @@ def run_query(query):
 
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
