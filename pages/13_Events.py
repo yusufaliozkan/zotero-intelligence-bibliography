28b25c411bf9018c87aff088e84b5dc39d200db9
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 21:57:55 +0000

    as

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -117,7 +117,7 @@ row_nu = len(jan.index)
 for i in range(row_nu):
     st.write(''+str(i+1)+') '+ df_gs1.iloc[i])
 
-st.expander('Events in February')
+st.write('Events in February')
 feb = df_gs[df_gs['month']=='02']
 df_gs1 = ('['+ feb['event_name'] + ']'+ '('+ feb['link'] + ')'', organised by ' + '**' + feb['organiser'] + '**' + '. Date: ' + feb['date_new'] + ', Venue: ' + feb['venue'])
 row_nu = len(feb.index)
