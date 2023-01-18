a6df4e212dbe720d466aa1b54d85bb90f0811b06
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 18:12:59 +0000

    s

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -87,7 +87,7 @@ for row in rows:
 pd.set_option('display.max_colwidth', None)
 df_gs = pd.DataFrame(data, columns=columns)
 df_gs['date_new'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
-df_gs = df_gs.sort_values(by='date', inplace=True)
+# df_gs = df_gs.sort_values(by='date', inplace=True)
 
 filter = (df_gs['date']>=today)
 df_gs = df_gs.loc[filter]
