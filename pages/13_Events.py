8500afb60325b653dc7dae450b2c5f644c4bf89a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-18 21:36:23 +0000

    sd

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -88,9 +88,9 @@ for row in rows:
 pd.set_option('display.max_colwidth', None)
 df_gs = pd.DataFrame(data, columns=columns)
 df_gs['date_new'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
+df_gs['month'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%m')
 df_gs.sort_values(by='date', ascending = True, inplace=True)
-jan = df_gs['date'].map(lambda x: x.month) == 1
-df_gs[jan]
+df_gs
 
 filter = (df_gs['date']>=today)
 filter2 = (df_gs['date']<today)
