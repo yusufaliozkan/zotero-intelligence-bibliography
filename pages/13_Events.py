5defb25ae2566b3f0c1b9f70b6173af35799ab3a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-19 08:31:15 +0000

    ds

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -115,6 +115,9 @@ df_gs = df_gs.loc[filter]
 if df_gs['event_name'].any() in ("", [], None, 0, False):
     st.write('No upcoming event!')
 
+for i in range(12):
+    print(i)
+
 if '01' in df_gs['month'].values:
     st.markdown('#### Events in January')
     jan = df_gs[df_gs['month']=='01']
