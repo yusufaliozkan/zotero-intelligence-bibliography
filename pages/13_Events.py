633fe3fdb223a57d0810f4f9b99153740af88ff7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-20 16:31:05 +0000

    s

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -118,7 +118,6 @@ with tab1:
         st.markdown('#### Events in January')
         mon = df_gs[df_gs['month']=='01']
         # mon = mon.reset_index(inplace=True)
-        mon
         df_gs1 = ('['+ mon['event_name'] + ']'+ '('+ mon['link'] + ')'', organised by ' + '**' + mon['organiser'] + '**' + '. Date: ' + mon['date_new'] + ', Venue: ' + mon['venue'])
         row_nu = len(mon.index)
         for i in range(row_nu):
