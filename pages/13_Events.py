3e4d07558705b5b4300fe8dfd017037732f2031d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-22 02:03:44 +0000

    j

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -276,7 +276,7 @@ with tab1:
 
         selector = st.checkbox('Select a year')
         if selector:
-            slider = st.slider('ssd', 2022,2023,2023)
+            slider = st.slider(2022,2023,2023)
             slider = str(slider)
             df_gs_plot =df_gs_plot[df_gs_plot['year']==slider]
 
