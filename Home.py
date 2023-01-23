048232b30c6607333f81bf61f74872d77501b8f9
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-23 23:34:26 +0000

    sd

--- Home.py
+++ Home.py
@@ -229,10 +229,14 @@ with tab1:
                             b='['+'['+df['Name_y'].iloc[i]+']' +'('+ df['Link_y'].iloc[i] + ')' +']'
                             if df['Name_y'].iloc[i]=='':
                                 b=''
+                        else:
+                            b=''
                             if 'Name' in df:
                                 c= '['+'['+df['Name'].iloc[i]+']' +'('+ df['Link'].iloc[i] + ')'+ ']'
                                 if df['Name'].iloc[i]=='':
                                     c=''
+                            else:
+                                c=''
                     else:
                         st.caption('No theme to display!')
                     st.caption('Theme(s):  \n ' + a + ' ' +b+ ' ' + c)
