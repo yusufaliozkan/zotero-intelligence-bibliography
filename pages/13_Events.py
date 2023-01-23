ccfb86862d0cba72dbc91d7507561ec29e22a734
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-23 13:39:20 +0000

    sd

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -269,7 +269,7 @@ with tab1:
                     row_nu2 = len(y2022.index)
                     df_gs3 = ('['+ y2022['event_name'] + ']'+ '('+ y2022['link'] + ')'', organised by ' + '**' + y2022['organiser'] + '**' + '. Date: ' + y2022['date_new'] + ', Venue: ' + y2022['venue'])
                     row_nu = len(df_gs.index)
-                    for i in range(y2022):
+                    for i in range(row_nu):
                         st.write(''+str(i+1)+') '+ df_gs3.iloc[i])
         
         st.header('Event visuals')
