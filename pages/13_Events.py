51850c3e78539d3038cc21d7efb1d0f35d599689
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-23 13:44:00 +0000

    df

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -268,7 +268,6 @@ with tab1:
                     y2022 = df_gs2[df_gs2['year']=='2022']
                     y2022 = y2022.reset_index()
                     row_nu2 = len(y2022.index)
-                    row_nu2
                     df_gs3 = ('['+ y2022['event_name'] + ']'+ '('+ y2022['link'] + ')'', organised by ' + '**' + y2022['organiser'] + '**' + '. Date: ' + y2022['date_new'] + ', Venue: ' + y2022['venue'])
                     row_nu = len(df_gs.index)
                     for i in range(row_nu2):
