6f005ae217118e67219f1263a6ee34f8d0b34a28
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 14:01:09 +0000

    s

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -317,7 +317,7 @@ if export_as_pdf:
     pdf.set_font('Arial', 'B', 16)
     pdf.cell(40, 10, report_text)
     
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+    html = create_download_link(pdf.output(dest="S"), "test")
 
     st.markdown(html, unsafe_allow_html=True)
 
