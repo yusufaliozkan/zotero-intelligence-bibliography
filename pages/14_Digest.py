5da09c5180573c9332a600fe82e1849ac4c5f96b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 14:35:15 +0000

    er

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -302,13 +302,14 @@ st.caption('[Go to top](#intelligence-studies-network-digest)')
 
 st.write('---')
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
 
-show_pdf('post1-compressed.pdf')
+with open("post1-compressed.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="Download PDF Tutorial", 
+        data=PDFbyte,
+        file_name="pandas-clean-id-column.pdf",
+        mime='application/octet-stream')
 
 def create_pdf(img_fn, pdf_fn):
     """
