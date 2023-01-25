e2ba837802ab1c35edd248bb0ea28493f0c187e1
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 23:21:35 +0000

    g

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -239,7 +239,6 @@ with tab1:
                     row_nu_1 = len(df.index)
                     for i in range(row_nu_1):
                         st.write(''+str(i+1)+') ' +df_items.iloc[i])
-                        df_items.fillna("nan") 
                         if display2:
                             st.caption(df['Abstract'].iloc[i])
 
