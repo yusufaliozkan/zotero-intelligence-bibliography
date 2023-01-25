207285ceb63dca013f9ba947b10e1289dc7dcd5c
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 11:32:51 +0000

    as

--- Home.py
+++ Home.py
@@ -410,6 +410,9 @@ with tab1:
             for i in range(row_nu):
                 st.write(''+str(i+1)+') '+ df_gs1.iloc[i])
             st.write('Visit the [Events on intelligence](https://intelligence.streamlit.app/Events) page to see more!')
+
+        with st.expander('Digest'):
+            st.write('See our dynamic [digest](https://intelligence.streamlit.app/Digest)')
         
 with tab2:
     st.header('Dashboard')
