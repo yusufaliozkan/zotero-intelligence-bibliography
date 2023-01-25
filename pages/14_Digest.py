ad05b6263f831c776475171512bc2b7ede8d578e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 14:39:07 +0000

    sd

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -303,6 +303,7 @@ st.caption('[Go to top](#intelligence-studies-network-digest)')
 st.write('---')
 
 df=df_last.copy()
+df=df.reset_index(inplace=True)
 st.write(df)
 
 export_as_pdf = st.button("Export Report")
