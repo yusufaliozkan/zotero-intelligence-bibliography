c116f8fb1e0b7901bd79a83196d66f1fe3914274
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-25 16:39:28 +0000

    sd

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -129,9 +129,9 @@ with st.expander('Publications:', expanded=True):
             df_csva = df_csv[df_csv['Publication type']==b]
             df_lasta = ('**'+ df_csva['Publication type']+ '**'+ ": '" + 
                     df_csva['Title'] + "'," +
-                    ' (First author: ' + '*' + df_csva['firstName'] + '*'+ ' ' + '*' + df_csva['lastName'] + '*' + '), ' +
-                    ' (Published in: ' + '*' + df_csva['Journal'] + '*' +'),' +
-                    ' (Published on: ' + df_csva['Date published new'] + '),' +
+                    ' (First author: ' + '*' + df_csva['firstName'] + '*'+ ' ' + '*' + df_csva['lastName'] + '*' + ') ' +
+                    ' (Published in: ' + '*' + df_csva['Journal'] + '*' +')' +
+                    ' (Published on: ' + df_csva['Date published new'] + ')' +
                     ", [Publication link]"+ '('+ df_csva['Link to publication'] + ')'
                     )
             row_nu = len(df_csva.index)
