465d02f14793522bf7fc9dc1a6633ab6f7dcb10f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-27 17:47:01 +0000

    fg

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -603,7 +603,6 @@ with tab3:
         row_nu_1 = len(df3.index)
         df = df.reset_index()
         df = df.drop(['index'], axis=1)
-        df
         if row_nu_1 >5:
             df=df.sample(n=5)
             df = df.reset_index()
@@ -649,9 +648,6 @@ with tab3:
                             "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
                             ' (Published on: ' +df['Date published'] + ')'
                             )
-        df = df.reset_index()
-        df = df.drop(['index'], axis=1)
-        df
         for i in range(row_nu_1):
             st.write(''+str(i+1)+') ' +df_items.iloc[i])
             df_items.fillna("nan") 
