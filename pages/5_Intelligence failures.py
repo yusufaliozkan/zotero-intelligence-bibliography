a644a4c254eb2d51435dea6e81e94439bed2bb7e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-27 20:53:06 +0000

    we

--- pages/5_Intelligence failures.py
+++ pages/5_Intelligence failures.py
@@ -218,6 +218,7 @@ with tab1:
             display2 = st.checkbox('Display abstracts')
 
             if sort_by_type:
+                df3=df.copy()
                 df = df.drop(columns=['index'])
                 df = df.sort_values(by=['Publication type'], ascending=True)
                 types = df['Publication type'].unique()
@@ -586,6 +587,16 @@ with tab2:
         st.pyplot() 
 
 with tab3:
+    df=df2.copy()
+    row_nu_1 = len(df.index)
+    df = df.reset_index()
+    df = df.drop(['index'], axis=1)
+    if row_nu_1 >5:
+        df=df.sample(n=5)
+        row_nu_1= len(df.index)
+    df = df.reset_index()
+    st.info('It may take some time for the app to bring new results as it searches the entire database.')
+    if st.button('Suggest me 5 random sources'):
         df=df2.copy()
         row_nu_1 = len(df.index)
         df = df.reset_index()
@@ -594,50 +605,41 @@ with tab3:
             df=df.sample(n=5)
             row_nu_1= len(df.index)
         df = df.reset_index()
-        if st.button('Suggest me 5 random sources'):
-            df=df2.copy()
-            row_nu_1 = len(df.index)
-            df = df.reset_index()
-            df = df.drop(['index'], axis=1)
-            if row_nu_1 >5:
-                df=df.sample(n=5)
-                row_nu_1= len(df.index)
-            df = df.reset_index()
-            if df['FirstName2'].any() in ("", [], None, 0, False):
-                # st.write('no author')
-                df['firstName'] = 'null'
-                df['lastName'] = 'null'
+    if df['FirstName2'].any() in ("", [], None, 0, False):
+        # st.write('no author')
+        df['firstName'] = 'null'
+        df['lastName'] = 'null'
 
-                df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
-                    df['Title'] + ' '+ 
-                    ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + 
-                    "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
-                    "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
-                    ' (Published on: ' +df['Date published'] + ')'
-                    )
-            else:
-                # st.write('author entered')
-                ## This section is for displaying the first author details but it doesn't work for now because of json normalization error.
-                df_fa = df['FirstName2']
-                df_fa = pd.DataFrame(df_fa.tolist())
-                df_fa = df_fa[0]
-                df_fa = df_fa.apply(lambda x: {} if pd.isna(x) else x) # https://stackoverflow.com/questions/44050853/pandas-json-normalize-and-null-values-in-json
-                df_new = pd.json_normalize(df_fa, errors='ignore') 
-                df = pd.concat([df, df_new], axis=1)
-                df['firstName'] = df['firstName'].fillna('null')
-                df['lastName'] = df['lastName'].fillna('null')    
-                df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
-                            df['Title'] + ' '+ 
-                            ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + # IT CANNOT READ THE NAN VALUES
-                            "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
-                            "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
-                            ' (Published on: ' +df['Date published'] + ')'
-                            )
-        for i in range(row_nu_1):
-            st.write(''+str(i+1)+') ' +df_items.iloc[i])
-            df_items.fillna("nan") 
-            if display2:
-                st.caption(df['Abstract'].iloc[i])
+        df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
+            df['Title'] + ' '+ 
+            ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + 
+            "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
+            "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
+            ' (Published on: ' +df['Date published'] + ')'
+            )
+    else:
+            # st.write('author entered')
+            ## This section is for displaying the first author details but it doesn't work for now because of json normalization error.
+            df_fa = df['FirstName2']
+            df_fa = pd.DataFrame(df_fa.tolist())
+            df_fa = df_fa[0]
+            df_fa = df_fa.apply(lambda x: {} if pd.isna(x) else x) # https://stackoverflow.com/questions/44050853/pandas-json-normalize-and-null-values-in-json
+            df_new = pd.json_normalize(df_fa, errors='ignore') 
+            df = pd.concat([df, df_new], axis=1)
+            df['firstName'] = df['firstName'].fillna('null')
+            df['lastName'] = df['lastName'].fillna('null')    
+            df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
+                        df['Title'] + ' '+ 
+                        ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' + # IT CANNOT READ THE NAN VALUES
+                        "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+
+                        "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
+                        ' (Published on: ' +df['Date published'] + ')'
+                        )
+    for i in range(row_nu_1):
+        st.write(''+str(i+1)+') ' +df_items.iloc[i])
+        df_items.fillna("nan") 
+        if display2:
+            st.caption(df['Abstract'].iloc[i])
 
 components.html(
 """
