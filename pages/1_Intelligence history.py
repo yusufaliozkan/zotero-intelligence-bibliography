b19ab9d1cddee18e316deef5f57d16de97f43f3e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-27 17:39:40 +0000

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -602,6 +602,7 @@ with tab3:
         row_nu_1 = len(df3.index)
         df = df.reset_index()
         df = df.drop(['index'], axis=1)
+        df
         if row_nu_1 >5:
             df=df.sample(n=5)
             row_nu_1= len(df.index)
