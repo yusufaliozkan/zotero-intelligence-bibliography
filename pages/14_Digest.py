ce9f25a3dfd66fc18116d6a5f86708969dbf254a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-28 13:44:22 +0000

    er

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -147,6 +147,7 @@ with st.expander('Publications:', expanded=ex):
             st.subheader(types2['Publication type'].iloc[i])
             b = types2['Publication type'].iloc[i]
             df_csva = df_csv[df_csv['Publication type']==b]
+            df_csva
             df_lasta = ('**'+ df_csva['Publication type']+ '**'+ ": '" + 
                     df_csva['Title'] + "'," +
                     ' (First author: ' + '*' + df_csva['firstName'] + '*'+ ' ' + '*' + df_csva['lastName'] + '*' + ') ' +
