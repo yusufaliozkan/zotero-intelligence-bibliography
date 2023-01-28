c2b1cec9ce2044500285d46e8df796dc022db4ce
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-28 14:01:43 +0000

    fg

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -154,8 +154,9 @@ with st.expander('Publications:', expanded=ex):
                     ' (Published on: ' + df_csva['Date published new'] + ')' +
                     ", [Publication link]"+ '('+ df_csva['Link to publication'] + ')'
                     )
-            row_nu = len(df_csva.index)
             df_lasta=df_lasta.dropna().reset_index(drop=True)
+            df_lasta
+            row_nu = len(df_csva.index)
             for i in range(row_nu):
                 df_lasta=df_lasta.dropna().reset_index(drop=True)                
                 st.write(''+str(i+1)+') ' +df_lasta.iloc[i])
