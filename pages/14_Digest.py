5959b2a02eec6d440d9860fd102c38fa0045251e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 23:01:05 +0000

    sd

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -122,9 +122,9 @@ with st.expander('Publications:', expanded=ex):
         rg = previous_360
         a='1 year'
     if range_day == 'Custom':
-        number = st.number_input('How many days do you want to go back:', min_value=1, max_value=11000, value=365, step=1)
-
+        number = st.number_input('How many days do you want to go back:', min_value=10, max_value=11000, value=365, step=1)
         a = str(int(number)) + ' days'
+        previous_custom = today - dt.timedelta(days=number)
 
     filter = (df_csv['Date published']>rg) & (df_csv['Date published']<=today)
     # rg2 = rg.dt.date.today().strftime('%d/%m/%Y')
