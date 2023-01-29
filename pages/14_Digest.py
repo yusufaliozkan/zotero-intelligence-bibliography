a2cebff4f3da86fe47eb2b75928781a288ef0d49
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 20:01:27 +0000

    ds

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -183,7 +183,7 @@ with st.expander('Publications:', expanded=ex):
         width=400,
         height=400,)
     fig.update_layout(title={'text':'Publications types in the last '+a, 'y':0.95, 'x':0.3, 'yanchor':'top'})
-    col2.plotly_chart(fig, use_container_width = True)
+    st.plotly_chart(fig, use_container_width = True)
     st.caption('[Go to top](#intelligence-studies-network-digest)')
 
 with st.expander('Events:', expanded=ex):
