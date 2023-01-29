53064bef1758b1fc6e79eaae646d79b0e245bf01
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 19:59:48 +0000

    ds

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -177,7 +177,9 @@ with st.expander('Publications:', expanded=ex):
     df_plot= df_csv['Publication type'].value_counts()
     df_plot=df_plot.reset_index()
     df_plot=df_plot.rename(columns={'index':'Publication type','Publication type':'Count'})
-    df_plot
+    fig = px.pie(df_plot, values='Count', names='Publication type')
+    fig.update_layout(title={'text':'Publications: '+collection_name, 'y':0.95, 'x':0.45, 'yanchor':'top'})
+    st.plotly_chart(fig, use_container_width = True)
     st.caption('[Go to top](#intelligence-studies-network-digest)')
 
 with st.expander('Events:', expanded=ex):
