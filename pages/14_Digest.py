ef052fcbe896d5721c9158a85c3facd548c97a83
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 20:42:29 +0000

    sd

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -206,7 +206,7 @@ with st.expander('Publications:', expanded=ex):
         st.plotly_chart(fig, use_container_width = True)
 
         fig2 = px.line(df_dates, x='Publication date', y='sum')
-        fig2.update_layout(title={'text':'Publications by date in the last: '+a, 'y':0.95, 'x':0.5, 'yanchor':'top'})
+        fig2.update_layout(title={'text':'Publications by date in the last '+a+ ' (cumulative sum)', 'y':0.95, 'x':0.5, 'yanchor':'top'})
         fig2.update_xaxes(tickangle=-70)
         st.plotly_chart(fig2, use_container_width = True)        
     st.caption('[Go to top](#intelligence-studies-network-digest)')
