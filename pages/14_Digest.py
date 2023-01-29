1fcc4c73b95cc85e0bb3381716eea5151bb3670a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 21:43:55 +0000

    sd

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -138,7 +138,7 @@ with st.expander('Publications:', expanded=ex):
     df_csv["Link to publication"].fillna("No link", inplace = True)
     st.subheader('Sources published in the last ' + str(a))
     num_items = len(df_csv)
-    num_items
+    st.write(str(num_items)+' sources were published in the last ' + str(a))
 
     if df_csv['Title'].any() in ("", [], None, 0, False):
         st.write('There is no publication published in the last '+ str(a))
