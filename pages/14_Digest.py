67d0df1ec4aa319dd856e4afdbdc7c1605ac4114
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-29 21:45:08 +0000

    we

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -123,6 +123,7 @@ with st.expander('Publications:', expanded=ex):
         a='1 year'
 
     filter = (df_csv['Date published']>rg) & (df_csv['Date published']<=today)
+    filter
     df_csv = df_csv.loc[filter]
 
     df_csv['Date published'] = pd.to_datetime(df_csv['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
