8796b8cad01e2c2528f7e58b1493ea99bbfc72be
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-01-30 20:13:46 +0000

    as

--- pages/14_Digest.py
+++ pages/14_Digest.py
@@ -320,6 +320,7 @@ with st.expander('Events:', expanded=ex):
     df_gs['date_new'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
     df_gs.sort_values(by='date', ascending = True, inplace=True)
     df_gs = df_gs.drop_duplicates(subset=['event_name', 'link'], keep='first')
+    df_gs['details'] = df_gs['details'].fillna('No details provided.')
 
     next_10 = today + dt.timedelta(days=10)    
     next_20 = today + dt.timedelta(days=20)
