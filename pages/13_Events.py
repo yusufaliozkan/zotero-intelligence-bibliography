dbda99bd14fc22a52a07707499950d1635d001ed
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-06 17:48:22 +0000

    sd

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -287,6 +287,7 @@ with tab1:
                 row_nu2 = len(y2023.index)
                 df_gs3 = ('['+ y2023['event_name'] + ']'+ '('+ y2023['link'] + ')'', organised by ' + '**' + y2023['organiser'] + '**' + '. Date: ' + y2023['date_new'] + ', Venue: ' + y2023['venue'])
                 row_nu = len(df_gs.index)
+                st.write(str(row_nu2) + ' events')
                 for i in range(row_nu2):
                     st.write(''+str(i+1)+') '+ df_gs3.iloc[i])
         if st.checkbox('Events in 2022', key='2022'):
