eef07bede6dd59960afc679ba93cbcb1645852f3
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-06 22:41:10 +0000

    gf

--- pages/12_Special collections.py
+++ pages/12_Special collections.py
@@ -360,9 +360,9 @@ with tab2:
     # df['Date published'] = df['Date published'].fillna('No date')
 
     df['Date published'] = pd.to_datetime(df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
-    df
     df['Date year'] = df['Date published'].dt.strftime('%Y')
     df['Date year'] = df['Date year'].fillna('No date')
+    df
     df_year=df['Date year'].value_counts()
     df_year=df_year.reset_index()
 
