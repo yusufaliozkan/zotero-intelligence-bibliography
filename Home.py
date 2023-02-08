9463bee30336785f32a48a36b83469531b73c78a
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-08 23:06:22 +0000

    df

--- Home.py
+++ Home.py
@@ -220,8 +220,7 @@ with tab1:
             # )
             row_nu_1= len(df_last.index)
             for i in range(row_nu_1):
-                st.write(''+str(i+1)+') ' +df_last.iloc[i])
-                df                
+                st.write(''+str(i+1)+') ' +df_last.iloc[i])              
                 if display:
                     a=''
                     b=''
