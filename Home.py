3cf01dfb96317c15ae3d41448e053a02216f6774
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-09 10:43:43 +0000

    sd

--- Home.py
+++ Home.py
@@ -207,11 +207,12 @@ with tab1:
 
             pub_info = ''
             if df['Publication type'].iloc[0] in ('Journal article'):
-                pub_info = ' (Published in: ' + '*' + df['Journal'] + '*' +')'
+                pub_info = ', (Published in: ' + '*' + df['Journal'] + '*' +'), '
 
             df_last = ('**'+ df['Publication type']+ '**'+ ': ' + 
             '['+ df['Title'] + ']'+ '('+ df['Link to publication'] + ')' +
             ' (by ' + '*' + df['firstName'] + '*'+ ' ' + '*' + df['lastName'] + '*' + ') ' +
+            pub_info+
             # "[[Publication link]]" +'('+ df['Link to publication'] + ')' +'  '+ 
             "[[Zotero link]]" +'('+ df['Zotero link'] + ')' +
             ' (Published on: ' + df['Date published']+', ' +
