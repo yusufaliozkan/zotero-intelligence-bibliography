7bbee807a17162ed690371f9070d2258c2612b83
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-10 16:27:36 +0000

    lo

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -237,13 +237,20 @@ with tab1:
                     row_nu_1 = len(df_a.index)
                     for i in range(row_nu_1):
                         if df_a['Publication type'].iloc[i] in ['Journal article', 'Magazine article', 'Newspaper article']:
-                            st.write('' + str(i+1) + ') ' + df_items.iloc[i] + ' (Published in: ' + '*' + df_a['Journal'].iloc[i] + '*' + ')')
+                            df_items = ('**'+ df_a['Publication type']+ '**'+ ': ' +
+                                df_a['Title'] + ' '+ 
+                                ' (by ' + '*' + df_a['firstName'] + '*'+ ' ' + '*' + df_a['lastName'] + '*' + ') ' +
+                                ' (Published on: ' +df_a['Date published'] + ') '+
+                                ' (Published in: ' + '*' + df_a['Journal'].iloc[i] + '*' +
+                                "[[Publication link]]" +'('+ df_a['Link to publication'] + ')' +'  '+
+                                "[[Zotero link]]" +'('+ df_a['Zotero link'] + ')'
+                                ) 
+                            st.write('' + str(i+1) + ') ' + df_items.iloc[i])
                         else:
                             st.write('' + str(i+1) + ') ' + df_items.iloc[i])
                         df_items.fillna("nan") 
                         if display2:
                             st.caption(df['Abstract'].iloc[i])
-
             else:           
                 for i in range(row_nu_1):
                     if df['Publication type'].iloc[i] in ['Journal article', 'Magazine article', 'Newspaper article']:
