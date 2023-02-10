3c7bbe66f5cad25757065469a925fe6b3a690ec7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-10 16:30:08 +0000

    df

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -254,7 +254,14 @@ with tab1:
             else:           
                 for i in range(row_nu_1):
                     if df['Publication type'].iloc[i] in ['Journal article', 'Magazine article', 'Newspaper article']:
-                        st.write('' + str(i+1) + ') ' + df_items.iloc[i] + ' (Published in: ' + '*' + df['Journal'].iloc[i] + '*' + ')')
+                        df_a['Title'] + ' '+ 
+                        ' (by ' + '*' + df_a['firstName'] + '*'+ ' ' + '*' + df_a['lastName'] + '*' + ') ' +
+                        ' (Published on: ' +df_a['Date published'] + ') '+
+                        ' (Published in: ' + '*' + df_a['Journal'].iloc[i] + '*' + ') ' +
+                        "[[Publication link]]" +'('+ df_a['Link to publication'] + ')' +'  '+
+                        "[[Zotero link]]" +'('+ df_a['Zotero link'] + ')'
+                        )
+                        st.write('' + str(i+1) + ') ' + df_items.iloc[i] )
                     else:
                         st.write('' + str(i+1) + ') ' + df_items.iloc[i])
                     df_items.fillna("nan") 
