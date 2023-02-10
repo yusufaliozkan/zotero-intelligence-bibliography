33f1a9d6cf9961d3255ff38b18097602b3e36d97
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-10 20:52:40 +0000

    fg

--- Home.py
+++ Home.py
@@ -54,14 +54,6 @@ st.set_page_config(layout = "wide",
 pd.set_option('display.max_colwidth', None)
 df = pd.DataFrame(data, columns=columns)
 
-authors_data = []
-for item in items:
-    for creator in item['data']['creators']:
-        authors_data.append((item['data']['title'], creator['firstName'], creator['lastName']))
-authors_df = pd.DataFrame(authors_data, columns=['Title', 'firstName', 'lastName'])
-
-authors_df
-items
 
 split_df= pd.DataFrame(df['Col key'].tolist())
 df_fa = df['FirstName']
