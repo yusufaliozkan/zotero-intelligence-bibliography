fd5ca1547e9645eb90791b8ea2337a7ab37d9c50
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-10 21:10:32 +0000

    sd

--- Home.py
+++ Home.py
@@ -92,6 +92,8 @@ df['Date added'] = df['Date added'].dt.strftime('%d/%m/%Y')
 df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
 df['Date modified'] = df['Date modified'].dt.strftime('%d/%m/%Y, %H:%M')
 
+df
+
 # Bringing collections
 bbb = zot.collections()
 data3=[]
