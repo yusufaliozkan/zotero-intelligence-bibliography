322f79d0eb07894f9e68ca01ce8850392fe28e9f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-15 17:49:03 +0000

    sd

--- Home.py
+++ Home.py
@@ -208,7 +208,7 @@ with tab1:
         # # st.caption(collection_name)
         today = datetime.date.today().isoformat()
         a = 'intelligence-bibliography-' + today
-        st.download_button('Download recently added items', csv, (a+'.csv'), mime="text/csv", key='download-csv')
+        st.download_button('💾 Download recently added items', csv, (a+'.csv'), mime="text/csv", key='download-csv')
 
         with st.expander('Click to hide the list', expanded=True):
             display = st.checkbox('Display theme and abstract')
