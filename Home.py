243516fd78a7084b889693d30c51f4cc230ee7ac
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-15 17:07:44 +0000

    df

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ from pyzotero import zotero
 import pandas as pd
 import streamlit as st
 from IPython.display import HTML
-# import streamlit.components.v1 as components
+import streamlit.components.v1 as components
 import numpy as np
 import altair as alt
 from pandas.io.json import json_normalize
