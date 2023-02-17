7502214f9ea9ad283ca3fd6c751d176cc9819d55
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-17 11:41:03 +0000

    df

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -148,11 +148,11 @@ with tab1:
             df_o = df_gs[df_gs['organiser']==c]
             df_last = ('['+ df_o['event_name'] + ']'+ '('+ df_o['link'] + ')'', organised by ' + '**' + df_o['organiser'] + '**' + '. Date: ' + df_o['date_new'] + ', Venue: ' + df_o['venue'])
             row_nu =len(df_o.index)
-            for i in range(row_nu):
-                st.write(''+str(i+1)+') ' +df_last.iloc[i])
+            for j in range(row_nu):
+                st.write(''+str(i+1)+') ' +df_last.iloc[j])
                 df_last.fillna('')
                 if display:
-                    st.caption('Details:'+'\n '+ df_gs['details'].iloc[i])
+                    st.caption('Details:'+'\n '+ df_gs['details'].iloc[j])
 
     else:
 
