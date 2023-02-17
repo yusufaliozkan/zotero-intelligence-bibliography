08f495de89f7dec2d9303ad32c7b5055b4eccab5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-17 08:16:40 +0000

    ds

--- Home.py
+++ Home.py
@@ -812,6 +812,8 @@ src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
 """
 )
 
+
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -822,7 +824,7 @@ def remote_css(url):
 def icon(icon_name):
     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("style2.css")
 remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
 icon("search")
