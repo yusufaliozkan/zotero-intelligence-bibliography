fea0c0e76ea8b991cac91cd90c48f9ae447a95ee
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-23 23:58:42 +0000

    sd

--- Home.py
+++ Home.py
@@ -163,7 +163,7 @@ Check it out the short guide for a quick intoduction.'''
 
 count = zot.count_items()
 
-col1, col2 = st.columns([1,7])
+col1, col2 = st.columns([1,9])
 with col1:
     hide = st.checkbox('Hide into')
 with col2:
