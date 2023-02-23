ee6d2e8a04a752072a3929dbfbce6e972ffffd7c
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-23 23:56:41 +0000

    ds

--- Home.py
+++ Home.py
@@ -154,17 +154,19 @@ df = df.fillna('')
 st.title("Intelligence bibliography")
 # st.header("[Zotero group library](https://www.zotero.org/groups/2514686/intelligence_bibliography/library)")
 
+into = '''
+Wellcome to **Intelligence bibliography**! 
+This website lists different sources, events, conferences, and call for papers on intelligence history and intelligence studies. 
+The current page shows the recently added or updated items. 
+**If you wish to see more sources under different themes, see the sidebar menu** :arrow_left: . 
+Check it out the short guide for a quick intoduction.'''
+
 count = zot.count_items()
 hide = st.checkbox('Hide into')
 if hide:
     st.write('')
 else:
-    st.write('''
-Wellcome to **Intelligence bibliography**! 
-This website lists different sources, events, conferences, and call for papers on intelligence history and intelligence studies. 
-The current page shows the recently added or updated items. 
-**If you wish to see more sources under different themes, see the sidebar menu** :arrow_left: . 
-Check it out the short guide for a quick intoduction.''')
+    st.write(into)
 
 st.write('There are '+  '**'+str(count)+ '**' + ' items in the [Intelligence bibliography Zotero group library](https://www.zotero.org/groups/2514686/intelligence_bibliography/items).')
 st.write('The library last updated on ' + '**'+ df.loc[0]['Date modified']+'**')
