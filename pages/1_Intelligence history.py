3fd3976c6155b2e6a27ca8037ade6a2ce261315d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-24 22:03:02 +0000

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -106,28 +106,30 @@ with st.spinner('Retrieving data & updating dashboard...'):
             pd.set_option('display.max_colwidth', None)
 
         # Collection items
-        
-            count_collection = zot.num_collectionitems(collection_code)
+            def get_zotero_data(collection_code):
+                count_collection = zot.num_collectionitems(collection_code)
 
-            items = zot.everything(zot.collection_items_top(collection_code))
+                items = zot.everything(zot.collection_items_top(collection_code))
 
-            data3=[]
-            columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date published', 'FirstName2', 'Publisher', 'Journal']
+                data3=[]
+                columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date published', 'FirstName2', 'Publisher', 'Journal']
 
-            for item in items:
-                data3.append((
-                    item['data']['title'], 
-                    item['data']['itemType'], 
-                    item['data']['url'], 
-                    item['data']['abstractNote'], 
-                    item['links']['alternate']['href'],
-                    item['data'].get('date'),
-                    item['data']['creators'],
-                    item['data'].get('publisher'),
-                    item['data'].get('publicationTitle')
-                    )) 
-            pd.set_option('display.max_colwidth', None)
-            df = pd.DataFrame(data3, columns=columns3)
+                for item in items:
+                    data3.append((
+                        item['data']['title'], 
+                        item['data']['itemType'], 
+                        item['data']['url'], 
+                        item['data']['abstractNote'], 
+                        item['links']['alternate']['href'],
+                        item['data'].get('date'),
+                        item['data']['creators'],
+                        item['data'].get('publisher'),
+                        item['data'].get('publicationTitle')
+                        )) 
+                pd.set_option('display.max_colwidth', None)
+                df = pd.DataFrame(data3, columns=columns3)
+                return df
+            df = get_zotero_data(collection_code)
 
             # df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
             # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
