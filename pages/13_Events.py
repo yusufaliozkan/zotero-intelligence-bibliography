45cfa6fa2c29cfd462ee2b800a47120d217a2f46
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-24 20:59:10 +0000

    fd

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -25,7 +25,8 @@ st.set_page_config(layout = "centered",
                     page_icon="https://images.pexels.com/photos/315918/pexels-photo-315918.png",
                     initial_sidebar_state="auto") 
 
-st.title("Intelligence studies network - Events")
+st.title("Intelligence studies network")
+st.header("Events on intelligence")
 image = 'https://images.pexels.com/photos/315918/pexels-photo-315918.png'
 
 
