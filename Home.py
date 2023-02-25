215dc32d9bdbbbb2eed2429d8bb9a460cd4bea12
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-25 14:46:06 +0000

    fd

--- Home.py
+++ Home.py
@@ -841,3 +841,23 @@ with st.spinner('Retrieving data & updating dashboard...'):
     © 2022 All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
     """
     )
+
+    components.html(
+        """
+        <div id="disqus_thread"></div>
+<script>
+
+    var disqus_config = function () {
+    this.page.url = 'https://sites.google.com/view/intelstudiesnetwork/home';  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = '/intelstudiesnetwork/home'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://intelligence-studies-network.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>"""
+    )
