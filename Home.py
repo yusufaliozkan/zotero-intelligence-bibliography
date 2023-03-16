5f9ffd9bd3d82db596d00a2e886adc3a51920c4b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-03-16 14:48:26 +0000

    sd

--- Home.py
+++ Home.py
@@ -748,8 +748,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
             col1.plotly_chart(fig) 
         with col2:
             st.markdown('##### Top 10 country names mentioned in titles')
-            # for i, row in enumerate(df_countries.head(10).values):
-            #     st.write(f"{i+1}. {row[0]}, {row[1]}, {row[2]}")
+            for i, row in enumerate(df_countries.values):
+                st.write(f"{i+1}. {row[0]}, {row[1]}, {row[2]}")
             st.dataframe(df_countries.head(10))
 
         st.write('---')
