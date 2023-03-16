8daf9ab5efd4380750c8ab1cdeb6ba39722c704f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-03-16 20:38:00 +0000

    ,l

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -128,7 +128,6 @@ with tab1:
     df_gs = pd.concat([df_gs, df_forms], axis=0)
     df_gs = df_gs.reset_index(drop=True)
     df_gs = df_gs.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
-    df_gs
     
     col1, col2 = st.columns(2)
 
@@ -153,7 +152,6 @@ with tab1:
         st.write('No upcoming event!')
 
     if sort_by == 'Most recently added':
-        df_gs
         df_gs = df_gs.sort_index(ascending=False)
         df_last = ('['+ df_gs['event_name'] + ']'+ '('+ df_gs['link'] + ')'', organised by ' + '**' + df_gs['organiser'] + '**' + '. Date: ' + df_gs['date_new'] + ', Venue: ' + df_gs['venue'])
         row_nu = len(df_gs.index)
