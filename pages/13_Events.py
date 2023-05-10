969b35c6ed3e8e153983e5bd894d628405f6168d
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-05-10 10:42:23 +0100

    sd

--- pages/13_Events.py
+++ pages/13_Events.py
@@ -153,6 +153,7 @@ with tab1:
 
     if sort_by == 'Most recently added':
         df_gs = df_gs.sort_index(ascending=False)
+        df_gs
         df_last = ('['+ df_gs['event_name'] + ']'+ '('+ df_gs['link'] + ')'', organised by ' + '**' + df_gs['organiser'] + '**' + '. Date: ' + df_gs['date_new'] + ', Venue: ' + df_gs['venue'])
         row_nu = len(df_gs.index)
         for i in range(row_nu):
