38c16c3711bfe52341e5f0b7dba0a3c742c9a634
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-11-08 10:03:36 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -109,7 +109,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             def get_zotero_data(collection_code):
                 items = zot.everything(zot.collection_items_top(collection_code))
                 data3=[]
-                columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date published', 'FirstName2', 'Publisher', 'Journal']
+                columns3=['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date published', 'FirstName2', 'Full author list', 'Publisher', 'Journal']
 
                 for item in items:
                     creators = item['data']['creators']
@@ -121,6 +121,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         item['data']['abstractNote'], 
                         item['links']['alternate']['href'],
                         item['data'].get('date'),
+                        item['data']['creators'],
                         creators_str,
                         item['data'].get('publisher'),
                         item['data'].get('publicationTitle')
