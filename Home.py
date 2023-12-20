08ff09d31c9ab5646ed3f06707126d4f3099e8f6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-20 23:39:00 +0000

    sd

--- Home.py
+++ Home.py
@@ -781,7 +781,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
         if not filtered_df.empty:
             st.write("Matching articles:")
-            st.write(filtered_df[['Title']])
+            st.write(filtered_df)
         else:
             st.write("No articles found with the given keyword/phrase.")
     else:
