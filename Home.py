992fe8f666e39c1e4289cc33cb927dec83c95afb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-20 23:29:59 +0000

    sd

--- Home.py
+++ Home.py
@@ -757,20 +757,34 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
         st.bar_chart(plot2['Publication type'].sort_values(), height=600, width=600, use_container_width=True)
 
-    options = df_csv['Title'].unique()
-    st.markdown(
-    """
-    <style>
-    .st-eb .st-ec .st-eu {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-    )
+    # options = df_csv['Title'].unique()
+    # st.markdown(
+    # """
+    # <style>
+    # .st-eb .st-ec .st-eu {
+    #     display: none;
+    # }
+    # </style>
+    # """,
+    # unsafe_allow_html=True,
+    # )
 
-    # Display the multiselect
-    selected_options = st.multiselect('Publication type', options, key='modified')
+    # # Display the multiselect
+    # selected_options = st.multiselect('Publication type', options, key='modified')
+
+    search_term = st.text_input('Enter keyword or phrase to search')
+
+    # Display outputs based on the search term
+    if search_term:
+        filtered_df = df_csv[df_csv['Title'].str.contains(search_term, case=False, na=False)]
+
+        if not filtered_df.empty:
+            st.write("Matching articles:")
+            st.write(filtered_df)
+        else:
+            st.write("No articles found with the given keyword/phrase.")
+    else:
+        st.write("Please enter a keyword or phrase to search.")
 
     st.write('---')
     with st.expander('Acknowledgements'):
