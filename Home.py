848e066059b44627113517e58a52f1d90d001eac
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-20 23:42:02 +0000

    sd

--- Home.py
+++ Home.py
@@ -757,31 +757,48 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
         st.bar_chart(plot2['Publication type'].sort_values(), height=600, width=600, use_container_width=True)
 
-    # options = df_csv['Title'].unique()
-    # st.markdown(
-    # """
-    # <style>
-    # .st-eb .st-ec .st-eu {
-    #     display: none;
-    # }
-    # </style>
-    # """,
-    # unsafe_allow_html=True,
-    # )
 
-    # # Display the multiselect
-    # selected_options = st.multiselect('Publication type', options, key='modified')
+    # search_term = st.text_input('Enter keyword or phrase to search')
 
+    # # Display outputs based on the search term
+    # if search_term:
+    #     filtered_df = df_csv[df_csv['Title'].str.contains(search_term, case=False, na=False)]
+    #     filtered_df = filtered_df.reset_index(drop=True)
+
+    #     if not filtered_df.empty:
+    #         st.write("Matching articles:")
+    #         st.write(filtered_df)
+    #     else:
+    #         st.write("No articles found with the given keyword/phrase.")
+    # else:
+    #     st.write("Please enter a keyword or phrase to search.")
+
+    df_csv = pd.DataFrame(data)
+
+    # Function to format the entries
+    def format_entry(row):
+        return (
+            '**' + row['Publication type'] + '**' + ': ' +
+            row['Title'] + ' ' +
+            '(by ' + '*' + row['firstName'] + '* ' + '*' + row['lastName'] + '*' + ') ' +
+            '(Published on: ' + row['Date published'] + ') ' +
+            '(Published in: ' + '*' + row['Journal'] + '*' + ') ' +
+            '[[Publication link]](' + row['Link to publication'] + ') ' +
+            '[[Zotero link]](' + row['Zotero link'] + ')'
+        )
+
+    # Title input from the user
     search_term = st.text_input('Enter keyword or phrase to search')
 
-    # Display outputs based on the search term
+    # Display formatted entries based on the search term
     if search_term:
         filtered_df = df_csv[df_csv['Title'].str.contains(search_term, case=False, na=False)]
-        filtered_df = filtered_df.reset_index(drop=True)
 
         if not filtered_df.empty:
             st.write("Matching articles:")
-            st.write(filtered_df)
+            for index, row in filtered_df.iterrows():
+                formatted_entry = format_entry(row)
+                st.markdown(formatted_entry)
         else:
             st.write("No articles found with the given keyword/phrase.")
     else:
