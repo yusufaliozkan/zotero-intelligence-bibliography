9c50373d79ae0643037e4ae7d859c622d19191df
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-21 15:45:05 +0000

    sd

--- Home.py
+++ Home.py
@@ -305,6 +305,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             unique_collections = [''] + list(df_csv_collections['Collection_Name'].unique())  # Adding an empty string as the first option
 
             selected_collection = st.selectbox('Select Collection(s)', unique_collections)
+            selected_collection
 
             if not selected_collection or selected_collection == '':
                 st.write('Pick a collection to see items')
