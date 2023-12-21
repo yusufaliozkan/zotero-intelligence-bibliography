99ce5c7e736efa03fcec155039070f82aa89e885
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-21 16:11:50 +0000

    sd

--- Home.py
+++ Home.py
@@ -302,6 +302,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             st.header('Search collections')
 
             df_csv_collections = pd.read_csv('all_items_duplicated.csv')
+            numeric_start_collections = df_csv_collections[df_csv_collections['Collection_Name'].str[0].str.isdigit()]['Collection_Name'].unique()
             unique_collections = [''] + list(df_csv_collections['Collection_Name'].unique())  # Adding an empty string as the first option
 
             selected_collection = st.selectbox('Select Collection(s)', unique_collections)
