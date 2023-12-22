d7b3c62d082266a4b8a31760d62f075594ec4990
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-22 22:23:39 +0000

    ew

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -352,7 +352,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_authors['FirstName2'].fillna('', inplace=True)
             df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
             df_authors = df_authors.explode('Author_name')
-            df_authors.reset_index(drop=True)
+            df_authors.reset_index(drop=True, inplace=True)
             df_authors
 
 
