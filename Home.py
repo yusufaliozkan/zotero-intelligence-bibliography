77b15d31c86a9ccbab03c8f9d66f7de0b270c981
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-22 15:34:39 +0000

    sd

--- Home.py
+++ Home.py
@@ -354,33 +354,29 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
                     
                     for index, row in filtered_collection_df.iterrows():
-                        publication_type = row['Publication type']
-                        title = row['Title']
-                        authors = row['FirstName2']
-                        date_published = row['Date published']
-                        link_to_publication = row['Link to publication']
-                        zotero_link = row['Zotero link']
+                        try:
+                            publication_type = row['Publication type']
+                            title = row['Title']
+                            authors = row['FirstName2']
+                            date_published = row['Date published']
+                            link_to_publication = row['Link to publication']
+                            zotero_link = row['Zotero link']
 
-                        if publication_type == 'Journal article':
-                            published_by_or_in = 'Published in'
-                            published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-                        elif publication_type == 'Book':
-                            published_by_or_in = 'Published by'
-                            published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
-                        else:
-                            published_by_or_in = ''
-                            published_source = ''
+                            # ... (existing logic)
 
-                        formatted_entry = (
-                            '**' + publication_type + '**' + ': ' +
-                            title + ' ' +
-                            '(by ' + '*' + authors + '*' + ') ' +
-                            '(Publication date: ' + str(date_published) + ') ' +
-                            ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
-                            '[[Publication link]](' + link_to_publication + ') ' +
-                            '[[Zotero link]](' + zotero_link + ')'
-                        )
-                        st.write(f"{index + 1}) {formatted_entry}")
+                            formatted_entry = (
+                                '**' + str(publication_type) + '**' + ': ' +
+                                str(title) + ' ' +
+                                '(by ' + '*' + str(authors) + '*' + ') ' +
+                                '(Publication date: ' + str(date_published) + ') ' +
+                                ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
+                                '[[Publication link]](' + str(link_to_publication) + ') ' +
+                                '[[Zotero link]](' + str(zotero_link) + ')'
+                            )
+                            st.write(f"{index + 1}) {formatted_entry}")
+                        except Exception as e:
+                            st.write(f"Error occurred at index {index}: {e}")
+
 
             st.header('Recently added or updated items')
             df['Abstract'] = df['Abstract'].str.strip()
