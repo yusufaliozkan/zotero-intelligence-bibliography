29e36773653e0d326349b2b556bfbedbf5a793b1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-23 20:10:22 +0000

    uy

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -845,7 +845,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             else:
                 publications_by_author = filtered_authors['Author_name'].value_counts().head(num_authors)
                 publications_by_author
-                fig = px.bar(publications_by_author, x=publications_by_author.index, y=publications_by_author.values, color='Publication type')
+                fig = px.bar(publications_by_author, x=publications_by_author.index, y=publications_by_author.values)
                 fig.update_layout(
                     title=f'Top {num_authors} Authors by Publication Count',
                     xaxis_title='Author',
