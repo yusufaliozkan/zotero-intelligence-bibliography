9893df207d89372a0ae7f40f19c528ec0388114b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-23 20:25:44 +0000

    sd

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -257,8 +257,37 @@ with st.spinner('Retrieving data & updating dashboard...'):
             st.header('Search in database')
             search_option = st.radio("Select search option", ("Search keywords", "Search author", "Search collections"))
 
-            if search_option == "Search keywords":
+            df_authors = pd.read_csv('all_items.csv')
+            # df_authors['FirstName2'].fillna('', inplace=True)
+            df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+            df_authors = df_authors.explode('Author_name')
+            df_authors.reset_index(drop=True, inplace=True)
+            df_authors = df_authors.dropna(subset=['FirstName2'])
+            name_replacements = {
+                'David Gioe': 'David V. Gioe',
+                'David Vincent Gioe': 'David V. Gioe',
+                'Michael Goodman': 'Michael S. Goodman',
+                'Michael S Goodman': 'Michael S. Goodman',
+                'Michael Simon Goodman': 'Michael S. Goodman',
+                'Thomas Maguire':'Thomas J. Maguire',
+                'Thomas Joseph Maguire':'Thomas J. Maguire',
+                'Huw John Davies':'Huw J. Davies',
+                'Huw Davies':'Huw J. Davies',
+                'Philip H.J. Davies':'Philip H. J. Davies',
+                'Philip Davies':'Philip H. J. Davies',
+                'Dan Lomas':'Daniel W. B. Lomas',
+                'Richard Aldrich':'Richard J. Aldrich',
+                'Richard J Aldrich':'Richard J. Aldrich',
+                'Steven Wagner':'Steven B. Wagner',
+                'Daniel Larsen':'Daniel R. Larsen',
+                'Daniel Richard Larsen':'Daniel R. Larsen',
+                'Loch Johnson':'Loch K. Johnson',
+                'Sir David Omand Gcb':'David Omand',
+                'Sir David Omand':'David Omand'
+            }
+            df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
 
+            if search_option == "Search keywords":
                 st.subheader('Search keywords')
                 search_term = st.text_input('Search keywords in titles or author names')
                 if search_term:
@@ -355,35 +384,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             # SEARCH AUTHORS
             elif search_option == "Search author":
                 st.subheader('Search author')
-                df_authors = pd.read_csv('all_items.csv')
-                # df_authors['FirstName2'].fillna('', inplace=True)
-                df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
-                df_authors = df_authors.explode('Author_name')
-                df_authors.reset_index(drop=True, inplace=True)
-                df_authors = df_authors.dropna(subset=['FirstName2'])
-                name_replacements = {
-                    'David Gioe': 'David V. Gioe',
-                    'David Vincent Gioe': 'David V. Gioe',
-                    'Michael Goodman': 'Michael S. Goodman',
-                    'Michael S Goodman': 'Michael S. Goodman',
-                    'Michael Simon Goodman': 'Michael S. Goodman',
-                    'Thomas Maguire':'Thomas J. Maguire',
-                    'Thomas Joseph Maguire':'Thomas J. Maguire',
-                    'Huw John Davies':'Huw J. Davies',
-                    'Huw Davies':'Huw J. Davies',
-                    'Philip H.J. Davies':'Philip H. J. Davies',
-                    'Philip Davies':'Philip H. J. Davies',
-                    'Dan Lomas':'Daniel W. B. Lomas',
-                    'Richard Aldrich':'Richard J. Aldrich',
-                    'Richard J Aldrich':'Richard J. Aldrich',
-                    'Steven Wagner':'Steven B. Wagner',
-                    'Daniel Larsen':'Daniel R. Larsen',
-                    'Daniel Richard Larsen':'Daniel R. Larsen',
-                    'Loch Johnson':'Loch K. Johnson',
-                    'Sir David Omand Gcb':'David Omand',
-                    'Sir David Omand':'David Omand'
-                }
-                df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
+
                 unique_authors = [''] + list(df_authors['Author_name'].unique())
 
                 author_publications = df_authors['Author_name'].value_counts().to_dict()
