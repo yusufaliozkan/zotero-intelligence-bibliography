b253dcd958ba5f08662007323f041ddd84d1340d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-23 12:54:17 +0000

    er

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -400,7 +400,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 filtered_collection_df_authors = filtered_collection_df_authors.sort_values(by=['Date published'], ascending=False)
                 filtered_collection_df_authors =filtered_collection_df_authors.reset_index(drop=True)
 
-                filtered_collection_df_authors
                 publications_by_type = filtered_collection_df_authors['Publication type'].value_counts()
 
                 with st.expander('Click to expand', expanded=True):
@@ -473,6 +472,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 filtered_collection_df['No date flag'] = filtered_collection_df['Date published'].isnull().astype(np.uint8)
                 filtered_collection_df = filtered_collection_df.sort_values(by=['No date flag', 'Date published'], ascending=[True, True])
                 filtered_collection_df = filtered_collection_df.sort_values(by=['Date published'], ascending=False)
+                filtered_collection_df
 
                 publications_by_type = filtered_collection_df_authors['Publication type'].value_counts()
 
