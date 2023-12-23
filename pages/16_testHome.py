d6f772a78360ad43ed74591c53a1c456d24e346e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-23 16:03:53 +0000

    sd

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -392,7 +392,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 selected_author_display = st.selectbox('Select author', select_options_author_with_counts)
                 selected_author = selected_author_display.split(' (')[0] if selected_author_display else None
                 # selected_author = st.selectbox('Select author', select_options_author)
-                search_term = None
 
                 if not selected_author  or selected_author =="":
                     st.write('Select an author to see items')
@@ -539,9 +538,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 '[[Zotero link]](' + str(zotero_link) + ')'
                             )
                             st.write(f"{index + 1}) {formatted_entry}")
-            if search_term is None:
-                st.subheader('Search keywords')  # This subheader will disappear if Search author is selected
-                search_term = st.text_input('Search keywords in titles or author names')                    
 
             # RECENTLY ADDED ITEMS
             st.header('Recently added or updated items')
