e95f44d53a3b788c6e130af9f7fd72ed9edc5a98
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 16:30:31 +0000

    ew

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -122,11 +122,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_collections = df_collections[df_collections['Publication type'].isin(types)]
                 df_collections = df_collections.reset_index(drop=True)
                 df_download = df_collections[['Publication type','Title','FirstName2','Abstract','Date published','Publisher','Journal','Link to publication','Zotero link']]
-                df_download
-                def convert_df(df_collections):
-                    return df_collections.to_csv(index=False).encode('utf-8-sig')
-
-                csv = convert_df(df_collections)
+                def convert_df(df_download):
+                    return df_download.to_csv(index=False).encode('utf-8-sig')
+                csv = convert_df(df_download)
                 today = datetime.date.today().isoformat()
                 num_items_collections = len(df_collections)
                 breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
