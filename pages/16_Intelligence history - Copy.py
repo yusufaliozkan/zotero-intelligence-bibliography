42a49852c83220ee975e21fa3d7d071b0d4f9656
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 17:16:06 +0000

    we

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -117,23 +117,22 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
             st.markdown('#### Collection theme: ' + collection_name)
             st.write(f"See the collection in [Zotero]({collection_link})")
+            types = st.multiselect('Publication type', df_collections['Publication type'].unique(),df_collections['Publication type'].unique(), key='original')
+            df_collections = df_collections[df_collections['Publication type'].isin(types)]
+            df_collections = df_collections.reset_index(drop=True)
+            df_download = df_collections[['Publication type','Title','FirstName2','Abstract','Date published','Publisher','Journal','Link to publication','Zotero link']]
+            df_download = df_download.reset_index(drop=True)
+            def convert_df(df_download):
+                return df_download.to_csv(index=False).encode('utf-8-sig')
+            csv = convert_df(df_download)
+            today = datetime.date.today().isoformat()
+            num_items_collections = len(df_collections)
+            breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
+            st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
+            a = f'{collection_name}_{today}'
+            st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
 
             with st.expander('Click to expand', expanded=True):
-                types = st.multiselect('Publication type', df_collections['Publication type'].unique(),df_collections['Publication type'].unique(), key='original')
-                df_collections = df_collections[df_collections['Publication type'].isin(types)]
-                df_collections = df_collections.reset_index(drop=True)
-                df_download = df_collections[['Publication type','Title','FirstName2','Abstract','Date published','Publisher','Journal','Link to publication','Zotero link']]
-                df_download = df_download.reset_index(drop=True)
-                def convert_df(df_download):
-                    return df_download.to_csv(index=False).encode('utf-8-sig')
-                csv = convert_df(df_download)
-                today = datetime.date.today().isoformat()
-                num_items_collections = len(df_collections)
-                breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
-                st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
-                a = f'{collection_name}_{today}'
-                st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
-
                 def format_entry(row):
                     publication_type = str(row['Publication type']) if pd.notnull(row['Publication type']) else ''
                     title = str(row['Title']) if pd.notnull(row['Title']) else ''
