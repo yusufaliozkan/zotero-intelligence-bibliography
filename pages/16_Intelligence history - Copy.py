9911980648618f6b1a75e85b43ae6fcb2c4c7975
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 16:34:10 +0000

    s

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -132,7 +132,40 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
                 a = f'{collection_name}_{today}'
                 st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
-                df_collections
+
+                articles_list = []  # Store articles in a list
+                for index, row in df_collections.iterrows():
+                    formatted_entry = format_entry(row)  # Assuming format_entry() is a function formatting each row
+                    articles_list.append(formatted_entry)                     
+                
+                for index, row in df_collections.iterrows():
+                    publication_type = row['Publication type']
+                    title = row['Title']
+                    authors = row['FirstName2']
+                    date_published = row['Date published']
+                    link_to_publication = row['Link to publication']
+                    zotero_link = row['Zotero link']
+
+                    if publication_type == 'Journal article':
+                        published_by_or_in = 'Published in'
+                        published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
+                    elif publication_type == 'Book':
+                        published_by_or_in = 'Published by'
+                        published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
+                    else:
+                        published_by_or_in = ''
+                        published_source = ''
+
+                    formatted_entry = (
+                        '**' + str(publication_type) + '**' + ': ' +
+                        str(title) + ' ' +
+                        '(by ' + '*' + str(authors) + '*' + ') ' +
+                        '(Publication date: ' + str(date_published) + ') ' +
+                        ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
+                        '[[Publication link]](' + str(link_to_publication) + ') ' +
+                        '[[Zotero link]](' + str(zotero_link) + ')'
+                    )
+                    st.write(f"{index + 1}) {formatted_entry}")
 
         # Collection items
             count_collection = zot.num_collectionitems(collection_code)
