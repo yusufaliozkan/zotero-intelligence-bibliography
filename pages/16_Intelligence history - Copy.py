fbdf18aeb4886e087bf081ff24ae9809bd37c5a4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 23:03:43 +0000

    ds

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -91,7 +91,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
 
     df_collections = df_collections.sort_values(by='Collection_Name')
-    df_collections=df_collections[df_collections['Collection_Name'].str.contains("01.")]
+    df_collections=df_collections[df_collections['Collection_Name'].str.contains("98.")]
     df_collections = df_collections.iloc[1: , :]
 
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
