1c41f893148e312eba0d93fa5ceab2e981967ef4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 18:12:44 +0000

    ew

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -617,7 +617,35 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 row_nu_1 = len(df.index)
 
         def format_entry(row):
-            # ... (existing format_entry function)
+            publication_type = str(row['Publication type']) if pd.notnull(row['Publication type']) else ''
+            title = str(row['Title']) if pd.notnull(row['Title']) else ''
+            authors = str(row['FirstName2'])
+            date_published = str(row['Date published']) if pd.notnull(row['Date published']) else ''
+            link_to_publication = str(row['Link to publication']) if pd.notnull(row['Link to publication']) else ''
+            zotero_link = str(row['Zotero link']) if pd.notnull(row['Zotero link']) else ''
+            published_by_or_in = ''
+            published_source = ''
+
+            if publication_type == 'Journal article':
+                published_by_or_in = 'Published in'
+                published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
+            elif publication_type == 'Book':
+                published_by_or_in = 'Published by'
+                published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
+            else:
+                # For other types, leave the fields empty
+                published_by_or_in = ''
+                published_source = ''
+
+            return (
+                '**' + publication_type + '**' + ': ' +
+                title + ' ' +
+                '(by ' + '*' + authors + '*' + ') ' +
+                '(Publication date: ' + str(date_published) + ') ' +
+                ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
+                '[[Publication link]](' + link_to_publication + ') ' +
+                '[[Zotero link]](' + zotero_link + ')'
+            )
 
         articles_list = []
         for index, row in df.iterrows():
