d3f3a5eaf4be976b0aeb6880f0fa7839a23d2f3b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-24 18:09:09 +0000

    ytr

--- pages/16_Intelligence history - Copy.py
+++ pages/16_Intelligence history - Copy.py
@@ -629,12 +629,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
         else:
                 # st.write('author entered')
                 ## This section is for displaying the first author details but it doesn't work for now because of json normalization error.
-                df_fa = df['FirstName2']
-                df_fa = pd.DataFrame(df_fa.tolist())
-                df_fa = df_fa[0]
-                df_fa = df_fa.apply(lambda x: {} if pd.isna(x) else x) # https://stackoverflow.com/questions/44050853/pandas-json-normalize-and-null-values-in-json
-                df_new = pd.json_normalize(df_fa, errors='ignore') 
-                df = pd.concat([df, df_new], axis=1)
                 df_items = ('**'+ df['Publication type']+ '**'+ ': ' +
                             df['Title'] + ' '+ 
                             ' (by ' + '*' + df['FirstName2'] + ') ' + # IT CANNOT READ THE NAN VALUES
