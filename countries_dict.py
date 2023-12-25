65a04c6c14bd55582e3c334c9f634c32fdd80e8e
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-12-25 17:27:39 +0000

    Create countries_dict.py

---
+++ countries_dict.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+df_authors = pd.read_csv('all_items.csv')
+# df_authors['FirstName2'].fillna('', inplace=True)
+df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+df_authors = df_authors.explode('Author_name')
+df_authors.reset_index(drop=True, inplace=True)
+df_authors = df_authors.dropna(subset=['FirstName2'])
+name_replacements = {
+    'David Gioe': 'David V. Gioe',
+    'David Vincent Gioe': 'David V. Gioe',
+    'Michael Goodman': 'Michael S. Goodman',
+    'Michael S Goodman': 'Michael S. Goodman',
+    'Michael Simon Goodman': 'Michael S. Goodman',
+    'Thomas Maguire':'Thomas J. Maguire',
+    'Thomas Joseph Maguire':'Thomas J. Maguire',
+    'Huw John Davies':'Huw J. Davies',
+    'Huw Davies':'Huw J. Davies',
+    'Philip H.J. Davies':'Philip H. J. Davies',
+    'Philip Davies':'Philip H. J. Davies',
+    'Dan Lomas':'Daniel W. B. Lomas',
+    'Richard Aldrich':'Richard J. Aldrich',
+    'Richard J Aldrich':'Richard J. Aldrich',
+    'Steven Wagner':'Steven B. Wagner',
+    'Daniel Larsen':'Daniel R. Larsen',
+    'Daniel Richard Larsen':'Daniel R. Larsen',
+    'Loch Johnson':'Loch K. Johnson',
+    'Sir David Omand Gcb':'David Omand',
+    'Sir David Omand':'David Omand'
+}
+df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
