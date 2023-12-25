c12807c6f27d2a80f2dacba8682d84568255a233
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-25 17:09:02 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -29,6 +29,7 @@ st.set_page_config(layout = "wide",
 st.title("Global intelligence")
 
 with st.spinner('Retrieving data & updating dashboard...'):
+    time.sleep(5)
 
     # # Connecting Zotero with API
     # library_id = '2514686' # intel 2514686
