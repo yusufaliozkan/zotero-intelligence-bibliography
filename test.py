5540bd454e26dce18a4600bc772ce0e23d948f87
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-26 13:05:11 +0000

    sd

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json, sys
 from datetime import date, timedelta  
 import datetime
 import plotly.express as px
-from pycountry import pycountry
+import pycountry
 import re
 import pandas as pd
 
