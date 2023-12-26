ac0f295844e01dfc01745a2b737a3f6fbc906d8e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-26 13:12:07 +0000

    m

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -19,7 +19,7 @@ from wordcloud import WordCloud
 from gsheetsdb import connect
 import datetime as dt
 from urllib.parse import urlparse
-from pycountry import pycountry
+import pycountry
 from test import df
 
 st.set_page_config(layout = "centered", 
