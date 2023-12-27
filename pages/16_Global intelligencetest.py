ba0508fd062202dba0e7b34f17db4957bc5cb54f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 12:44:16 +0000

    ty

--- pages/16_Global intelligencetest.py
+++ pages/16_Global intelligencetest.py
@@ -333,6 +333,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 else:
                     st.write('Please select a country')
                 country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=True)
+                country_pub_counts
                 fig = px.bar(x=country_pub_counts.values, y=country_pub_counts.index, orientation='h')
                 fig.update_layout(title='Number of Publications by Country', xaxis_title='Number of Publications', yaxis_title='Country')
                 st.plotly_chart(fig)
