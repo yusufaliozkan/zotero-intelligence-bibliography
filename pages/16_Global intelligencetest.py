35ded15a22979320b53571771c29f27557afb6ef
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 12:47:11 +0000

    we

--- pages/16_Global intelligencetest.py
+++ pages/16_Global intelligencetest.py
@@ -332,8 +332,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 st.caption(row['Abstract'])
                 else:
                     st.write('Please select a country')
-                country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=True)
-                top_10_countries = country_pub_counts.head(10)
+                country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=False)
+                top_10_countries = country_pub_counts.head(10).sort_values(ascending=False)
                 top_10_df = pd.DataFrame({'Country': top_10_countries.index, 'Publications': top_10_countries.values})
                 fig = px.bar(top_10_df, x='Publications', y='Country', orientation='h')
                 fig.update_layout(title='Top 10 Countries by Number of Publications', xaxis_title='Number of Publications', yaxis_title='Country')
