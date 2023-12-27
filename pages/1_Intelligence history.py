26f4ee1758422a959a991cdde94cbc7ecec67956
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 11:28:56 +0000

    ;l,

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -483,7 +483,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             height=700,
                             showlegend=False)
                         fig.update_xaxes(tickangle=-70)
-                        fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles (in log scale)', 'y':0.95, 'x':0.4, 'yanchor':'top'})
+                        fig.update_layout(title={'text':'Top ' + str(number2) + ' journals that publish intelligence articles (in log scale)'})
                         col2.plotly_chart(fig, use_container_width = True)
                     else:
                         fig = px.bar(df_journal, x='Journal', y='Count', color='Journal', log_y=True)
