dd7bfc0e5930355e889416ed4e0f1ba6a72e56dd
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 12:52:34 +0000

    z

--- pages/16_Global intelligencetest.py
+++ pages/16_Global intelligencetest.py
@@ -234,7 +234,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 country_counts = df_countries['Country'].value_counts().to_dict()
                 unique_countries = sorted(df_countries['Country'].unique())
                 unique_countries = [''] + unique_countries
-                options_with_counts = [f"{country} ({country_counts.get(country, 0)})" if country != '' and country_counts.get(country, 0) > 0 else country for country in unique_countries]
+                options_with_counts = [f"{country} ({country_counts.get(country, 0)})" for country in unique_countries]
                 selected_country = st.selectbox('Select a Country', options_with_counts)
 
                 # Filter the DataFrame based on the selected country
