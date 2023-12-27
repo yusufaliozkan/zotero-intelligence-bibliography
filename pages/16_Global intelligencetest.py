5e4fb02c2d118c77a5e558c26c0a345f5f83e2f3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 12:46:31 +0000

    dd

--- pages/16_Global intelligencetest.py
+++ pages/16_Global intelligencetest.py
@@ -332,10 +332,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 st.caption(row['Abstract'])
                 else:
                     st.write('Please select a country')
-                country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=True)
-                country_pub_counts
-                fig = px.bar(x=country_pub_counts.values, y=country_pub_counts.index, orientation='h')
-                fig.update_layout(title='Number of Publications by Country', xaxis_title='Number of Publications', yaxis_title='Country')
+                country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=False)
+                top_10_countries = country_pub_counts.head(10)
+                top_10_df = pd.DataFrame({'Country': top_10_countries.index, 'Publications': top_10_countries.values})
+                fig = px.bar(top_10_df, x='Publications', y='Country', orientation='h')
+                fig.update_layout(title='Top 10 Countries by Number of Publications', xaxis_title='Number of Publications', yaxis_title='Country')
                 st.plotly_chart(fig)
 
 #UNTIL HERE
