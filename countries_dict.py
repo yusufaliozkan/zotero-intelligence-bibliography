d73d87b21f3e503bf7acc77efceb28356500f50f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2023-12-27 17:28:29 +0000

    er

--- countries_dict.py
+++ countries_dict.py
@@ -91,7 +91,7 @@ continent_country_names = [
     "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi",
     "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
     "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (formerly Burma)", "Namibia",
-    "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia",
+    "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Republic of the Niger", "Nigeria", "North Korea", "North Macedonia",
     "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay", "Peru",
     "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia",
     "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal",
