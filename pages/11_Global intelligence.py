473f0a080c900ce0ac222c76b358603309d5ed60
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-02 13:10:24 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -287,6 +287,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     a = f'{collection_name}_{today}'
                     st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
                     st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
+                    df_countries
                     def format_entry(row):
                         publication_type = str(row['Publication type']) if pd.notnull(row['Publication type']) else ''
                         title = str(row['Title']) if pd.notnull(row['Title']) else ''
