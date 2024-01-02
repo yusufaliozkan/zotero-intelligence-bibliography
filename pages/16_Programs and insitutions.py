fb72edb904f293b6f86e4b7cc687e873a566b167
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-02 23:53:59 +0000

    sd

--- pages/16_Programs and insitutions.py
+++ pages/16_Programs and insitutions.py
@@ -28,7 +28,7 @@ st.set_page_config(layout = "wide",
                     initial_sidebar_state="auto") 
 
 st.title("Intelligence studies network")
-st.header('Institutions & programs on intelligence')
+st.header('Institutions & programs on intelligence studies')
 
 with st.spinner('Preparing...'):
 
