6b4ffa63d6fb7d73094fe74c97b7c98d6d38e756
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 11:24:25 +0000

    sd

--- pages/16_Programs and insitutions.py
+++ pages/16_Programs and insitutions.py
@@ -129,15 +129,12 @@ with col1:
                 if selected_country:
                     type_programs = type_programs[type_programs['Country'].isin(selected_country)]
                     programme_levels = type_programs['Programme_level'].unique()
+
+                else:
+                    programme_levels = type_programs['Programme_level'].unique()
                     selected_level = st.selectbox("Filter by Programme Level:", ['All'] + list(programme_levels))
                     if selected_level != 'All':
                         type_programs = type_programs[type_programs['Programme_level'] == selected_level]
-
-                programme_levels = type_programs['Programme_level'].unique()
-
-                selected_level = st.selectbox("Filter by Programme Level:", ['All'] + list(programme_levels))
-                if selected_level != 'All':
-                    type_programs = type_programs[type_programs['Programme_level'] == selected_level]
 
             display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
 
