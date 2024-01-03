fe0a8b670a6b0ce58d169c7d69dbf5a04c8c564b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 12:41:18 +0000

    sd

--- pages/16_Programs and insitutions.py
+++ pages/16_Programs and insitutions.py
@@ -158,7 +158,7 @@ with col1:
                             selected_country_str = selected_country[0].split(" (")[0]
                             st.write(f'**{len(type_programs)} program(s) found in {selected_country_str}**')
                         else:
-                            st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} country**')
+                            st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
             display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
 
 with col2:
