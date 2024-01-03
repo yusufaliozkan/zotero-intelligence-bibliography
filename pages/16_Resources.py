ee1f5230ee54fbec100e4c9ee9b3a0e36bbf83e4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 14:37:22 +0000

    sd

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -145,30 +145,35 @@ with col1:
                     st.write(f'**{len(type_programs)} program(s) found in {num_unique_countries} countries**')
 
             if prog_type != 'Academic programs':
-                if num_unique_countries!=1:
+                if num_unique_countries != 1:
                     prog_type = prog_type.sort_values(by=['Country'], ascending=True)
                     current_type = None
                     for index, row in prog_type.iterrows():
                         if row['Country'] != current_type:
                             current_type = row['Country']
                             st.subheader(current_type)
+                        # Assuming display_numbered_list shows a list of programs based on type_programs
                         display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
                         count_by_type[current_type] += 1
+
                     num_unique_countries = type_programs['Country'].nunique()
                     country_counts = type_programs['Country'].value_counts().sort_values(ascending=False)
                     countries_sorted = country_counts.index.tolist()
                     country_counts_dict = {country: f"{country} ({count})" for country, count in country_counts.items()}
                     selected_country = st.multiselect('Filter by country:', countries_sorted, format_func=lambda x: country_counts_dict[x])
+
                     if selected_country:
                         type_programs = type_programs[type_programs['Country'].isin(selected_country)]
                         num_unique_countries = type_programs['Country'].nunique()
-                        if num_unique_countries==1:
+                        if num_unique_countries == 1:
                             selected_country_str = selected_country[0].split(" (")[0]
                             st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
                         else:
                             st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                     else:
                         st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
+
+            # Displaying the numbered list outside the loop
             display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
 
 with col2:
