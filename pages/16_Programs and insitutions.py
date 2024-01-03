f3efcb8b2764d6c831666af4dd892204e79bc575
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 11:33:33 +0000

    ds

--- pages/16_Programs and insitutions.py
+++ pages/16_Programs and insitutions.py
@@ -136,7 +136,7 @@ with col1:
                 selected_level = st.selectbox("Filter by Programme Level:", ['All'] + list(programme_levels))
                 if selected_level != 'All':
                     type_programs = type_programs[type_programs['Programme_level'] == selected_level]
-                st.write(f'{len(type_programs)} program(s) found in {countries_sorted} country')
+                st.write(f'{len(type_programs)} program(s) found in {num_unique_countries} country')
 
             display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
 
