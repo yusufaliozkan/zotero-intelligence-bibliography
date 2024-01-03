c093b2b09cb57b3e6e033749bb5cbbd22bf4672a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 21:46:14 +0000

    sd

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -126,21 +126,25 @@ with col1:
                     country_counts = type_programs['Country'].value_counts().sort_values(ascending=False)
                     countries_sorted = country_counts.index.tolist()
                     country_counts_dict = {country: f"{country} ({count})" for country, count in country_counts.items()}
-                    selected_country = st.multiselect('Filter by country:', countries_sorted, format_func=lambda x: country_counts_dict[x])
-                    if selected_country:
-                        type_programs = type_programs[type_programs['Country'].isin(selected_country)]
+                    selected_countries = st.multiselect('Filter by country:', countries_sorted, format_func=lambda x: country_counts_dict[x])
+                    if selected_countries:
+                        type_programs = type_programs[type_programs['Country'].isin(selected_countries)]
                         num_unique_countries = type_programs['Country'].nunique()
                         if num_unique_countries == 1:
-                            selected_country_str = selected_country[0].split(" (")[0]
+                            selected_country_str = selected_countries[0].split(" (")[0]
                             st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
                         else:
                             st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                     else:
-                        selected_country = countries_sorted  # Show all countries if no country is selected
+                        selected_countries = countries_sorted  # Show all countries if none is selected
                         type_programs = df[df['Type'] == prog_type]  # Show all programs initially
 
-                for country in countries_sorted:
-                    if num_unique_countries == 1 or (selected_country and country in selected_country):
+                if len(selected_countries) == 1:
+                    country_programs = type_programs[type_programs['Country'] == selected_countries[0]]
+                    st.write(f'**{len(country_programs)} {prog_type} found in {selected_countries[0].split(" (")[0]}**')
+                    display_numbered_list(country_programs, prog_type, show_country=True)
+                else:
+                    for country in selected_countries:
                         country_programs = type_programs[type_programs['Country'] == country]
                         st.subheader(country)
                         display_numbered_list(country_programs, prog_type, show_country=True)
