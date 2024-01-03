00735c3b784f5de680767f265d6311e03f633e9f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 21:53:28 +0000

    sd

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -133,7 +133,7 @@ with col1:
                         if num_unique_countries == 1:
                             selected_country_str = selected_countries[0].split(" (")[0]
                             st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
-                        else:
+                        #else:
                             # st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                     else:
                         selected_countries = countries_sorted  # Show all countries if none is selected
