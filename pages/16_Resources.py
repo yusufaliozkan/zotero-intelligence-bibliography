ad704e99e8c2dc3c6e8790cb8d64c252ffe1609c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 21:52:12 +0000

    er

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -144,9 +144,9 @@ with col1:
                     # st.write(f'**{len(country_programs)} {prog_type} found in {selected_countries[0].split(" (")[0]}**')
                     display_numbered_list(country_programs, prog_type, show_country=False)
                 else:
+                    st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                     for country in selected_countries:
                         country_programs = type_programs[type_programs['Country'] == country]
-                        st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                         st.subheader(country)
                         display_numbered_list(country_programs, prog_type, show_country=False)
         else:
