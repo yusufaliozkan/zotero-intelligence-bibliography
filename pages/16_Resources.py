f901f586f6d7bac61095379da5ec76c6acbaa5f7
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 21:53:14 +0000

    s

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -132,9 +132,9 @@ with col1:
                         num_unique_countries = type_programs['Country'].nunique()
                         if num_unique_countries == 1:
                             selected_country_str = selected_countries[0].split(" (")[0]
-                            #st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
+                            st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
                         else:
-                            st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
+                            # st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                     else:
                         selected_countries = countries_sorted  # Show all countries if none is selected
                         type_programs = df[df['Type'] == prog_type]  # Show all programs initially
