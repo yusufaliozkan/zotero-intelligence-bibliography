8a9fb01ef05d92201f9e1502d6149652bd4b889a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 09:39:35 +0000

    ds

--- pages/16_Programs and insitutions.py
+++ pages/16_Programs and insitutions.py
@@ -106,7 +106,7 @@ with st.spinner('Preparing...'):
                 
                 programme_info = ""
                 if programme_level and programme_name:
-                    programme_info = f"[{programme_name}]({row['Link']}) ({programme_level}), *{row['Institution']}*, {Country}"
+                    programme_info = f"[{programme_name}]({row['Link']}) ({programme_level}), *{row['Institution']}*, {row['Country']}"
                 else:
                     programme_info = f"[{row['Institution']}]({row['Link']})"
                 
