4c0267bb7df908566430c9e860b948f30336c5b5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-03 21:39:13 +0000

    r

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -127,7 +127,7 @@ with col1:
                 for country in selected_countries :
                     country_programs = type_programs[type_programs['Country'] == country]
                     st.subheader(f'{country}')
-                    display_numbered_list(country_programs, prog_type, show_country=True)       
+                    display_numbered_list(country_programs, prog_type, show_country=False)       
         else:
             with st.expander(f"{prog_type} ({len(type_programs)})"):
                 if prog_type == 'Academic programs':
