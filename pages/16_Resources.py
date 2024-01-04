70734ae757ccae15cacef53b3fb083c94a4d6a83
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-04 12:08:34 +0000

    sd

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -164,56 +164,37 @@ with col1:
                     country_counts_dict = {country: f"{country} ({count})" for country, count in country_counts.items()}
 
                     selected_country = st.multiselect('Filter by country:', countries_sorted, format_func=lambda x: country_counts_dict[x])
-                    
+
                     if selected_country:
-                        type_programs = type_programs[type_programs['Country'].isin(selected_country)]
-                        programme_levels = type_programs['Programme_level'].unique()
-                    
-                    programme_levels = type_programs['Programme_level'].unique()
-                    selected_level = st.multiselect("Filter by Programme Level:", programme_levels)
+                        for country in selected_country:
+                            country_programs = type_programs[type_programs['Country'] == country]
+                            st.markdown(f"#### {country}")
+                            display_numbered_list(country_programs, prog_type, show_country=False, show_programme_level=True)
 
-                    if selected_level:
-                        type_programs = type_programs[type_programs['Programme_level'].isin(selected_level)]
-
-                    num_unique_countries = type_programs['Country'].nunique()
-                    if num_unique_countries==1:
-                        selected_country_str = selected_country[0].split(" (")[0]
-                        st.write(f'**{len(type_programs)} program(s) found in {selected_country_str}**')
                     else:
-                        st.write(f'**{len(type_programs)} program(s) found in {num_unique_countries} countries**')
+                        for country in countries_sorted:
+                            country_programs = type_programs[type_programs['Country'] == country]
+                            st.markdown(f"#### {country}")
+                            display_numbered_list(country_programs, prog_type, show_country=False, show_programme_level=True)
 
-                elif prog_type == 'Academic programs':
-                    with st.expander(f"**{prog_type} ({len(type_programs)})**"):
+                if prog_type != 'Academic programs':
+                    if num_unique_countries!=1:
+                        num_unique_countries = type_programs['Country'].nunique()
                         country_counts = type_programs['Country'].value_counts().sort_values(ascending=False)
                         countries_sorted = country_counts.index.tolist()
                         country_counts_dict = {country: f"{country} ({count})" for country, count in country_counts.items()}
-
                         selected_country = st.multiselect('Filter by country:', countries_sorted, format_func=lambda x: country_counts_dict[x])
-
                         if selected_country:
                             type_programs = type_programs[type_programs['Country'].isin(selected_country)]
-                            programme_levels = type_programs['Programme_level'].unique()
-
-                        programme_levels = type_programs['Programme_level'].unique()
-                        selected_level = st.multiselect("Filter by Programme Level:", programme_levels)
-
-                        if selected_level:
-                            type_programs = type_programs[type_programs['Programme_level'].isin(selected_level)]
-
-                        num_unique_countries = type_programs['Country'].nunique()
-                        if num_unique_countries == 1:
-                            selected_country_str = selected_country[0].split(" (")[0]
-                            st.write(f'**{len(type_programs)} program(s) found in {selected_country_str}**')
+                            num_unique_countries = type_programs['Country'].nunique()
+                            if num_unique_countries==1:
+                                selected_country_str = selected_country[0].split(" (")[0]
+                                st.write(f'**{len(type_programs)} {prog_type} found in {selected_country_str}**')
+                            else:
+                                st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
                         else:
-                            st.write(f'**{len(type_programs)} program(s) found in {num_unique_countries} countries**')
-
-                        # Adding markdown listing countries
-                        st.markdown("##### Countries with Academic programs:")
-                        st.markdown("\n".join([f"- {country}" for country in countries_sorted]))
-
-                        display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
-                    else:
-                        display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
+                            st.write(f'**{len(type_programs)} {prog_type} found in {num_unique_countries} countries**')
+                display_numbered_list(type_programs, prog_type, show_country=False if prog_type != 'Academic' else False)
 
 with col2:
     with st.expander('Collections', expanded=True):
