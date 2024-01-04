669101050d01e0057a455397ddae8cb2577b9c78
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-04 20:30:15 +0000

    s

--- Home.py
+++ Home.py
@@ -455,13 +455,14 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             st.plotly_chart(fig)
 
                             author_df = filtered_collection_df_authors
+                            author_df['Year'] = pd.to_datetime(author_df['Date published']).dt.year
                             author_df
-                            publications_by_year = author_df['Date published'].value_counts().sort_index()
+                            publications_by_year = author_df['Year'].value_counts().sort_index()
                             publications_by_year
-                            fig_year = px.bar(publications_by_year, x=publications_by_year.index, y=publications_by_year.values,
-                                            labels={'x': 'Publication Year', 'y': 'Number of Publications'},
-                                            title=f'Publications by Year for {selected_author}')
-                            st.plotly_chart(fig_year)
+                            fig_year_line = px.line(publications_by_year, x=publications_by_year.index, y=publications_by_year.values,
+                                                    labels={'x': 'Publication Year', 'y': 'Number of Publications'},
+                                                    title=f'Publications by Year for {selected_author}')
+                            st.plotly_chart(fig_year_line)
 
                         else:
                             for index, row in filtered_collection_df_authors.iterrows():
