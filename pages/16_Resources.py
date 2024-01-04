db6a2d27e548deb32897e6568c80a279db35f982
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-04 14:09:43 +0000

    s

--- pages/16_Resources.py
+++ pages/16_Resources.py
@@ -179,8 +179,9 @@ with col1:
 
                     for country in countries_to_display:
                         country_programs = type_programs[type_programs['Country'] == country]
-                        st.markdown(f'### {country}')
-                        display_numbered_list(country_programs, prog_type, show_country=False, show_programme_level=True)  # Updated function call
+                        if len(country_programs) > 0:  # Check if there are items to display
+                            st.markdown(f'### {country}')
+                            display_numbered_list(country_programs, prog_type, show_country=False, show_programme_level=True)  # Updated function call
                 else:
                     if num_unique_countries != 1:
                         num_unique_countries = type_programs['Country'].nunique()
