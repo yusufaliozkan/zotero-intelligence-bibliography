9ad18bc43d23dfb890ffc0ffc63b1f911911f8c8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-05 15:05:44 +0000

    rt

--- Home.py
+++ Home.py
@@ -654,6 +654,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                             collection_author_df['Author_name'] = collection_author_df['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
                             collection_author_df = collection_author_df.explode('Author_name')
+                            collection_author_df.reset_index(drop=True, inplace=True)
                             collection_author_df
 
                         else:
