7ecadb8fe5958d0edc5b107cdc2b00d3d9734bf9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-05 16:11:09 +0000

    s

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -20,6 +20,7 @@ from gsheetsdb import connect
 import datetime as dt     
 import random
 from authors_dict import df_authors, name_replacements
+from custom_component import display_custom_license
 
 st.set_page_config(layout = "wide", 
                     page_title='Intelligence studies network',
