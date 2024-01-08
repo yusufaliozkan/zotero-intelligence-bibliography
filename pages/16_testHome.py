60396e0e008b4ff3c6a1b930c7944f7b1a3d8acd
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-08 21:33:50 +0000

    f

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -496,6 +496,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                     if display_abstracts:
                                         abstract = abstracts_list[i - 1]  # Get the corresponding abstract for this article
                                         if pd.notnull(abstract):
+                                            highlighted_abstract = highlight_terms(abstract, search_terms)  # Highlight search terms in abstract
                                             st.caption(f"**Abstract for article {i}**: {abstract}")
                                         else:
                                             st.caption(f"**Abstract for article {i}**: N/A")
