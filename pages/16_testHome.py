08a9c13af5250736cf04be13ff5a9feb2127f8b7
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-08 22:37:51 +0000

    s

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -308,7 +308,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             # Search for the entire phrase first
                             filtered_df = df_csv[
                                 (df_csv['Title'].str.contains(phrase_filter, case=False, na=False, regex=True)) |
-                                (df_csv['FirstName2'].str.contains(phrase_filter, case=False, na=False, regex=True)) |
+                                # (df_csv['FirstName2'].str.contains(phrase_filter, case=False, na=False, regex=True)) 
                                 (df_csv['Abstract'].str.contains(phrase_filter, case=False, na=False, regex=True))
                             ]
 
@@ -316,7 +316,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             for keyword in keyword_filters:
                                 keyword_filter_df = df_csv[
                                     (df_csv['Title'].str.contains(keyword, case=False, na=False, regex=True)) |
-                                    (df_csv['FirstName2'].str.contains(keyword, case=False, na=False, regex=True)) |
+                                    # (df_csv['FirstName2'].str.contains(keyword, case=False, na=False, regex=True)) |
                                     (df_csv['Abstract'].str.contains(keyword, case=False, na=False, regex=True))
                                 ]
                                 filtered_df = pd.concat([filtered_df, keyword_filter_df])
