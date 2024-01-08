1e4065ba6d4d534f709a6339de968ce7e466c580
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-08 16:32:05 +0000

    s

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -296,12 +296,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 
                 if search_term:
                     with st.expander('Click to expand', expanded=True):
-                        df_csv
                         search_terms = re.findall(r'(?:"[^"]*"|\w+)', search_term)  # Updated regex pattern
                         phrase_filter = '|'.join(search_terms)  # Filter for the entire phrase
                         keyword_filters = [term.strip('"') for term in search_terms]  # Separate filters for individual keywords
 
                         df_csv = pd.read_csv('all_items.csv')
+                        df_csv
 
                         # Search for the entire phrase first
                         filtered_df = df_csv[
