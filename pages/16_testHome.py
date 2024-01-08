17039b8cea2cef465ab78d565b7e94f2a05bc7fd
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-08 21:45:21 +0000

    sd

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -499,9 +499,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                         if pd.notnull(abstract):
                                             # Highlight search terms in the abstract using the same function
                                             highlighted_abstract = highlight_terms(abstract, search_terms)
-                                            st.markdown(f"<small>**Abstract for article {i}**: {highlighted_abstract}</small>", unsafe_allow_html=True)
+                                            st.markdown(f"**Abstract for article {i}**: <span style='font-size: small;'>{highlighted_abstract}</span>", unsafe_allow_html=True)
                                         else:
-                                            st.markdown(f"<small>**Abstract for article {i}**: N/A</small>")
+                                            st.markdown(f"**Abstract for article {i}**: N/A")
 
                         else:
                             st.write("No articles found with the given keyword/phrase.")
