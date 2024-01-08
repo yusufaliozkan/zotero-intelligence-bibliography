edb032baae9715d00c0221ee62c83f7425a01253
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-08 17:01:46 +0000

    s

--- pages/16_testHome.py
+++ pages/16_testHome.py
@@ -318,11 +318,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                         # Search for individual keywords separately and combine the results
                         for keyword in keyword_filters:
-                            keyword_filter_df = df_csv[
-                                (df_csv['Title'].str.contains(keyword, case=False, na=False, regex=True)) |
-                                (df_csv['FirstName2'].str.contains(keyword, case=False, na=False, regex=True)) |
-                                (df_csv['Abstract'].str.contains(keyword, case=False, na=False, regex=True))
-                            ]
+                            keyword_filter_df = df_csv[df_csv[search_columns].apply(lambda row: row.str.contains(keyword, case=False, na=False, regex=True)).any(axis=1)]
                             filtered_df = pd.concat([filtered_df, keyword_filter_df])
 
                         # Remove duplicates, if any
