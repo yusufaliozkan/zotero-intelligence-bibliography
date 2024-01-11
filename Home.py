1c63d62b28eb6e7378a490f61a8edbc2820ac685
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-11 22:10:57 +0000

    df

--- Home.py
+++ Home.py
@@ -1543,8 +1543,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
             st.header('Item includion history')
             df_added = df_csv.copy()
             df_added['Date added'] = pd.to_datetime(df_added['Date added'])
-            df_added['Month'] = df_added['Date added'].dt.to_period('M')
-            monthly_counts = df_added.groupby('Month').size()
+            df_added['YearMonth'] = df_added['Date added'].dt.to_period('M').astype(str)
+            monthly_counts = df_added.groupby('YearMonth').size()
             monthly_counts
 
         else:
