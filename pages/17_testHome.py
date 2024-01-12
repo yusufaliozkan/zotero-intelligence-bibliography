68900849575b0cf7f040711b284bc9ea6d5c6700
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-12 17:20:43 +0000

    s

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1134,6 +1134,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 years = st.slider('Publication years between:', int(min_y), int(max_y), (int(min_y), int(max_y)), key='years')
                 filter = (df_all['Date year'] >= years[0]) & (df_all['Date year'] < years[1])
                 df_all = df_all.loc[filter]
+                number_of_items = len(df_all)
+                number_of_items
                 
                 articles_list = []  # Store articles in a list
                 abstracts_list = [] #Store abstracts in a list
