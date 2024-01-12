dd93c45a059fe5e07102668e9fa1d2886dceee4a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-12 16:45:41 +0000

    s

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1124,7 +1124,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         '[[Zotero link]](' + zotero_link + ')'
                     )
                 df_all = pd.read_csv('all_items.csv')
-                df_all['Date published'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
+                df_all['Date published2'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
 
                 
                 articles_list = []  # Store articles in a list
