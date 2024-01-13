8efef943a27476e8d2aefeb68f1d984833bf3e70
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-13 20:30:06 +0000

    d

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -75,7 +75,7 @@ df['Abstract'] = df['Abstract'].fillna('No abstract')
 
 split_df= pd.DataFrame(df['Col key'].tolist())
 df = pd.concat([df, split_df], axis=1)
-df['Authors'] = df['Authors'].fillna('null')
+df['Authors'] = df['Authors'].fillna('null')  
 
 # Change type name
 type_map = {
