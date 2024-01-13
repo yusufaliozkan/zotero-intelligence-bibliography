31de5587f89e4e2f3e234d89e24d47075b2ea79f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-13 19:16:28 +0000

    s

--- pages/17_testHome - Copy.py
+++ pages/17_testHome - Copy.py
@@ -41,121 +41,6 @@ st.set_page_config(layout = "wide",
                     initial_sidebar_state="auto") 
 pd.set_option('display.max_colwidth', None)
 
-zot = zotero.Zotero(library_id, library_type)
-
-@st.cache_data(ttl=600)
-def zotero_data(library_id, library_type):
-    items = zot.top(limit=10)
-
-    data=[]
-    columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Col key', 'Authors', 'Pub_venue']
-
-    for item in items:
-        creators = item['data']['creators']
-        creators_str = ", ".join([creator.get('firstName', '') + ' ' + creator.get('lastName', '') for creator in creators])
-        data.append((item['data']['title'], 
-        item['data']['itemType'], 
-        item['data']['url'], 
-        item['data']['abstractNote'], 
-        item['links']['alternate']['href'],
-        item['data']['dateAdded'],
-        item['data'].get('date'), 
-        item['data']['dateModified'],
-        item['data']['collections'],
-        creators_str,
-        item['data'].get('publicationTitle')
-        ))
-    df = pd.DataFrame(data, columns=columns)
-    return df
-
-df = zotero_data(library_id, library_type) 
-
-df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
-df['Abstract'] = df['Abstract'].fillna('No abstract')
-
-split_df= pd.DataFrame(df['Col key'].tolist())
-df = pd.concat([df, split_df], axis=1)
-df['Authors'] = df['Authors'].fillna('null')
-
-# Change type name
-type_map = {
-    'thesis': 'Thesis',
-    'journalArticle': 'Journal article',
-    'book': 'Book',
-    'bookSection': 'Book chapter',
-    'blogPost': 'Blog post',
-    'videoRecording': 'Video',
-    'podcast': 'Podcast',
-    'magazineArticle': 'Magazine article',
-    'webpage': 'Webpage',
-    'newspaperArticle': 'Newspaper article',
-    'report': 'Report',
-    'forumPost': 'Forum post',
-    'conferencePaper' : 'Conference paper',
-    'audioRecording' : 'Podcast',
-    'preprint':'Preprint'
-}
-df['Publication type'] = df['Publication type'].replace(type_map)
-
-df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
-df['Date published'] = pd.to_datetime(df['Date published'],utc=True).dt.tz_convert('Europe/London')
-df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
-df['Date published'] = df['Date published'].fillna('No date')
-# df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
-
-df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce')
-df['Date added'] = df['Date added'].dt.strftime('%d/%m/%Y')
-df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
-df['Date modified'] = df['Date modified'].dt.strftime('%d/%m/%Y, %H:%M')
-
-# Bringing collections
-
-@st.cache_data(ttl=600)
-def zotero_collections2(library_id, library_type):
-    collections = zot.collections()
-    data = [(item['data']['key'], item['data']['name'], item['meta']['numItems'], item['links']['alternate']['href']) for item in collections]
-    df_collections = pd.DataFrame(data, columns=['Key', 'Name', 'Number', 'Link'])
-    return df_collections
-df_collections_2 = zotero_collections2(library_id, library_type)
-
-@st.cache_data
-def zotero_collections(library_id, library_type):
-    collections = zot.collections()
-    data2 = [(item['data']['key'], item['data']['name'], item['links']['alternate']['href']) for item in collections]
-    df_collections = pd.DataFrame(data2, columns=['Key', 'Name', 'Link'])
-    pd.set_option('display.max_colwidth', None)
-    return df_collections.sort_values(by='Name')
-df_collections = zotero_collections(library_id, library_type)
-
-#To be deleted
-if 0 in df:
-    merged_df = pd.merge(
-        left=df,
-        right=df_collections,
-        left_on=0,
-        right_on='Key',
-        how='left'
-    )
-    if 1 in merged_df:
-        merged_df = pd.merge(
-            left=merged_df,
-            right=df_collections,
-            left_on=1,
-            right_on='Key',
-            how='left'
-        )
-        if 2 in merged_df:
-            merged_df = pd.merge(
-                left=merged_df,
-                right=df_collections,
-                left_on=2,
-                right_on='Key',
-                how='left'
-            ) 
-df = merged_df.copy()
-#To be deleted
-
-df = df.fillna('')
 
 # Streamlit app
 
@@ -183,6 +68,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
         with st.expander('Intro'):
             st.info(into)
     with col1:
+        df_intro = pd.read_csv('all_items.csv')
+        df_intro
         st.write('There are '+  '**'+str(count)+ '**' + ' items in this library.')
         st.write('The library last updated on ' + '**'+ df.loc[0]['Date modified']+'**')
 
