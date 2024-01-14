9bf294d890121f0ed0c3e18723a215b808d09288
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-14 20:50:19 +0000

    f

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -356,6 +356,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                             # Remove duplicates, if any
                             filtered_df = filtered_df.drop_duplicates()
+                        filtered_df 
                         
                         filtered_df['Date published'] = pd.to_datetime(filtered_df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
                         filtered_df['Date published'] = filtered_df['Date published'].dt.strftime('%Y-%m-%d')
