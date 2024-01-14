6c420e03c8485d80730a0250a8aff61a68e982fa
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-14 14:06:40 +0000

    r

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -47,8 +47,6 @@ zot = zotero.Zotero(library_id, library_type)
 def zotero_data(library_id, library_type):
     items = zot.top(limit=5)
     items = sorted(items, key=lambda x: x['data']['dateAdded'], reverse=True)
-
-
     data=[]
     columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Col key', 'Authors', 'Pub_venue']
 
