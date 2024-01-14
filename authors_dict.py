843b669015a887f42d4277e90fe3dac68a77f4c4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-14 19:44:28 +0000

    f

--- authors_dict.py
+++ authors_dict.py
@@ -6,7 +6,7 @@ df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', 
 df_authors = df_authors.explode('Author_name')
 df_authors.reset_index(drop=True, inplace=True)
 df_authors = df_authors.dropna(subset=['FirstName2'])
-name_replacements = {
+name_replacements = { 
     'David Gioe': 'David V. Gioe',
     'David Vincent Gioe': 'David V. Gioe',
     'Michael Goodman': 'Michael S. Goodman',
