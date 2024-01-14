314450112cf8d323bce9f0f3eb072d01053db3ab
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-14 17:24:35 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -309,9 +309,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     include_abstracts = st.selectbox('🔍 options', ['In title','In title & abstract'])
                 with cola:
                     search_term = st.text_input('Search keywords in titles or abstracts')
-                if search_term and len(search_term) > 2:
-                    st.warning("Please enter at least two letters in the search box.")
-                elif search_term:
+                
+                if search_term:
                     with st.expander('Click to expand', expanded=True):
                         search_terms = re.findall(r'(?:"[^"]*"|\w+)', search_term)  # Updated regex pattern
                         phrase_filter = '|'.join(search_terms)  # Filter for the entire phrase
@@ -1100,7 +1099,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 ).transform_filter(
                     alt.datum.YearMonth % step == 0
                 )
-                st.subheader('Growth of the')
+                st.subheader('Growth of the library')
                 st.altair_chart(cumulative_chart + data_labels, use_container_width=True)
 
                 def format_entry(row):
