6de18744c74264a5c14e2154c9e2ada90560505c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-14 00:07:04 +0000

    s

--- pages/17_Home_without_API.py
+++ pages/17_Home_without_API.py
@@ -1137,6 +1137,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_collections = pd.read_csv('all_items_duplicated.csv')  
             df_collections = df_collections[['Collection_Name']]
             item_counts = df_collections['Collection_Name'].value_counts()
+            item_counts = item_counts[item_counts['Collection_Name']!='01 Intelligence history']
             number_of_collections  = st.slider('Select a number collections', 3,30,15)
             top_collections = item_counts.head(number_of_collections)
             st.dataframe(top_collections)
