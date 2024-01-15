7569b4ed75f8bdd3cffcb83d361da3eaa6b8a649
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-15 11:02:00 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1075,7 +1075,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 articles_list = [format_entry(row) for _, row in df_intro.iterrows()]
                 for index, formatted_entry in enumerate(articles_list):
                     st.write(f"{index + 1}) {formatted_entry}")
-                    if display:
+                    if display2:
                         st.caption(df_intro.iloc[index]['Abstract'])
 
             st.header('All items in database')
