253dca53c57c55337ac7b709de62f561ee531447
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-17 09:18:32 +0000

    e

--- Home.py
+++ Home.py
@@ -1171,7 +1171,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             collection_df = df_all.copy()
                             
                             publications_by_type = collection_df['Publication type'].value_counts()
-                            if abs(years[1]-years[0])>0 or years[0]>current_year:
+                            if abs(years[1]-years[0])>0 and years[0]>current_year:
                                 fig = px.bar(publications_by_type, x=publications_by_type.index, y=publications_by_type.values,
                                             labels={'x': 'Publication Type', 'y': 'Number of Publications'},
                                             title=f'Publications by Type between {int(years[0])} and {int(years[1])}')
