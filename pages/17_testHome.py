f495434b2a0f7507465e5bc7c2819f7d14398c51
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-17 21:38:45 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -605,34 +605,37 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         else:
                             if not on:  # If the toggle is off, display the publications
                                 with st.container(height=600):
-                                    for index, row in filtered_collection_df_authors.iterrows():
-                                        publication_type = row['Publication type']
-                                        title = row['Title']
-                                        authors = row['FirstName2']
-                                        date_published = row['Date published']
-                                        link_to_publication = row['Link to publication']
-                                        zotero_link = row['Zotero link']
+                                    if len(filtered_collection_df_authors) =! 0:
+                                        for index, row in filtered_collection_df_authors.iterrows():
+                                            publication_type = row['Publication type']
+                                            title = row['Title']
+                                            authors = row['FirstName2']
+                                            date_published = row['Date published']
+                                            link_to_publication = row['Link to publication']
+                                            zotero_link = row['Zotero link']
 
-                                        if publication_type == 'Journal article':
-                                            published_by_or_in = 'Published in'
-                                            published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-                                        elif publication_type == 'Book':
-                                            published_by_or_in = 'Published by'
-                                            published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
+                                            if publication_type == 'Journal article':
+                                                published_by_or_in = 'Published in'
+                                                published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
+                                            elif publication_type == 'Book':
+                                                published_by_or_in = 'Published by'
+                                                published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
+                                            else:
+                                                published_by_or_in = ''
+                                                published_source = ''
+
+                                            formatted_entry = (
+                                                '**' + str(publication_type) + '**' + ': ' +
+                                                str(title) + ' ' +
+                                                '(by ' + '*' + str(authors) + '*' + ') ' +
+                                                '(Publication date: ' + str(date_published) + ') ' +
+                                                ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
+                                                '[[Publication link]](' + str(link_to_publication) + ') ' +
+                                                '[[Zotero link]](' + str(zotero_link) + ')'
+                                            )
+                                            st.write(f"{index + 1}) {formatted_entry}")
                                         else:
-                                            published_by_or_in = ''
-                                            published_source = ''
-
-                                        formatted_entry = (
-                                            '**' + str(publication_type) + '**' + ': ' +
-                                            str(title) + ' ' +
-                                            '(by ' + '*' + str(authors) + '*' + ') ' +
-                                            '(Publication date: ' + str(date_published) + ') ' +
-                                            ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
-                                            '[[Publication link]](' + str(link_to_publication) + ') ' +
-                                            '[[Zotero link]](' + str(zotero_link) + ')'
-                                        )
-                                        st.write(f"{index + 1}) {formatted_entry}")
+                                            st.write('No publication type is selected.')
 
                             else:  # If toggle is on but no publications are available
                                 st.write("No publication type selected.")
