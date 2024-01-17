f09dc1696001262becb06767ed82b7c1da31228d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-17 22:54:55 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -319,8 +319,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         on = st.toggle('Generate dashboard')
 
                         if on and len(filtered_df) > 0:
-                            with st.container(height=600):
-                            # with st.expander('Click to expand', expanded=True):
+                            # with st.container(height=600):
+                            with st.expander('Click to expand', expanded=True):
                                 st.info(f'Dashboard for search terms: {phrase_filter}')
                                 search_df = filtered_df.copy()
                                 publications_by_type = search_df['Publication type'].value_counts()
