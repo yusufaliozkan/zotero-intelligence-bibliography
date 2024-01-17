85f743de18d0ae68a4771c664c8f8d410b7e5e37
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-17 07:55:42 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1161,21 +1161,103 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         st.write(f"**{number_of_items}** sources found published in **{int(years[0])}**")
                     else:
                         st.write(f"**{number_of_items}** sources found published between **{int(years[0])}** and **{int(years[1])}**")
+
+                    dashboard_all = st.toggle('Generate dashboard')
                     
-                    if number_of_items > 25:
-                        show_first_25 = st.checkbox("Show only first 25 items (untick to see all)", value=True, key='all_items')
-                        if show_first_25:
-                            df_all = df_all.head(25)
-                    articles_list = []  # Store articles in a list
-                    abstracts_list = [] #Store abstracts in a list
-                    for index, row in df_all.iterrows():
-                        formatted_entry = format_entry(row)
-                        articles_list.append(formatted_entry)  # Append formatted entry to the list
-                        abstract = row['Abstract']
-                        abstracts_list.append(abstract if pd.notnull(abstract) else 'N/A')
-                    for i, article in enumerate(articles_list, start=1):
-                        # Display the article with highlighted search terms
-                        st.markdown(f"{i}. {article}", unsafe_allow_html=True)
+                    if dashboard_all:
+                        st.write('Dashboard')
+                        if on and len(df_all) > 0: 
+                            st.info(f'Dashboard for items')
+                            collection_df = df_all.copy()
+                            
+                            publications_by_type = collection_df['Publication type'].value_counts()
+                            fig = px.bar(publications_by_type, x=publications_by_type.index, y=publications_by_type.values,
+                                        labels={'x': 'Publication Type', 'y': 'Number of Publications'},
+                                        title=f'Publications by Type')
+                            st.plotly_chart(fig)
+
+                            collection_df = df_all.copy()
+                            collection_df['Year'] = pd.to_datetime(collection_df['Date published']).dt.year
+                            publications_by_year = collection_df['Year'].value_counts().sort_index()
+                            fig_year_bar = px.bar(publications_by_year, x=publications_by_year.index, y=publications_by_year.values,
+                                                labels={'x': 'Publication Year', 'y': 'Number of Publications'},
+                                                title=f'Publications by Year')
+                            st.plotly_chart(fig_year_bar)
+                        
+                            collection_author_df = df_all.copy()
+                            collection_author_df['Author_name'] = collection_author_df['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+                            collection_author_df = collection_author_df.explode('Author_name')
+                            collection_author_df.reset_index(drop=True, inplace=True)
+                            collection_author_df['Author_name'] = collection_author_df['Author_name'].map(name_replacements).fillna(collection_author_df['Author_name'])
+                            collection_author_df = collection_author_df['Author_name'].value_counts().head(10)
+                            fig = px.bar(collection_author_df, x=collection_author_df.index, y=collection_author_df.values)
+                            fig.update_layout(
+                                title=f'Top 10 Authors by Publication Count',
+                                xaxis_title='Author',
+                                yaxis_title='Number of Publications',
+                                xaxis_tickangle=-45,
+                            )
+                            st.plotly_chart(fig)
+
+                            author_df = df_all.copy()
+                            def clean_text (text):
+                                text = text.lower() # lowercasing
+                                text = re.sub(r'[^\w\s]', ' ', text) # this removes punctuation
+                                text = re.sub('[0-9_]', ' ', text) # this removes numbers
+                                text = re.sub('[^a-z_]', ' ', text) # removing all characters except lowercase letters
+                                return text
+                            author_df['clean_title'] = author_df['Title'].apply(clean_text)
+                            author_df['clean_title'] = author_df['clean_title'].apply(lambda x: ' '.join ([w for w in x.split() if len (w)>2])) # this function removes words less than 2 words
+                            def tokenization(text):
+                                text = re.split('\W+', text)
+                                return text    
+                            author_df['token_title']=author_df['clean_title'].apply(tokenization)
+                            stopword = nltk.corpus.stopwords.words('english')
+                            SW = ['york', 'intelligence', 'security', 'pp', 'war','world', 'article', 'twitter', 'nan',
+                                'new', 'isbn', 'book', 'also', 'yet', 'matter', 'erratum', 'commentary', 'studies',
+                                'volume', 'paper', 'study', 'question', 'editorial', 'welcome', 'introduction', 'editorial', 'reader',
+                                'university', 'followed', 'particular', 'based', 'press', 'examine', 'show', 'may', 'result', 'explore',
+                                'examines', 'become', 'used', 'journal', 'london', 'review']
+                            stopword.extend(SW)
+                            def remove_stopwords(text):
+                                text = [i for i in text if i] # this part deals with getting rid of spaces as it treads as a string
+                                text = [word for word in text if word not in stopword] #keep the word if it is not in stopword
+                                return text
+                            author_df['stopword']=author_df['token_title'].apply(remove_stopwords)
+                            wn = nltk.WordNetLemmatizer()
+                            def lemmatizer(text):
+                                text = [wn.lemmatize(word) for word in text]
+                                return text
+                            author_df['lemma_title'] = author_df['stopword'].apply(lemmatizer)
+                            listdf = author_df['lemma_title']
+                            df_list = [item for sublist in listdf for item in sublist]
+                            string = pd.Series(df_list).str.cat(sep=' ')
+                            wordcloud_texts = string
+                            wordcloud_texts_str = str(wordcloud_texts)
+                            wordcloud = WordCloud(stopwords=stopword, width=1500, height=750, background_color='white', collocations=False, colormap='magma').generate(wordcloud_texts_str)
+                            plt.figure(figsize=(20,8))
+                            plt.axis('off')
+                            plt.title(f"Word Cloud for Titles in")
+                            plt.imshow(wordcloud)
+                            plt.axis("off")
+                            plt.show()
+                            st.set_option('deprecation.showPyplotGlobalUse', False)
+                            st.pyplot()
+                    else:
+                        if number_of_items > 25:
+                            show_first_25 = st.checkbox("Show only first 25 items (untick to see all)", value=True, key='all_items')
+                            if show_first_25:
+                                df_all = df_all.head(25)
+                        articles_list = []  # Store articles in a list
+                        abstracts_list = [] #Store abstracts in a list
+                        for index, row in df_all.iterrows():
+                            formatted_entry = format_entry(row)
+                            articles_list.append(formatted_entry)  # Append formatted entry to the list
+                            abstract = row['Abstract']
+                            abstracts_list.append(abstract if pd.notnull(abstract) else 'N/A')
+                        for i, article in enumerate(articles_list, start=1):
+                            # Display the article with highlighted search terms
+                            st.markdown(f"{i}. {article}", unsafe_allow_html=True)
                 else:
                     st.download_button('💾 Download all items', csv, (a+'.csv'), mime="text/csv", key='download-csv-2')
                     df_all_items
