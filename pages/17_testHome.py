cccfa68a6bf1e91f368ffe0bec0189f02eb4b68b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-17 14:57:38 +0000

    df

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -27,6 +27,7 @@ from PIL import Image, ImageDraw, ImageFilter
 import json
 from authors_dict import df_authors, name_replacements
 from copyright import display_custom_license
+from sidebar_content import sidebar_content
 
 # Connecting Zotero with API 
 library_id = '2514686'
@@ -197,37 +198,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
     image = 'https://images.pexels.com/photos/315918/pexels-photo-315918.png'
 
-    with st.sidebar:
-        st.image(image, width=150)
-        st.sidebar.markdown("# Intelligence studies network")
-        with st.expander('About'):
-            st.write('''This website lists secondary sources on intelligence studies and intelligence history.
-            The sources are originally listed in the [Intelligence bibliography Zotero library](https://www.zotero.org/groups/2514686/intelligence_bibliography).
-            This website uses [Zotero API](https://github.com/urschrei/pyzotero) to connect the *Intelligence bibliography Zotero group library*.
-            To see more details about the sources, please visit the group library [here](https://www.zotero.org/groups/2514686/intelligence_bibliography/library). 
-            If you need more information about Zotero, visit [this page](https://www.intelligencenetwork.org/zotero).
-            ''')
-            st.write('This website was built and is managed by [Yusuf Ozkan](https://www.kcl.ac.uk/people/yusuf-ali-ozkan) | [Twitter](https://twitter.com/yaliozkan) | [LinkedIn](https://www.linkedin.com/in/yusuf-ali-ozkan/) | [ORCID](https://orcid.org/0000-0002-3098-275X) | [GitHub](https://github.com/YusufAliOzkan)')
-            components.html(
-            """
-            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
-            src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
-            © 2022 All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
-            """
-            )
-        with st.expander('Source code'):
-            st.info('''
-            Source code of this app is available [here](https://github.com/YusufAliOzkan/zotero-intelligence-bibliography).
-            ''')
-        with st.expander('Disclaimer'):
-            st.warning('''
-            This website and the Intelligence bibliography Zotero group library do not list all the sources on intelligence studies. 
-            The list is created based on the creator's subjective views.
-            ''')
-        with st.expander('Contact us'):
-            st.write('If you have any questions or suggestions, please do get in touch with us by filling the form [here](https://www.intelligencenetwork.org/contact-us).')
-            st.write('Report your technical issues or requests [here](https://github.com/YusufAliOzkan/zotero-intelligence-bibliography/issues).')
-        st.write('See our dynamic [digest](https://intelligence.streamlit.app/Digest)')
+    sidebar_content()
 
     tab1, tab2, tab3 = st.tabs(['📑 Publications', '📊 Dashboard', '🔀 Surprise me'])
     with tab1:
