973c00189a02309d7e4920969e19fbbe9f45ce5c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-20 13:49:09 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -991,22 +991,20 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     )
                 with st.expander('Click to expand', expanded=True):                    
                     df_all = pd.read_csv('all_items.csv')
-                    df_all = df_all.drop_duplicates()
-                    df_all['Date published'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
-                    df_all['Date published'] = df_all['Date published'].dt.strftime('%Y-%m-%d')
-                    # df_all['Date year'] = df_all['Date published'].dt.strftime('%Y')
+                    df_all['Date published2'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
+                    df_all['Date year'] = df_all['Date published2'].dt.strftime('%Y')
                     df_all['Date year'] = pd.to_numeric(df_all['Date year'], errors='coerce', downcast='integer')
                     numeric_years = df_all['Date year'].dropna()
                     current_year = date.today().year
                     min_y = numeric_years.min()
                     max_y = numeric_years.max()
 
-                    # df_all['Date published'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
-                    # df_all['Date published'] = df_all['Date published'].dt.strftime('%Y-%m-%d')
-                    # df_all['Date published'] = df_all['Date published'].fillna('')
-                    # df_all['No date flag'] = df_all['Date published'].isnull().astype(np.uint8)
-                    # df_all = df_all.sort_values(by=['No date flag', 'Date published'], ascending=[True, True])
-                    # df_all = df_all.sort_values(by=['Date published'], ascending=False)
+                    df_all['Date published'] = pd.to_datetime(df_all['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
+                    df_all['Date published'] = df_all['Date published'].dt.strftime('%Y-%m-%d')
+                    df_all['Date published'] = df_all['Date published'].fillna('')
+                    df_all['No date flag'] = df_all['Date published'].isnull().astype(np.uint8)
+                    df_all = df_all.sort_values(by=['No date flag', 'Date published'], ascending=[True, True])
+                    df_all = df_all.sort_values(by=['Date published'], ascending=False)
                     df_all
 
                     current_year = date.today().year
