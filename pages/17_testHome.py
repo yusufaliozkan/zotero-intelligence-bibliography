3e139cdbed1d4c83caae8b51ec589a179a409aff
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-22 16:18:32 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1483,7 +1483,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     df_collections_2 = df_collections_2[df_collections_2['Publication type'].isin(types)]
                     df_collections_2 = df_collections_2[df_collections_2['Date year'] !='No date']
                     filter_collection = (df_collections_2['Date year'].astype(int)>=years[0]) & (df_collections_2['Date year'].astype(int)<years[1])
-                    df_collections_2 df_collections_2.loc[filter_collection]
+                    df_collections_2 = df_collections_2.loc[filter_collection]
 
 
 
