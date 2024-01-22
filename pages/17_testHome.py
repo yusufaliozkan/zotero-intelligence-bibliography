53ba33e60b8c0dd846028742f0e3f3d3b51c1530
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-22 15:54:27 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1537,12 +1537,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
             with col2:
                 df_csv['Author_name'] = df_csv['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
-                df_csv
-                collection_author_df = collection_author_df.explode('Author_name')
-                collection_author_df.reset_index(drop=True, inplace=True)
-                collection_author_df['Author_name'] = collection_author_df['Author_name'].map(name_replacements).fillna(collection_author_df['Author_name'])
-                collection_author_df = collection_author_df['Author_name'].value_counts().head(10)
-                fig = px.bar(collection_author_df, x=collection_author_df.index, y=collection_author_df.values)
+                df_csv = df_csv.explode('Author_name')
+                df_csv.reset_index(drop=True, inplace=True)
+                df_csv['Author_name'] = df_csv['Author_name'].map(name_replacements).fillna(df_csv['Author_name'])
+                df_csv = df_csv['Author_name'].value_counts().head(10)
+                fig = px.bar(df_csv, x=df_csv.index, y=df_csv.values)
                 fig.update_layout(
                     title=f'Top 10 Authors by Publication Count ({selected_collection})',
                     xaxis_title='Author',
