f4923f9a3c2d770787dace4a4c92a2feab677296
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-22 15:53:29 +0000

    ds

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1536,9 +1536,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 col1.plotly_chart(fig, use_container_width = True)
 
             with col2:
-            df_csv
-            df_csv['Author_name'] = df_csv['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
-            df_csv
+                df_csv['Author_name'] = df_csv['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+                df_csv
                             collection_author_df = collection_author_df.explode('Author_name')
                             collection_author_df.reset_index(drop=True, inplace=True)
                             collection_author_df['Author_name'] = collection_author_df['Author_name'].map(name_replacements).fillna(collection_author_df['Author_name'])
