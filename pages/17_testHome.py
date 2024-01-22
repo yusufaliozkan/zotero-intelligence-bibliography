4bef96fd02b93bfbfe262ebe8c2319bab758ba19
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-22 23:38:24 +0000

    ds

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1450,8 +1450,10 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_collections_2 =df_csv.copy()
 
             df_csv = df_csv.drop_duplicates(subset=['Zotero link'], keep='first')
+            df_csv.reset_index(drop=True)
             df_csv
             df_csv2 = pd.read_csv('all_items.csv')
+            df_csv2.reset_index(drop=True)
             df_csv2
             df_csv = df_csv.reset_index(drop=True)
             df_csv['Date published'] = pd.to_datetime(df_csv['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
