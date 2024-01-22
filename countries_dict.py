78903c31065ad5b9c90e4b42c6da0e22f8fce45d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-22 11:14:56 +0000

    ds

--- countries_dict.py
+++ countries_dict.py
@@ -162,7 +162,7 @@ continent_replacements = {
     'Christofascism':'Romania',
     'Abdülhamid':'Turkey',
     'Czechoslovakia':'Czechia',
-    'Czechoslovakia':'Czechia'
+    'Czechoslovakia':'Czechia',
     'Third Reich':'Germany',
     'Kriegsmarine':'Germany',
     'Bundesnachrichtendienst':'Germany',
