04324ecb6b14cfc73bb158d8ca3b293e7527fa7e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-23 09:05:30 +0000

    ew

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1556,14 +1556,15 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 col1.plotly_chart(fig, use_container_width = True)
 
             with col2:
-                df_csv['Author_name'] = df_csv['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
-                df_csv = df_csv.explode('Author_name')
-                df_csv.reset_index(drop=True, inplace=True)
-                max_authors = len(df_csv['Author_name'].unique())
+                df_authors = df_csv.copy()
+                df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+                df_authors = df_authors.explode('Author_name')
+                df_authors.reset_index(drop=True, inplace=True)
+                max_authors = len(df_authors['Author_name'].unique())
                 num_authors = st.slider('Select number of authors to display:', 5, min(30, max_authors), 20)
-                df_csv['Author_name'] = df_csv['Author_name'].map(name_replacements).fillna(df_csv['Author_name'])
-                df_csv_authors = df_csv['Author_name'].value_counts().head(num_authors)
-                fig = px.bar(df_csv_authors, x=df_csv_authors.index, y=df_csv_authors.values)
+                df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_csv['Author_name'])
+                df_authors = df_authors['Author_name'].value_counts().head(num_authors)
+                fig = px.bar(df_authors, x=df_authors.index, y=df_authors.values)
                 fig.update_layout(
                     title=f'Top {num_authors} Authors by Publication Count',
                     xaxis_title='Author',
