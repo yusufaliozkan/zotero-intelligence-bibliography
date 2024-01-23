bdcefbad868b23c697c1a09bea5decc62fc6f273
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-23 16:37:26 +0000

    rt

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -966,6 +966,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 journals = st.multiselect('Select a journal', unique_journals_sorted)     
                 selected_journal = df_csv[df_csv['Journal'].isin(journals)]
                 selected_journal_df = st.multiselect('Select a publication type', selected_journal)
+                selected_journal
 
                 selected_journal_df['Date published'] = (
                     selected_journal_df['Date published']
