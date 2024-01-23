17c5448d5dfd8630e6f74e94d0fced798ff513d2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-23 12:03:24 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1478,7 +1478,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
          
             with st.expander('Select parameters', expanded=True):
-                unique_types =  [''] + ['All'] + list(unique_countries)
                 types = st.multiselect('Publication type', df_csv['Publication type'].unique(), df_csv['Publication type'].unique())
                 years = st.slider('Publication years between:', min_y, max_y+1, (min_y,max_y+1), key='years2')
                 # if st.button('Update dashboard'):
