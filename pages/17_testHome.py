c0563b5037635976ad5d408622462cbafea39b7a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-23 09:05:54 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1562,7 +1562,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_authors.reset_index(drop=True, inplace=True)
                 max_authors = len(df_authors['Author_name'].unique())
                 num_authors = st.slider('Select number of authors to display:', 5, min(30, max_authors), 20)
-                df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_csv['Author_name'])
+                df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
                 df_authors = df_authors['Author_name'].value_counts().head(num_authors)
                 fig = px.bar(df_authors, x=df_authors.index, y=df_authors.values)
                 fig.update_layout(
