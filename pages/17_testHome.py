361db00caebfe6aa6ad41766c01155cab474eac1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-24 14:01:54 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1630,7 +1630,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_journals = df_journals[df_journals['Publication type'] == 'Journal article']
                 journal_counts = df_journals['Journal'].value_counts()
                 unique_journals_sorted = journal_counts.index.tolist()
-                journals = st.multiselect('Select a journal', unique_journals_sorted)                 
+                journals = st.multiselect('Select a journal', unique_journals_sorted, key='big_dashboard_journals')                 
 
                 years = st.slider('Publication years between:', min_y, max_y+1, (min_y,max_y+1), key='years2')
                 if st.button('Update dashboard'):
