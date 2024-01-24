ead6d1191d6b5a727dd97776ea0f78947e5c761b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-24 15:11:13 +0000

    s

--- countries_dict.py
+++ countries_dict.py
@@ -30,6 +30,7 @@ country_names = [
     'Vichy Regime','Safavids','Hungarian','Austro','Swedish','Nasser','Jewish','Finnish', 'Christofascism','Venice', 'Abdülhamid','Czechoslovakia', 'Third Reich', 'Kriegsmarine',
     'Bundesnachrichtendienst','Vappala Balachandran', 'Andropov', 'Kosova', 'Yom Kippur', "The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East",
     "The Politics and Intelligence of the Oslo Peace Process", 'Shin bet','Sixteenth-Century Istanbul', 'Cheka','Arabizing the Omani intelligence services: Clash of cultures',
+    'Putin'
 ]
 replacements = {
     'Arabizing the Omani intelligence services: Clash of cultures':'Oman',
@@ -82,6 +83,7 @@ replacements = {
     'Shin bet':'Israel',
     'Sixteenth-Century Istanbul':'Turkey',
     'Cheka':'Russia',
+    'Putin':'Russia'
     }
 
 replacements['\\bOttoman\\b'] = 'Turkey'
@@ -134,7 +136,7 @@ continent_country_names = [
     'Vichy Regime','Safavids','Hungarian','Austro','Swedish','Nasser','Jewish','Finnish','Africa','Asia','Arab','South America','Medina','Eastern Europe',
     'Latin America','Venice', 'Christofascism','Abdülhamid','Czechoslovakia', 'Third Reich','Kriegsmarine','Bundesnachrichtendienst', 'Vappala Balachandran','Andropov',
     'Kosova', 'Yom Kippur', 'The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East', "The Politics and Intelligence of the Oslo Peace Process",
-    'Shin bet','Sixteenth-Century Istanbul','Cheka', 'Omani'
+    'Shin bet','Sixteenth-Century Istanbul','Cheka', 'Omani','Putin'
 ]
 continent_replacements = {
     'Belgian': 'Belgium', 
@@ -187,7 +189,8 @@ continent_replacements = {
     'Shin bet':'Israel',
     'Sixteenth-Century Istanbul':'Turkey',
     'Cheka':'Russia',
-     'Omani':'Oman'
+     'Omani':'Oman',
+     'Putin':'Russia'
     } 
 
 df_continent = df_countries.copy()
