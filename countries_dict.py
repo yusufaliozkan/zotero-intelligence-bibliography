bb60c4cd9825959a48b84eb15e69f068f63b8bd3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-24 08:10:51 +0000

    sd

--- countries_dict.py
+++ countries_dict.py
@@ -29,7 +29,7 @@ country_names = [
     'FSB', 'Dutch', 'German', 'Mossad', 'Norwegian', 'Ottoman', 'Italian', 'Teşkilat-ı Mahsusa', 'Tsar', 'ACADEMIC INTELLIGENCE – A PLURIVALENT CONCEPT',
     'Vichy Regime','Safavids','Hungarian','Austro','Swedish','Nasser','Jewish','Finnish', 'Christofascism','Venice', 'Abdülhamid','Czechoslovakia', 'Third Reich', 'Kriegsmarine',
     'Bundesnachrichtendienst','Vappala Balachandran', 'Andropov', 'Kosova', 'Yom Kippur', "The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East",
-    "The Politics and Intelligence of the Oslo Peace Process", 'Shin bet'
+    "The Politics and Intelligence of the Oslo Peace Process", 'Shin bet','Sixteenth-Century Istanbul'
 ]
 replacements = { 
     'Belgian': 'Belgium',
@@ -78,7 +78,8 @@ replacements = {
     'Yom Kippur':'Israel',
     "The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East":"Israel",
     "The Politics and Intelligence of the Oslo Peace Process":'Israel',
-    'Shin bet':'Israel'
+    'Shin bet':'Israel',
+    'Sixteenth-Century Istanbul':'Turkey'
     }
 
 replacements['\\bOttoman\\b'] = 'Turkey'
@@ -127,7 +128,7 @@ continent_country_names = [
     'Vichy Regime','Safavids','Hungarian','Austro','Swedish','Nasser','Jewish','Finnish','Africa','Asia','Arab','South America','Medina','Eastern Europe',
     'Latin America','Venice', 'Christofascism','Abdülhamid','Czechoslovakia', 'Third Reich','Kriegsmarine','Bundesnachrichtendienst', 'Vappala Balachandran','Andropov',
     'Kosova', 'Yom Kippur', 'The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East', "The Politics and Intelligence of the Oslo Peace Process",
-    'Shin bet'
+    'Shin bet','Sixteenth-Century Istanbul'
 ]
 continent_replacements = {
     'Belgian': 'Belgium', 
@@ -177,7 +178,8 @@ continent_replacements = {
     'Yom Kippur':'Israel',
     "The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East":"Israel",
     "The Politics and Intelligence of the Oslo Peace Process":'Israel',
-    'Shin bet':'Israel'
+    'Shin bet':'Israel',
+    'Sixteenth-Century Istanbul':'Turkey'
     } 
 
 df_continent = df_countries.copy()
