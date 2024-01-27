f4bb5ba0b269f93f4f39ca36b4e72ac6b0fdac52
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-27 16:50:48 +0000

    ds

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1683,7 +1683,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 st.plotly_chart(fig, use_container_width = True)
 
                 df_collections_22 = df_collections_2.copy()
-                df_collections_22 = df_collections_22.head(number0+1)
                 collection_counts = df_collections_22.groupby(['Date year', 'Collection_Name']).size().unstack().fillna(0)
                 collection_counts = collection_counts.reset_index()
                 collection_counts
