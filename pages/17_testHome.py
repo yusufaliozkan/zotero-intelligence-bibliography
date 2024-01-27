0f207abc7fbc0b85941026b21898cb4974166f71
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-27 09:48:50 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1679,6 +1679,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 fig.update_layout(title={'text':'Top ' + str(number0) + ' collections in the library', 'y':0.95, 'x':0.4, 'yanchor':'top'})
                 st.plotly_chart(fig, use_container_width = True)
 
+                collection_counts = df_collections_2.groupby(['Date year', 'Collection_Name']).size().unstack().fillna(0)
+                collection_counts
+
 
                 # PUBLICATION TYPES
                 df_types = pd.DataFrame(df_csv['Publication type'].value_counts())
