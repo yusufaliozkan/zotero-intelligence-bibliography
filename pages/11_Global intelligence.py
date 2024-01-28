e6b534f5525afd43395e1d313a7ab480c3dffc20
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-28 08:21:07 +0000

    ds

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -96,7 +96,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             collection_link = df_collections[df_collections['Collection_Name'] == collection_name]['Collection_Link'].iloc[0] 
 
             st.markdown('#### Collection theme: ' + collection_name)
-            container_info = container.info(f"See the collection in [Zotero]({collection_link}) from which you can easily generate citations.")
+            container_info = container_info.info(f"See the collection in [Zotero]({collection_link}) from which you can easily generate citations.")
             st.write('This collection lists academic sources that are **non-UK/US** on intelligence.')
 
             df_countries_chart = df_countries.copy()
