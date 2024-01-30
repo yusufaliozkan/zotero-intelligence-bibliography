18520fe50b40266fa94f337bf16cbe17819153ed
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-30 14:37:28 +0000

    tg

--- Home.py
+++ Home.py
@@ -1422,7 +1422,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_intro = df_intro.sort_values(by=['No date flag', 'Date published'], ascending=[True, True])
                 df_intro = df_intro.sort_values(by=['Date published'], ascending=False)
                 df_intro = df_intro.reset_index(drop=True)
-                df_intro = df_intro.head(10)
+                df_intro = df_intro.head(5)
                 articles_list = [format_entry(row) for _, row in df_intro.iterrows()]
                 for index, formatted_entry in enumerate(articles_list):
                     st.write(f"{index + 1}) {formatted_entry}")
