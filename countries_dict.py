fcd8923727a968f558de41dd4f64fd64a24342f1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-30 12:46:15 +0000

    sd

--- countries_dict.py
+++ countries_dict.py
@@ -31,7 +31,7 @@ country_names = [
     'Bundesnachrichtendienst','Vappala Balachandran', 'Andropov', 'Kosova', 'Yom Kippur', "The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East",
     "The Politics and Intelligence of the Oslo Peace Process", 'Shin bet','Sixteenth-Century Istanbul', 'Cheka','Arabizing the Omani intelligence services: Clash of cultures',
     'Putin', 'New memoirs from Moscow', "Mao's Secret", 'Shedding light on the rising Sun','The nonuse of intelligence', 'Moshe Dayan','Scandinavia','Andropov', 'Saddam', 'NKVD',
-    'Dezinformatsiya','Abwehr','Basque','Stasi','Republic of Korea','Napoleon','Brussels','Deuxième Bureau','Franco','Bundeswehr','Sino','Malayan'
+    'Dezinformatsiya','Abwehr','Basque','Stasi','Republic of Korea','Napoleon','Brussels','Deuxième Bureau','Franco','Bundeswehr','Sino','Malayan','Irish'
 ]
 replacements = {
     'Arabizing the Omani intelligence services: Clash of cultures':'Oman',
@@ -107,7 +107,8 @@ replacements = {
     'Franco':'France',
     'Bundeswehr':'Germany',
     'Sino':'China',
-    'Malayan':'Malaysia'
+    'Malayan':'Malaysia',
+    'Irish':'Ireland'
     }
 
 replacements['\\bOttoman\\b'] = 'Turkey'
@@ -165,7 +166,7 @@ continent_country_names = [
     'Kosova', 'Yom Kippur', 'The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East', "The Politics and Intelligence of the Oslo Peace Process",
     'Shin bet','Sixteenth-Century Istanbul','Cheka', 'Omani','Putin','New memoirs from Moscow',"Mao's Secret", 'Shedding light on the rising Sun','The nonuse of intelligence',
     'Moshe Dayan', 'Scandinavia', 'Andropov', 'Saddam', 'NKVD','Dezinformatsiya','Abwehr','Basque','Stasi','Republic of Korea','Czech','Napoleon','Brussels','Deuxième Bureau',
-    'Franco', 'Bundeswehr','Sino','Malayan'
+    'Franco', 'Bundeswehr','Sino','Malayan','Irish'
 ]
 continent_replacements = { 
     'Belgian': 'Belgium', 
@@ -242,7 +243,8 @@ continent_replacements = {
     'Franco':'France',
     'Bundeswehr':'Germany',
     'Sino':'China',
-    'Malayan':'Malaysia'
+    'Malayan':'Malaysia',
+    'Irish':'Ireland'
     }
 
 df_continent = df_countries.copy()
