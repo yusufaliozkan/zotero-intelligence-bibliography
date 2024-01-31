7e489dd5e32e05f1a25c37913741802b98ad305c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-01-31 21:04:02 +0000

    sd

--- countries_dict.py
+++ countries_dict.py
@@ -32,7 +32,7 @@ country_names = [
     "The Politics and Intelligence of the Oslo Peace Process", 'Shin bet','Sixteenth-Century Istanbul', 'Cheka','Arabizing the Omani intelligence services: Clash of cultures',
     'Putin', 'New memoirs from Moscow', "Mao's Secret", 'Shedding light on the rising Sun','The nonuse of intelligence', 'Moshe Dayan','Scandinavia','Andropov', 'Saddam', 'NKVD',
     'Dezinformatsiya','Abwehr','Basque','Stasi','Republic of Korea','Napoleon','Brussels','Deuxième Bureau','Franco','Bundeswehr','Sino','Malayan','Irish','Hitler',
-    'Socratic'
+    'Socratic','R&AW'
 ]
 replacements = {
     'Arabizing the Omani intelligence services: Clash of cultures':'Oman',
@@ -111,7 +111,8 @@ replacements = {
     'Malayan':'Malaysia',
     'Irish':'Ireland',
     'Hitler':'Germany',
-    'Socratic':'Greece'
+    'Socratic':'Greece',
+    'R&AW':'India'
     }
 
 replacements['\\bOttoman\\b'] = 'Turkey'
@@ -169,7 +170,7 @@ continent_country_names = [
     'Kosova', 'Yom Kippur', 'The Spy who Fell to Earth: My Relationship with the Secret Agent who Rocked the Middle East', "The Politics and Intelligence of the Oslo Peace Process",
     'Shin bet','Sixteenth-Century Istanbul','Cheka', 'Omani','Putin','New memoirs from Moscow',"Mao's Secret", 'Shedding light on the rising Sun','The nonuse of intelligence',
     'Moshe Dayan', 'Scandinavia', 'Andropov', 'Saddam', 'NKVD','Dezinformatsiya','Abwehr','Basque','Stasi','Republic of Korea','Czech','Napoleon','Brussels','Deuxième Bureau',
-    'Franco', 'Bundeswehr','Sino','Malayan','Irish','Hitler','Socratic'
+    'Franco', 'Bundeswehr','Sino','Malayan','Irish','Hitler','Socratic','R&AW'
 ]
 continent_replacements = { 
     'Belgian': 'Belgium', 
@@ -249,7 +250,8 @@ continent_replacements = {
     'Malayan':'Malaysia',
     'Irish':'Ireland',
     'Hitler':'Germany',
-    'Socratic':'Greece'
+    'Socratic':'Greece',
+    'R&AW':'India'
     } 
 
 df_continent = df_countries.copy()
