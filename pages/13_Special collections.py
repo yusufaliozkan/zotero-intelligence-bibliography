e60772eae78866f84c955b379a2d7e64178dfc0b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-02 23:44:28 +0000

    sd

--- pages/13_Special collections.py
+++ pages/13_Special collections.py
@@ -106,7 +106,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
             st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
             a = f'{collection_name}_{today}'
-            st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
+            st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4') 
 
             with st.expander('Click to expand', expanded=True):
                 def format_entry(row):
