a70bf1d6198bdcdf95a45493c1a9e273191d2558
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-03 09:07:57 +0000

    fd

--- Home.py
+++ Home.py
@@ -982,7 +982,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 else:
                     selected_journal_df = df_csv[df_csv['Journal'].isin(journals)]
 
-                    selected_journal_df['Date published'] = pd.to_datetime(selected_journal_df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
+                    selected_journal_df['Date published'] = (
+                        selected_journal_df['Date published']
+                        .str.strip()
+                        .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
+                    )
+                    # selected_journal_df['Date published'] = pd.to_datetime(selected_journal_df['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
                     selected_journal_df['Date published'] = selected_journal_df['Date published'].dt.strftime('%Y-%m-%d')
                     selected_journal_df['Date published'] = selected_journal_df['Date published'].fillna('')
                     selected_journal_df['No date flag'] = selected_journal_df['Date published'].isnull().astype(np.uint8)
