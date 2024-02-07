2cba4c9bbfe4c92ea8046fc26f1078ae6e7f28e3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-07 12:06:31 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -104,7 +104,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_continent = df_continent.copy()
             df_continent_chart = df_continent.copy() 
 
-#             unique_items_count = df_countries_chart['Country'].nunique()
+            unique_items_count = df_countries_chart['Country'].nunique()
 #             num_items_collections = len(df_collections)
 #             # st.write(f"**{num_items_collections}** sources found for **{unique_items_count-1}** countries.")
 #             container_metric = container_metric.metric(label='Number of items in this collection', value=num_items_collections, help=f'sources found for **{unique_items_count-1}** countries.')
