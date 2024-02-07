d9096d9071b19608cb6610eebf34f927bae32205
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-07 13:43:03 +0000

    ds

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -121,7 +121,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_countries = df_countries.sort_values(by=['No date flag', 'Date published'], ascending=[True, True])
             df_countries = df_countries.sort_values(by=['Date published'], ascending=False)
             df_countries = df_countries.drop_duplicates(subset=['Country', 'Zotero link'])
-#             df_countries = df_countries.reset_index(drop=True)
+            df_countries = df_countries.reset_index(drop=True)
 #             unique_countries = sorted(df_countries['Country'].unique())
 #             unique_countries =  [''] + ['All Countries'] + list(unique_countries)  # Added 'All Countries' option
 #             selected_country = st.selectbox('Select a Country', unique_countries)
