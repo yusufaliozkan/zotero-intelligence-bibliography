07184f699fde0720b22f99ccf08ef1579542622d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-07 11:45:27 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -69,7 +69,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     df_collections = df_collections[df_collections['Collection_Name'].str.contains("14.")]
     df_collections
 
-#     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     
 #     # container = st.container()
 
