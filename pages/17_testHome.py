18c8dc964284135f3d5b1c8a7531b6b2afc5dde3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-13 16:15:52 +0000

    df

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1517,6 +1517,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_intro = df_intro.reset_index(drop=True)
                 df_intro = df_intro.head(5)
                 articles_list = [format_entry(row) for _, row in df_intro.iterrows()]
+                articles_list = [format_entry(row, include_citation=False) for _, row in df_intro.iterrows()]
                 for index, formatted_entry in enumerate(articles_list):
                     st.write(f"{index + 1}) {formatted_entry}")
                     if display2:
