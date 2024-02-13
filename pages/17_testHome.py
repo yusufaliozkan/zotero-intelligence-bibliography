716f6c884d7f8c46f29fe86f8b532eacd1ddfcab
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-13 12:32:32 +0000

    df

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -695,6 +695,10 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         num_items_collections = len(filtered_collection_df)
                         breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
                         st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
+
+                        citation_count = filtered_collection_df['Citation'].value_counts()
+                        citation_count
+
                         a = f'{selected_collection}_{today}'
                         st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
 
