ea3406b791a2bd519c6adca21b8b0f346f42576e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-13 12:10:32 +0000

    s

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1144,6 +1144,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     zotero_link = str(row['Zotero link']) if pd.notnull(row['Zotero link']) else ''
                     published_by_or_in = ''
                     published_source = ''
+                    citation = str(row['Citation']) if pd.notnull(row['Citation']) else '0'  
+                    citation = int(float(citation))
 
                     published_by_or_in_dict = {
                         'Journal article': 'Published in',
@@ -1166,7 +1168,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         '(Publication date: ' + str(date_published) + ') ' +
                         ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
                         '[[Publication link]](' + link_to_publication + ') ' +
-                        '[[Zotero link]](' + zotero_link + ')'
+                        '[[Zotero link]](' + zotero_link + '), ' + 'Cited by ' +  str(citation)
                     )
                 with st.expander('Click to expand', expanded=True):                    
                     df_all = pd.read_csv('all_items.csv') 
