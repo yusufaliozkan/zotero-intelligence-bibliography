0383e8c64136aed24a6de74e8e91c4f7b4c97cd0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-14 23:25:42 +0000

    ds

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1491,7 +1491,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     )
                 with st.expander('Click to expand', expanded=True):                    
                     df_cited = pd.read_csv('all_items.csv')
-                    df_cited
+                    non_nan_id = df_cited['ID'].count()
+                    non_nan_id
                     df_cited = df_cited[(df_cited['Citation'].notna()) & (df_cited['Citation'] != 0)]
                     df_cited = df_cited.reset_index(drop=True)
                     df_cited['Date published2'] = (
