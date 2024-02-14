43a7883b0907a8472c78c000f7cd9c4830848118
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-14 23:12:00 +0000

    df

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1574,7 +1574,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             author_citations = collection_df.groupby('Author_name')['Citation'].sum().reset_index()
                             author_citations = author_citations.sort_values(by='Citation', ascending=False)
                             fig = px.bar(author_citations.head(10), x='Author_name', y='Citation',
-                                        title=f'Top 10 Authors by Citation Count ({selected_collection})',
+                                        title=f'Top 10 Authors by Citation Count',
                                         labels={'Citation': 'Number of Citations', 'Author_name': 'Author'})
                             fig.update_layout(xaxis_tickangle=-45)
                             st.plotly_chart(fig)
