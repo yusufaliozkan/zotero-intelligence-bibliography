05f3babeffcb759d7ebdf4bcfd9c31340a0f2836
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-14 22:27:50 +0000

    ui

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -169,7 +169,8 @@ Welcome to **Intelligence studies bibliography**.
 This website lists **hundreds of sources, events, conferences, and call for papers** on intelligence history and intelligence studies. Finding sources on intelligence
 can sometimes be challening. This bibliography will be your helpful research assistant in finding and saving sources.
 
-Check out the [short guide](https://medium.com/@yaliozkan/introduction-to-intelligence-studies-network-ed63461d1353) for a quick intoduction about the website.
+Check out the following guides for a quick intoduction about the website:
+
 Ozkan, Yusuf Ali. ‘Enhancing the “Intelligence Studies Network” Website’. Medium (blog), 20 January 2024. https://medium.com/@yaliozkan/enhancing-the-intelligence-studies-network-website-13aa0c80f7f4.
 ———. ‘Introduction to “Intelligence Studies Bibliography”’. Medium (blog), 26 December 2023. https://medium.com/@yaliozkan/introduction-to-intelligence-studies-network-ed63461d1353.
 
