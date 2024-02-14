cb6e5fe83be9db5cb2e9114bc3f616d73d2e7fb8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-14 23:19:15 +0000

    ds

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1618,10 +1618,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             wordcloud = WordCloud(stopwords=stopword, width=1500, height=750, background_color='white', collocations=False, colormap='magma').generate(wordcloud_texts_str)
                             plt.figure(figsize=(20,8))
                             plt.axis('off')
-                            if abs(years[1]-years[0])>0 and years[0]<current_year:
-                                plt.title(f"Word Cloud for Titles between {int(years[0])} and {int(years[1])}")
-                            else:
-                                plt.title(f"Word Cloud for Titles in {int(years[0])}")
+                            plt.title(f"Word Cloud for cited papers")
                             plt.imshow(wordcloud)
                             plt.axis("off")
                             plt.show()
