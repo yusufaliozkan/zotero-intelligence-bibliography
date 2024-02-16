357157681f13bee1645807c42b1c5bceb6f497f3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-16 23:32:04 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1530,6 +1530,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         df_cited = df_cited[df_cited['Publication type'].isin(selected_type)]
                     
                     df_cited = df_cited.reset_index(drop=True)
+                    df_cited
 
                     df_cited_download = df_cited.copy()
                     df_cited_download = df_cited_download[['Publication type', 'Title', 'Abstract', 'FirstName2', 'Link to publication', 'Zotero link', 'Date published', 'Citation']]
@@ -1569,7 +1570,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     if dashboard_all:
                         if dashboard_all and len(df_cited) > 0: 
                             st.info(f'Dashboard for cited items in the library')
-                            df_cited          
 
                             citation_distribution = df_cited['Citation'].value_counts().sort_index().reset_index()
                             citation_distribution.columns = ['Number of Citations', 'Number of Articles']
