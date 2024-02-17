b6f38b61be173f6fbf8e4daf539ded0d11e42a85
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-17 23:46:58 +0000

    yt

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -798,7 +798,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         else:
                             if not on:
                                 sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication date :arrow_up:', 'Citation'))
-                                if sort_by == 'Publication date :arrow_down:' or filtered_collection_df['Citation'].sum() == 0:
+                                if sort_by == 'Publication date :arrow_down:':
                                     filtered_collection_df = filtered_collection_df.sort_values(by=['Date published'], ascending=False)
                                     filtered_collection_df = filtered_collection_df.reset_index(drop=True)
                                 elif sort_by == 'Publication date :arrow_up:' or filtered_collection_df['Citation'].sum() == 0:
