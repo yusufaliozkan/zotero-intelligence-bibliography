7646e747a5d89bcfca4c300e224729be91a530e7
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-17 23:48:11 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -600,9 +600,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             st.pyplot()
                         else:
                             if not on:  # If the toggle is off, display the publications
-                                sort_by = st.radio('Sort by:', ('Publication date', 'Citation'))
-                                if sort_by == 'Publication date' or filtered_collection_df_authors['Citation'].sum() == 0:
+                                sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication date :arrow_up:', 'Citation'))
+                                if sort_by == 'Publication date :arrow_down:' or filtered_collection_df_authors['Citation'].sum() == 0:
                                     filtered_collection_df_authors = filtered_collection_df_authors.sort_values(by=['Date published'], ascending=False)
+                                    filtered_collection_df_authors =filtered_collection_df_authors.reset_index(drop=True)
+                                elif sort_by == 'Publication date :arrow_up:' or filtered_collection_df_authors['Citation'].sum() == 0:
+                                    filtered_collection_df_authors = filtered_collection_df_authors.sort_values(by=['Date published'], ascending=True)
                                     filtered_collection_df_authors =filtered_collection_df_authors.reset_index(drop=True)
                                 else:
                                     filtered_collection_df_authors = filtered_collection_df_authors.sort_values(by=['Citation'], ascending=False)
