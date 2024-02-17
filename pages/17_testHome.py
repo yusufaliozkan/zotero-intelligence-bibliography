69bd1421d48dc07ace4f5e7fdd7261e1e26da939
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-17 23:50:05 +0000

    sd

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -416,9 +416,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 st.pyplot()
 
                             else:
-                                sort_by = st.radio('Sort by:', ('Publication date', 'Citation'))
-                                if sort_by == 'Publication date' or filtered_df['Citation'].sum() == 0:
+                                sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication date :arrow_up:', 'Citation'))
+                                if sort_by == 'Publication date :arrow_down:' or filtered_df['Citation'].sum() == 0:
                                     filtered_df = filtered_df.sort_values(by=['Date published'], ascending=False)
+                                    filtered_df = filtered_df.reset_index(drop=True)
+                                elif sort_by == 'Publication date :arrow_up:' or filtered_df['Citation'].sum() == 0:
+                                    filtered_df = filtered_df.sort_values(by=['Date published'], ascending=True)
                                     filtered_df = filtered_df.reset_index(drop=True)
                                 else:
                                     filtered_df = filtered_df.sort_values(by=['Citation'], ascending=False)
