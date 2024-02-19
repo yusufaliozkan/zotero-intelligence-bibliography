2e1a7c85a72c2524c565ca993da3593039e675f6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-19 00:03:32 +0000

    d

--- pages/17_testHome.py
+++ pages/17_testHome.py
@@ -1514,8 +1514,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         current_year
                         note = st.info(f'''
                         The trends section shows the citations occured in the last two years to the papers published in the same period. 
-                        
-                        Citations occured in {current_year}-{current_year - 1} to papers published in {current_year}-{current_year - 1}.
+                        {current_year}-{current_year - 1} citations to papers published in {current_year}-{current_year - 1}.
                         ''')
 
                     max_value = int(df_cited['Citation'].max())
