f3dc4e2dac808a22f2c3c7e5dc5363837d40bfd3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-20 23:33:15 +0000

    s

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -168,7 +168,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     '(Publication date: ' + str(date_published) + ') ' +
                     ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
                     '[[Publication link]](' + link_to_publication + ') ' +
-                    '[[Zotero link]](' + zotero_link + ')' +
+                    '[[Zotero link]](' + zotero_link + ') ' +
                     (citation_text if include_citation else '')
                 )
             if not selected_country or selected_country=="":
