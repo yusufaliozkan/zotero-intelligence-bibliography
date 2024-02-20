e286c8c71060b1a1e9960dd993de33c131724c93
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-20 23:48:28 +0000

    fd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -267,7 +267,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                     sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation'))
                     display2 = st.checkbox('Display abstracts', key='type_count2')
-                    if sort_by == 'Publication date :arrow_down:' or df_cited['Citation'].sum() == 0:
+                    if sort_by == 'Publication date :arrow_down:' or df_collections['Citation'].sum() == 0:
                         count = 1
                         for index, row in df_collections.iterrows():
                             formatted_entry = format_entry(row)
@@ -275,7 +275,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             count += 1
                             if display2:
                                 st.caption(row['Abstract']) 
-                    elif sort_by == 'Publication type' or df_cited['Citation'].sum() == 0:
+                    elif sort_by == 'Publication type' or df_collections['Citation'].sum() == 0:
                         df_collections = df_collections.sort_values(by=['Publication type'], ascending=True)
                         current_type = None
                         count_by_type = {}
