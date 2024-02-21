6c5377a9fd3ada6adb6150d6acc5c74065023d04
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-21 13:59:13 +0000

    sd

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -173,19 +173,14 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         if display2:
                             st.caption(row['Abstract'])
                 else:
-                    df_collections = df_collections.sort_values(by=['Publication type'], ascending=True)
-                    current_type = None
-                    count_by_type = {}
+                    df_collections = df_collections.sort_values(by=['Citation'], ascending=False)
+                    count = 1
                     for index, row in df_collections.iterrows():
-                        if row['Publication type'] != current_type:
-                            current_type = row['Publication type']
-                            st.subheader(current_type)
-                            count_by_type[current_type] = 1
                         formatted_entry = format_entry(row)
-                        st.write(f"{count_by_type[current_type]}) {formatted_entry}")
-                        count_by_type[current_type] += 1
+                        st.write(f"{count}) {formatted_entry}")
+                        count += 1
                         if display2:
-                            st.caption(row['Abstract'])
+                            st.caption(row['Abstract']) 
 
 #UNTIL HERE
         with col2:
