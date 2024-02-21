6f154eb61220ece7d041fb61bfad9c81bbee3307
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-21 14:45:21 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -104,6 +104,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 if only_citation:
                     df_collections = df_collections[(df_collections['Citation'].notna()) & (df_collections['Citation'] != 0)]
                     df_countries = df_countries[(df_countries['Citation'].notna()) & (df_countries['Citation'] != 0)]
+                    df_continent = df_continent[(df_continent['Citation'].notna()) & (df_continent['Citation'] != 0)]
             
             container_info = container_info.info('This collection lists academic sources that are **non-UK/US** on intelligence.')
 
