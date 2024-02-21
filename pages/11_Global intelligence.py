0608e6faa0f9b6998636d2f8cf486325f312b07e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-21 00:11:02 +0000

    SD

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -131,46 +131,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_countries = df_countries[df_countries['Country'] == selected_country]
 
             # Display the filtered DataFrame
-            # def format_entry(row, include_citation=True):
-            #     publication_type = str(row['Publication type']) if pd.notnull(row['Publication type']) else ''
-            #     title = str(row['Title']) if pd.notnull(row['Title']) else ''
-            #     authors = str(row['FirstName2'])
-            #     date_published = str(row['Date published']) if pd.notnull(row['Date published']) else ''
-            #     link_to_publication = str(row['Link to publication']) if pd.notnull(row['Link to publication']) else ''
-            #     zotero_link = str(row['Zotero link']) if pd.notnull(row['Zotero link']) else ''
-            #     published_by_or_in = ''
-            #     published_source = ''
-            #     citation = str(row['Citation']) if pd.notnull(row['Citation']) else '0'  
-            #     citation = int(float(citation))
-            #     citation_link = str(row['Citation_list']) if pd.notnull(row['Citation_list']) else ''
-            #     citation_link = citation_link.replace('api.', '')
-
-            #     published_by_or_in_dict = {
-            #         'Journal article': 'Published in',
-            #         'Magazine article': 'Published in',
-            #         'Newspaper article': 'Published in',
-            #         'Book': 'Published by',
-            #     }
-
-            #     publication_type = row['Publication type']
-
-            #     published_by_or_in = published_by_or_in_dict.get(publication_type, '')
-            #     published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-            #     if publication_type == 'Book':
-            #         published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
-            #     citation_text = ('Cited by [' + str(citation) + '](' + citation_link + ')' if citation > 0 
-            #         else '')
-
-            #     return (
-            #         '**' + publication_type + '**' + ': ' +
-            #         title + ' ' +
-            #         '(by ' + '*' + authors + '*' + ') ' +
-            #         '(Publication date: ' + str(date_published) + ') ' +
-            #         ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
-            #         '[[Publication link]](' + link_to_publication + ') ' +
-            #         '[[Zotero link]](' + zotero_link + ') ' +
-            #         (citation_text if include_citation else '')
-            #     )
+            # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
             if not selected_country or selected_country=="":
                 st.write('Please select a country')
             
@@ -191,46 +152,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     a = f'{collection_name}_{today}'
                     st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
                     st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
-                    # def format_entry(row, include_citation=True):
-                    #     publication_type = str(row['Publication type']) if pd.notnull(row['Publication type']) else ''
-                    #     title = str(row['Title']) if pd.notnull(row['Title']) else ''
-                    #     authors = str(row['FirstName2'])
-                    #     date_published = str(row['Date published']) if pd.notnull(row['Date published']) else ''
-                    #     link_to_publication = str(row['Link to publication']) if pd.notnull(row['Link to publication']) else ''
-                    #     zotero_link = str(row['Zotero link']) if pd.notnull(row['Zotero link']) else ''
-                    #     published_by_or_in = ''
-                    #     published_source = ''
-                    #     citation = str(row['Citation']) if pd.notnull(row['Citation']) else '0'  
-                    #     citation = int(float(citation))
-                    #     citation_link = str(row['Citation_list']) if pd.notnull(row['Citation_list']) else ''
-                    #     citation_link = citation_link.replace('api.', '')
-
-                    #     published_by_or_in_dict = {
-                    #         'Journal article': 'Published in',
-                    #         'Magazine article': 'Published in',
-                    #         'Newspaper article': 'Published in',
-                    #         'Book': 'Published by',
-                    #     }
-
-                    #     publication_type = row['Publication type']
-
-                    #     published_by_or_in = published_by_or_in_dict.get(publication_type, '')
-                    #     published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-                    #     if publication_type == 'Book':
-                    #         published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
-                    #     citation_text = ('Cited by [' + str(citation) + '](' + citation_link + ')' if citation > 0 
-                    #         else '')
-
-                    #     return (
-                    #         '**' + publication_type + '**' + ': ' +
-                    #         title + ' ' +
-                    #         '(by ' + '*' + authors + '*' + ') ' +
-                    #         '(Publication date: ' + str(date_published) + ') ' +
-                    #         ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
-                    #         '[[Publication link]](' + link_to_publication + ') ' +
-                    #         '[[Zotero link]](' + zotero_link + ') ' +
-                    #         (citation_text if include_citation else '')
-                    #     )
+                    # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
 
                     articles_list = []  # Store articles in a list
                     for index, row in df_collections.iterrows():
