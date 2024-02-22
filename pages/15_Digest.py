a2e528ade309faca6d9eee5d569162501a3cd5f1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-22 09:40:59 +0000

    sd

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -321,7 +321,7 @@ with st.spinner('Preparing digest...'):
                     '[[Publication link]](' + str(link_to_publication) + ') ' +
                     '[[Zotero link]](' + str(zotero_link) + ')'
                 )
-                sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Citation'), key='citation')
+                # sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Citation'), key='citation')
                 display2 = st.checkbox('Display abstracts')
                 if sort_by == 'Publication date :arrow_down:' or df_cited['Citation'].sum() == 0:
                     count = 1
