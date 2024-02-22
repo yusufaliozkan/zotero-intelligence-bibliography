f78b3a85bf75f6735cb333f3766499c1b32ae8d9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-22 22:38:22 +0000

    ds

--- Home.py
+++ Home.py
@@ -1994,7 +1994,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         today = datetime.datetime.now()
                         items = []
                         for entry in feed.entries:
-                            publication_date = datetime.strptime(entry.updated, '%Y-%m-%dT%H:%M:%SZ')
+                            publication_date = datetime.datetime.strptime(entry.updated, '%Y-%m-%dT%H:%M:%SZ')
                             days_difference = (today - publication_date).days
                             if days_difference <= 60 and 'Correction' not in entry.title:
                                 title = entry.title  # Remove .lower() conversion
