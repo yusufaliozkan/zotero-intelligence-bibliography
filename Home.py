6553f76857a63d5c8f562261dba328537ada708b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-23 23:07:53 +0000

    ds

--- Home.py
+++ Home.py
@@ -1991,7 +1991,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         ]
  
                     # Define keywords for filtering
-                    keywords = ['intelligence', 'spy', 'counterintelligence', 'espionage', 'gallant']
+                    keywords = ['intelligence', 'spy', 'counterintelligence', 'espionage', 'Gallant']
 
                     # Initialize an empty list to store DataFrame for each API link
                     dfs = []
