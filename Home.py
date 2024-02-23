c95e6aab9c1cad0eb06beb1270e8098da0de740f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-23 01:13:11 +0000

    sd

--- Home.py
+++ Home.py
@@ -2039,11 +2039,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     merged_df = pd.merge(filtered_final_df, df_dois[['DOI']], on='DOI', how='left', indicator=True)
                     items_not_in_df2 = merged_df[merged_df['_merge'] == 'left_only']
                     items_not_in_df2.drop('_merge', axis=1, inplace=True)
-                    items_not_in_df2 = items_not_in_df2.reset_index(drop=True)
 
                     words_to_exclude = ['paperback', 'hardback'] 
-                    mask = ~items_not_in_df2['Title'].str.contains('|'.join(words_to_exclude), case=False)
-                    items_not_in_df2 = items_not_in_df2[mask]
+                    # mask = ~items_not_in_df2['Title'].str.contains('|'.join(words_to_exclude), case=False)
+                    # items_not_in_df2 = items_not_in_df2[mask]
+                    items_not_in_df2 = items_not_in_df2.reset_index(drop=True)
                     items_not_in_df2
 
         with col2:
