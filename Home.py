75993bd1c4fdb50cb7d8112e517a70cfa73ee33d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-28 22:57:45 +0000

    x

--- Home.py
+++ Home.py
@@ -721,8 +721,15 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         breakdown_string = ', '.join([f"{key}: {value}" for key, value in publications_by_type.items()])
                         st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
 
+                        true_count = filtered_collection_df[filtered_collection_df['Publication type']=='Journal article']['OA status'].sum()
+                        total_count = len(filtered_collection_df[filtered_collection_df['Publication type']=='Journal article'])
+                        if total_count == 0:
+                            oa_ratio = 0.0
+                        else:
+                            oa_ratio = true_count / total_count * 100
+
                         citation_count = filtered_collection_df['Citation'].sum()
-                        st.write(f'**Number of citations:** {int(citation_count)}')
+                        st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
 
                         a = f'{selected_collection}_{today}'
                         st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
