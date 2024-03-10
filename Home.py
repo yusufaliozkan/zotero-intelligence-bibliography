a33fcae9b1d57a3cb8019cee6a660dde19d1b7c5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-03-10 11:49:50 +0000

    as

--- Home.py
+++ Home.py
@@ -625,9 +625,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             # st.pyplot()
                         else:
                             if not on:  # If the toggle is off, display the publications
-                                with st.popover('Sort by'):
-                                    sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Citation'))
-                                if pub_date == 'Publication date :arrow_down:' or filtered_collection_df_authors['Citation'].sum() == 0:
+                                sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Citation'))
+                                if sort_by == 'Publication date :arrow_down:' or filtered_collection_df_authors['Citation'].sum() == 0:
                                     filtered_collection_df_authors = filtered_collection_df_authors.sort_values(by=['Date published'], ascending=False)
                                     filtered_collection_df_authors =filtered_collection_df_authors.reset_index(drop=True)
                                 else:
