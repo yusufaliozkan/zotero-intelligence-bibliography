0b787ab5db05b389ef34a27952a5766526d12b2a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-03-12 19:45:05 +0000

    ert

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -318,6 +318,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
         col1, col2 = st.columns(2)
         with col1:
             df_year=df_year.rename(columns={'index':'Publication year','Date year':'Count'})
+            df_year
             df_year.drop(df_year[df_year['Publication year']== 'No date'].index, inplace = True)
             df_year=df_year.sort_values(by='Publication year', ascending=True)
             fig = px.bar(df_year, x='Publication year', y='Count')
