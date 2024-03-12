fa52a667642e070722e7c90dede5d12195e6b41d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-03-12 19:43:31 +0000

    ds

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -292,6 +292,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_plot = df_plot.rename(columns={'index': 'Publication type', 'Publication type': 'Count'})
 
             plot = df_plot.copy()
+            plot
 
             # st.bar_chart(plot.sort_values(ascending=False), height=600, width=600, use_container_width=True)
 
