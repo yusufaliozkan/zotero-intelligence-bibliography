4e35e52a7418a2c142bce79f4dfdee23dbcdca5b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-03-19 11:31:17 +0000

    sd

--- copyright.py
+++ copyright.py
@@ -5,6 +5,7 @@ import streamlit as st
 cite_today = datetime.date.today().strftime("%d %B %Y")
 
 def display_custom_license():
+    st.write(f'**Cite this page:** Ozkan, Yusuf A. ‘*Intelligence Studies Network*’, 1 June 2020, Accessed {cite_today}. https://intelligence.streamlit.app/.')
     components.html(
     f"""
     <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
@@ -12,5 +13,3 @@ def display_custom_license():
     © 2024 Yusuf Ozkan. All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
     """
     )
-
-    st.write(f'**Cite this page:** Ozkan, Yusuf A. ‘*Intelligence Studies Network*’, 1 June 2020, Accessed {cite_today}. https://intelligence.streamlit.app/.')
