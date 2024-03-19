3cdd9d425f96e1ab3a5bcf5a4c3de07e2e84d54c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-03-19 11:30:04 +0000

    sd

--- copyright.py
+++ copyright.py
@@ -9,7 +9,7 @@ def display_custom_license():
     <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
     src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
     © 2024 Yusuf Ozkan. All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
-    
-    **Cite this page:** Ozkan, Yusuf A. ‘*Intelligence Studies Network*’, 1 June 2020, Accessed {cite_today}. https://intelligence.streamlit.app/.
     """
-    )    
+    )
+
+    st.write(f'**Cite this page:** Ozkan, Yusuf A. ‘*Intelligence Studies Network*’, 1 June 2020, Accessed {cite_today}. https://intelligence.streamlit.app/.')
