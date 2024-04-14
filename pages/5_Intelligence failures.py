5ba453c64442bbf53d168c3daa36fc25844c06d2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-04-14 18:03:26 +0100

    ds

--- pages/5_Intelligence failures.py
+++ pages/5_Intelligence failures.py
@@ -277,6 +277,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     with tab2:
         st.header('Dashboard')
         st.markdown('#### Collection theme: ' + collection_name)
+        st.toast('Hip!')
 
         if df_collections['Title'].any() in ("", [], None, 0, False):
             all = st.checkbox('Show all types')
