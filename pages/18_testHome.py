9dbc46c92b94f6980966910002c23f664c62c60d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-30 17:14:37 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -269,7 +269,22 @@ with st.spinner('Retrieving data & updating dashboard...'):
             #     )
 
             # Title input from the user
-            @st.dialog("Cast your vote")
+            @st.experimental_dialog("Cast your vote")
+            def vote(item):
+                st.write(f"Why is {item} your favorite?")
+                reason = st.text_input("Because...")
+                if st.button("Submit"):
+                    st.session_state.vote = {"item": item, "reason": reason}
+                    st.rerun()
+
+            if "vote" not in st.session_state:
+                st.write("Vote for your favorite")
+                if st.button("A"):
+                    vote("A")
+                if st.button("B"):
+                    vote("B")
+            else:
+                f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
 
 
             def parse_search_terms(search_term):
