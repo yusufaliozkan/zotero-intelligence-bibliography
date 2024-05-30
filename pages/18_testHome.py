99e4559ee307cb2477cea78d133e00b9a3d9f9e7
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-30 17:13:59 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -269,6 +269,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
             #     )
 
             # Title input from the user
+            @st.dialog("Cast your vote")
+
 
             def parse_search_terms(search_term):
                 # Split the search term by spaces while keeping phrases in quotes together
