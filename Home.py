57ed35077dc31726da5a584a0dd28507231cd560
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 00:22:30 +0100

    zx

--- Home.py
+++ Home.py
@@ -18,8 +18,8 @@ import nltk
 # nltk.download('all') 
 from nltk.corpus import stopwords
 # nltk.download('stopwords')
-from wordcloud import WordCloud
-from gsheetsdb import connect
+# from wordcloud import WordCloud
+# from gsheetsdb import connect
 # import gsheetsdb as gdb
 import datetime as dt
 import time
