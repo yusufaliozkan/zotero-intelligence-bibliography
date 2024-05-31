0bcaa96b68df8220468b924455d8430b0bf6381b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 02:25:42 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -227,7 +227,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_gs
                 df_forms = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=1941981997')
                 df_forms = df_forms.rename(columns={'Event name':'event_name', 'Event organiser':'organiser','Link to the event':'link','Date of event':'date', 'Event venue':'venue', 'Details':'details'})
-                df_forms = df_forms.drop(columns=['TimeStamp'])
+                df_forms = df_forms.drop(columns=['Timestamp'])
                 df_forms
 
 
