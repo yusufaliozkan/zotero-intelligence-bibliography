55950ee5fc42e6b0bb9edd3f69dc7d9e849219df
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 17:42:30 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -413,24 +413,24 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                         filtered_df = dynamic_filters.display_df()
                         
-                        types = filtered_df['Publication type'].dropna().unique()  # Exclude NaN values
-                        journals = filtered_df['Journal'].dropna().unique()
-                        collections = filtered_df['Collection_Name'].dropna().unique()
+                        # types = filtered_df['Publication type'].dropna().unique()  # Exclude NaN values
+                        # journals = filtered_df['Journal'].dropna().unique()
+                        # collections = filtered_df['Collection_Name'].dropna().unique()
                         
-                        with st.popover("Filters and more"):
-                            types2 = st.multiselect('Publication types', types, key='original2')
-                            journals = st.multiselect('Journal', journals, key='original_journal' )
-                            collections = st.multiselect('Collection', collections, key='original_collection')
-                            container_download_button = st.container()
+                        # with st.popover("Filters and more"):
+                        #     types2 = st.multiselect('Publication types', types, key='original2')
+                        #     journals = st.multiselect('Journal', journals, key='original_journal' )
+                        #     collections = st.multiselect('Collection', collections, key='original_collection')
+                        #     container_download_button = st.container()
 
-                        if types2:
-                            filtered_df = filtered_df[filtered_df['Publication type'].isin(types2)]
+                        # if types2:
+                        #     filtered_df = filtered_df[filtered_df['Publication type'].isin(types2)]
                         
-                        if journals:
-                            filtered_df = filtered_df[filtered_df['Journal'].isin(journals)]                        
+                        # if journals:
+                        #     filtered_df = filtered_df[filtered_df['Journal'].isin(journals)]                        
 
-                        if collections:
-                            filtered_df = filtered_df[filtered_df['Collection_Name'].isin(collections)] 
+                        # if collections:
+                        #     filtered_df = filtered_df[filtered_df['Collection_Name'].isin(collections)] 
 
 
                         if not filtered_df.empty:
