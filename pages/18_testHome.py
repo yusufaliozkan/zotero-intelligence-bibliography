acc6bc96920a4bdd1078162605a868fe8bd4011c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 11:47:47 +0100

    df

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -405,7 +405,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         filtered_df
                         types = filtered_df['Publication type'].dropna().unique()  # Exclude NaN values
                         journals = filtered_df['Journal'].dropna().unique()
-                        colleections = filtered_df['Collection_Name'].dropna().unique()
+                        collections = filtered_df['Collection_Name'].dropna().unique()
                         
                         with st.popover("Filters and more"):
                             types2 = st.multiselect('Publication types', types, key='original2')
