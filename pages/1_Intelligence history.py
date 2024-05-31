a2c77e82d51d93107211c82d93435914ad4755e4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 01:13:19 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -17,6 +17,7 @@ from nltk.corpus import stopwords
 # nltk.download('stopwords')
 from wordcloud import WordCloud
 # from gsheetsdb import connect
+from streamlit_gsheets import GSheetsConnection
 import datetime as dt     
 import random
 from authors_dict import df_authors, name_replacements
