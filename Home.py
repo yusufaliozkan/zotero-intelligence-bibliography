43a70ec8b7e763bc890e94d43d61eda5a9e94c4f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 03:31:01 +0100

    sd

--- Home.py
+++ Home.py
@@ -1974,14 +1974,14 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_con = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=939232836')
                 df_con['date'] = pd.to_datetime(df_con['date'])
                 df_con['date_new'] = df_con['date'].dt.strftime('%Y-%m-%d')
-                df_con
                 df_con['date_new'] = pd.to_datetime(df_con['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
                 df_con['date_new_end'] = pd.to_datetime(df_con['date_end'], dayfirst = True).dt.strftime('%d/%m/%Y')
                 df_con.sort_values(by='date', ascending = True, inplace=True)
                 df_con['details'] = df_con['details'].fillna('No details')
                 df_con['location'] = df_con['location'].fillna('No details')
-                df_con = df_con.fillna('')            
-                filter = (df_con['date_end']>=today)
+                df_con = df_con.fillna('')
+                df_con['date'] = pd.to_datetime(df_con['date'], dayfirst=True)     
+                filter = df_con['date_end']>=pd.to_datetime(today)
                 df_con = df_con.loc[filter]
                 df_con = df_con.head(1)
                 if df_con['conference_name'].any() in ("", [], None, 0, False):
