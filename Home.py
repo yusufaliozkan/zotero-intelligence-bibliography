b2dc1f1cb4f50a39e405d0243d641461d107f986
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 03:26:39 +0100

    sd

--- Home.py
+++ Home.py
@@ -1970,7 +1970,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     st.write(''+str(i+1)+') '+ df_gs1.iloc[i])
                 st.write('Visit the [Events on intelligence](https://intelligence.streamlit.app/Events) page to see more!')
                 
-            #     st.markdown('##### Next conference')
+                st.markdown('##### Next conference')
+                df_con = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=939232836')
+                df_con
             #     sheet_url2 = st.secrets["public_gsheets_url2"]
             #     rows = run_query(f'SELECT * FROM "{sheet_url2}"')
             #     data = []
