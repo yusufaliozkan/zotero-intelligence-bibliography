1b39a883f0e9454fbca02e1d592821bd57e21224
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 18:50:30 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -370,13 +370,14 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 if st.button('Search guide'):
                     st.toast('''
                     **Search guide**
-                    
+
                     The following Boolean operators are available: AND, OR, NOT (e.g. "covert action" NOT british).
 
                     Search with double quote is available. (e.g. "covert action")
 
                     Search with parantheses is **not** available.                   
                     ''')
+                    time.sleep(.5)
                 cols, cola = st.columns([2,6])
                 with cols:
                     include_abstracts = st.selectbox('🔍 options', ['In title','In title & abstract'])
