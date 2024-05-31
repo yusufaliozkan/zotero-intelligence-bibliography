e3489c41c3f2cca7df35c186ffde3f5f3c57e0c3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 10:29:17 +0100

    sd

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -45,6 +45,7 @@ df_gs = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpz
 
 df_forms = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=1941981997')
 df_forms = df_forms.rename(columns={'Event name':'event_name', 'Event organiser':'organiser','Link to the event':'link','Date of event':'date', 'Event venue':'venue', 'Details':'details'})
+df_forms2 = df_forms.copy()
 df_forms = df_forms.drop(columns=['Timestamp'])
 
 # Perform SQL query on the Google Sheet.
