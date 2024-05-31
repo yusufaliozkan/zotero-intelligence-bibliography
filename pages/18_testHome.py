073efd01eb62603191c1643e7f7c7c6e607a1664
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 18:11:01 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -413,7 +413,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             collections = st.multiselect('Collection', collections, key='original_collection')
                             container_download_button = st.container()
 
-                        st.toast("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                        st.toast('''
+                        **Search guide**                        
+                        ''')
 
 
                         if types2:
