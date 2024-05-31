6caf5e38992385471a9d827dab2abd6601e05329
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-05-31 02:43:53 +0100

    er

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -223,38 +223,33 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                 conn = st.connection("gsheets", type=GSheetsConnection)
                 df_gs = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=0') 
+                df_gs
                 df_forms = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=1941981997')
-
-                # Rename columns and drop unnecessary columns
                 df_forms = df_forms.rename(columns={'Event name':'event_name', 'Event organiser':'organiser','Link to the event':'link','Date of event':'date', 'Event venue':'venue', 'Details':'details'})
                 df_forms = df_forms.drop(columns=['Timestamp'])
 
-                # Convert 'date' columns to datetime objects
-                df_gs['date'] = pd.to_datetime(df_gs['date'], dayfirst=True)
-                df_forms['date'] = pd.to_datetime(df_forms['date'], dayfirst=True)
+                df_gs['date'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%Y-%m-%d')
+                df_gs['date_new'] = pd.to_datetime(df_gs['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
 
-                # Extract additional date features
-                df_forms['month'] = df_forms['date'].dt.strftime('%m')
-                df_forms['year'] = df_forms['date'].dt.strftime('%Y')
-                df_forms['month_year'] = df_forms['date'].dt.strftime('%Y-%m')
-
-                # Sort and drop duplicates
-                df_forms.sort_values(by='date', ascending=True, inplace=True)
-                df_forms.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first', inplace=True)
-
-                # Handle missing values
+                df_forms['date'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%Y-%m-%d')
+                df_forms['date_new'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
+                df_forms['month'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%m')
+                df_forms['year'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%Y')
+                df_forms['month_year'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%Y-%m')
+                df_forms.sort_values(by='date', ascending = True, inplace=True)
+                df_forms = df_forms.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
+                
                 df_forms['details'] = df_forms['details'].fillna('No details')
+                df_forms = df_forms.fillna('')
                 df_gs = pd.concat([df_gs, df_forms], axis=0)
-                df_gs.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first', inplace=True)
-
-                # Sort and reset index
-                df_gs.sort_values(by='date', ascending=True, inplace=True)
-                df_gs.reset_index(drop=True, inplace=True)
+                df_gs = df_gs.reset_index(drop=True)
+                df_gs = df_gs.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
+                
+                df_gs = df_gs.sort_values(by='date', ascending = True)
                 df_gs
-
-                # Filter data for events happening today or later
                 today = dt.date.today()
-                filter = (df_gs['date'] >= pd.Timestamp(today))
+                today
+                filter = (df_gs['date']>=today)
                 df_gs = df_gs.loc[filter]
                 df_gs = df_gs.fillna('')
                 df_gs = df_gs.head(3)
