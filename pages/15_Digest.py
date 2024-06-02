fa0655334e9bdd8cd87dd83b3f2f874913c17d36
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-02 19:05:50 +0100

    sd

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -302,7 +302,7 @@ with st.spinner('Preparing digest...'):
                 st.warning(f'No citation yet in {current_year}') 
             else:
                 row_nu99 = len(df_cited)
-                st.info(f'{row_nu99} **Paper(s) cited in {current_year}**.')
+                st.info(f'**{row_nu99} paper(s) cited in {current_year}**.')
                 articles_list = []  # Store articles in a list
                 for index, row in df_cited.iterrows():
                     formatted_entry = format_entry(row)  # Assuming format_entry() is a function formatting each row
