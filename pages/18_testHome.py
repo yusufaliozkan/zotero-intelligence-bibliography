f614191dab433991a4304b59d0309f1f61e1164b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-02 18:39:08 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -205,7 +205,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
     df_dedup = pd.read_csv('all_items.csv')
     df_duplicated = pd.read_csv('all_items_duplicated.csv')
-    df_dedup
 
     col1, col2 = st.columns([3,5])
     with col2:
