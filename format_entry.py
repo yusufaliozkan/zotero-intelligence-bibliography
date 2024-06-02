4dfdbd1215d7e6a292ae78f37492e1bddf39bde4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-02 18:51:08 +0100

    sd

--- format_entry.py
+++ format_entry.py
@@ -9,7 +9,8 @@ def format_entry(row, include_citation=True):
     zotero_link = str(row['Zotero link']) if pd.notnull(row['Zotero link']) else ''
     published_by_or_in = ''
     published_source = ''
-    citation = str(row['Citation']) if pd.notnull(row['Citation']) else '0'  
+    book_title = ''
+    citation = str(row['Citation']) if pd.notnull(row['Citation']) else '0'
     citation = int(float(citation))
     citation_link = str(row['Citation_list']) if pd.notnull(row['Citation_list']) else ''
     citation_link = citation_link.replace('api.', '')
@@ -22,28 +23,41 @@ def format_entry(row, include_citation=True):
     }
 
     publication_type = row['Publication type']
-
     published_by_or_in = published_by_or_in_dict.get(publication_type, '')
-    published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-    if publication_type == 'Book':
+
+    if publication_type == 'Journal article' or publication_type == 'Magazine article' or publication_type == 'Newspaper article':
+        published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
+    elif publication_type == 'Book':
         published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
-    if publication_type == 'Book chapter':
-        published_source = str(row['Book_title']) if pd.notnull(row['Book_title']) else ''
+    elif publication_type == 'Book chapter':
+        book_title = str(row['Book_title']) if pd.notnull(row['Book_title']) else ''
 
     citation_text = ('Cited by [' + str(citation) + '](' + citation_link + ')' if citation > 0 else '')
-    
     oa_url = str(row['OA_link']) if pd.notnull(row['OA_link']) else ''
     oa_url_fixed = oa_url.replace(' ', '%20')
     oa_link_text = '[[Open access version](' + oa_url_fixed + ')]' if oa_url_fixed else ''
 
-    return (
-        '**' + publication_type + '**' + ': ' +
-        title + ' ' +
-        '(by ' + '*' + authors + '*' + ') ' +
-        '(Publication date: ' + str(date_published) + ') ' +
-        ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
-        '[[Publication link]](' + link_to_publication + ') ' +
-        '[[Zotero link]](' + zotero_link + ') ' +
-        (oa_link_text + ' ' if oa_link_text else '') +
-        (citation_text if include_citation else '')
-    )
+    if publication_type == 'Book chapter':
+        return (
+            '**' + publication_type + '**' + ': ' +
+            title + ' ' +
+            '(In: ' + book_title + ') ' +
+            '(by ' + '*' + authors + '*' + ') ' +
+            '(Publication date: ' + str(date_published) + ') ' +
+            '[[Publication link]](' + link_to_publication + ') ' +
+            '[[Zotero link]](' + zotero_link + ') ' +
+            (oa_link_text + ' ' if oa_link_text else '') +
+            (citation_text if include_citation else '')
+        )
+    else:
+        return (
+            '**' + publication_type + '**' + ': ' +
+            title + ' ' +
+            '(by ' + '*' + authors + '*' + ') ' +
+            '(Publication date: ' + str(date_published) + ') ' +
+            ('(' + published_by_or_in + ': ' + '*' + published_source + '*' + ') ' if published_by_or_in else '') +
+            '[[Publication link]](' + link_to_publication + ') ' +
+            '[[Zotero link]](' + zotero_link + ') ' +
+            (oa_link_text + ' ' if oa_link_text else '') +
+            (citation_text if include_citation else '')
+        )
