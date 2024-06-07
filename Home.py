b1bcb1958ce1321b565c393ac0a2ff367ed749f3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-07 11:53:35 +0100

    sd

--- Home.py
+++ Home.py
@@ -531,10 +531,10 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                     'university', 'followed', 'particular', 'based', 'press', 'examine', 'show', 'may', 'result', 'explore',
                                     'examines', 'become', 'used', 'journal', 'london', 'review']
                                 stopword.extend(SW)
-                                
-                                stopword.extend(search_term.split())
-                                stopword.append(search_term)
-                                search_term
+
+                                custom_stopwords = extract_quoted_phrases(search_term)
+                                stopword.extend(custom_stopwords)
+                                custom_stopwords
                                 def remove_stopwords(text):
                                     text = [i for i in text if i] # this part deals with getting rid of spaces as it treads as a string
                                     text = [word for word in text if word not in stopword] #keep the word if it is not in stopword
