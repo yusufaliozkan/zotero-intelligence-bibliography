bcbc40048cc6c3fab4fa4d048d957601935d964b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-08 21:16:26 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -662,6 +662,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 sorted_authors_by_publications = sorted(unique_authors, key=lambda author: author_publications.get(author, 0), reverse=True)
                 select_options_author_with_counts = [''] + [f"{author} ({author_publications.get(author, 0)})" for author in sorted_authors_by_publications]
 
+                # Retrieve current query parameters
                 query_params = st.query_params.to_dict()
                 search_term = query_params.get("author", "")
 
@@ -671,10 +672,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     index=select_options_author_with_counts.index(f"{search_term} ({author_publications.get(search_term, 0)})") if search_term else 0
                 )
                 selected_author = selected_author_display.split(' (')[0] if selected_author_display else None
-                # selected_author = st.selectbox('Select author', select_options_author)
+
+                # Update the URL parameters for the shareable link
                 st.query_params.from_dict({"search_option": search_option, "author": selected_author})
 
-                if not selected_author or selected_author =="":
+                if not selected_author or selected_author == "":
                     st.write('Select an author to see items')
                 else:
 
