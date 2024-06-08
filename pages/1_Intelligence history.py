14dd040be773e6d88d4fb58c05683dd3296bf0a4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-08 17:30:39 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -89,7 +89,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             # collection_name = st.selectbox('Select a collection:', clist)
             collection_name = radio
             if collection_name:
-                st.experimental_set_query_params(collection_name=collection_name)
+                # st.experimental_set_query_params(collection_name=collection_name)
             # st.experimental_set_query_params(collection_name=radio)
 
             df_collections = df_collections.loc[df_collections['Collection_Name']==collection_name]
