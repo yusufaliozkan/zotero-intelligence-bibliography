dd941eb1d54995f64518b970ccd213def92bd513
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-09 00:04:21 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -418,7 +418,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     with st.status("Searching publications...", expanded=True) as status:
                         search_tokens = parse_search_terms(search_term)
 
-                        st.query_params.from_dict({"search": search_term})
                         print(f"Search Tokens: {search_tokens}")  # Debugging: Print search tokens
                         df_csv = df_duplicated.copy()
 
@@ -453,6 +452,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         
                         types = filtered_df['Publication type'].dropna().unique()  # Exclude NaN values
                         collections = filtered_df['Collection_Name'].dropna().unique()
+                        st.query_params.from_dict({"search": search_term})
 
                         with st.popover("Filters and more"):
                             types2 = st.multiselect('Publication types', types, key='original2')
