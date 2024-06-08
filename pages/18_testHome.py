7028ecc9f8d63005a4dbd20193affde6d2824be0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-08 21:48:44 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -687,7 +687,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                 # Retrieve current query parameters
                 query_params = st.query_params.to_dict()
-                search_term = query_params.get("author", None)
+                search_term = query_params.get("author", "")
 
                 selected_author_display = st.selectbox(
                     'Select author', 
