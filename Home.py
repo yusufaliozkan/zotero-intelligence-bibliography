9c3491a9b6566004466df7c2b9d2b787201e0c1e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-09 20:24:12 +0100

    ref

--- Home.py
+++ Home.py
@@ -645,6 +645,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
             # SEARCH AUTHORS
             elif search_option == "Search author":
+                st.query_params.clear()
                 st.subheader('Search author') 
 
                 unique_authors = [''] + list(df_authors['Author_name'].unique())
@@ -816,6 +817,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
             # SEARCH IN COLLECTIONS
             elif search_option == "Search collection": 
+                st.query_params.clear()
                 st.subheader('Search collection')
 
                 df_csv_collections = df_duplicated.copy()
@@ -1015,6 +1017,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 st.write("No publication type selected.")
 
             elif search_option == "Publication types":
+                st.query_params.clear()
                 st.subheader('Publication types')
 
                 df_csv_types = df_dedup.copy()
@@ -1189,6 +1192,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 st.write(f"{index + 1}) {formatted_entry}")
 
             elif search_option == "Search journal":
+                st.query_params.clear()
                 st.subheader('Search journal')
 
                 df_csv = df_dedup.copy()
@@ -1400,7 +1404,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 formatted_entry = format_entry(row)
                                 st.write(f"{index + 1}) {formatted_entry}")
 
-            elif search_option == "Publication year":                
+            elif search_option == "Publication year": 
+                st.query_params.clear()               
                 st.subheader('Items by publication year')
 
                 with st.expander('Click to expand', expanded=True):                    
@@ -1626,7 +1631,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             # Display the article with highlighted search terms
                             st.markdown(f"{i}. {article}", unsafe_allow_html=True)
 
-            elif search_option == "Cited papers":                
+            elif search_option == "Cited papers": 
+                st.query_params.clear()               
                 st.subheader('Cited items in the library')
 
                 with st.expander('Click to expand', expanded=True):                    
