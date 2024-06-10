25bae4fce4b25346f1e3b1bdcdc1f97174da64a9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-10 21:36:13 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -158,8 +158,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 a = f'{collection_name}_{today}'
                 st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
 
-            st.metric(label='Number of items', value=num_items_collections, help=breakdown_string)
-            # st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
+            # st.metric(label='Number of items', value=num_items_collections, help=breakdown_string)
+            st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
             st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
             # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
 
