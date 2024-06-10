6f56046b828930e54edf815a7dc120cb687f42b2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-10 18:04:05 +0100

    sd

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -100,7 +100,7 @@ with col1:
                 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s154337186&sort=publication_year:desc', #Leiden Journal of International Law
                 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s156235965&sort=publication_year:desc', #International & Comparative Law Quarterl
                 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s68909633&sort=publication_year:desc', #Journal of Conflict and Security Law
-                # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s42104779&sort=publication_year:desc', #Journal of International Dispute Settlement
+                'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s42104779&sort=publication_year:desc', #Journal of International Dispute Settlement
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s2764513295&sort=publication_year:desc', #Security and Human Rights
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s82119083&sort=publication_year:desc', #Modern Law Review
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s129176075&sort=publication_year:desc', #International Theory
@@ -123,7 +123,7 @@ with col1:
                 'The Historian', 'The British Journal of Politics and International Relations', 'Terrorism and Political Violence', "Mariner's Mirror",
                 'Small Wars & Insurgencies', 'Journal of Cyber Policy', 'South Asia:Journal of South Asian Studies', 'International Journal', 'German Law Journal',
                 'American Journal of International Law', 'European Journal of International Law', 'Human Rights Law Review', 'Leiden Journal of International Law',
-                'International & Comparative Law Quarterly', 'Journal of Conflict and Security Law', #'Journal of International Dispute Settlement', 'Security and Human Rights',
+                'International & Comparative Law Quarterly', 'Journal of Conflict and Security Law', 'Journal of International Dispute Settlement', #'Security and Human Rights',
                 # 'Modern Law Review', 'International Theory', 'Journal of Global Security Studies', 'Michigan Journal of International Law'
                 ]
 
