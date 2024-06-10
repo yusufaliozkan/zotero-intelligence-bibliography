2cf59ee2c9564f536401b2c5c9d5dbf31b3d6703
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-10 20:16:58 +0100

    sd

--- pages/2_Intelligence studies.py
+++ pages/2_Intelligence studies.py
@@ -157,6 +157,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
 
             with st.expander('Click to expand', expanded=True):
+                st.write(f"**{num_items_collections}** sources found ({breakdown_string})")
+                st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
 
                 articles_list = []  # Store articles in a list
                 for index, row in df_collections.iterrows():
