f22c4c49b43eb2345a25b12c56b66307645af4db
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 00:18:57 +0100

    ds

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -168,6 +168,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
 
             with st.expander('Click to expand', expanded=True):
+                sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation')) 
                 if table_view:
                     df_table_view = df_collections[['Publication type','Title','Date published','FirstName2', 'Abstract','Publisher','Journal','Collection_Name','Link to publication','Zotero link']]
                     df_table_view = df_table_view.rename(columns={'FirstName2':'Author(s)','Collection_Name':'Collection','Link to publication':'Publication link'})
@@ -205,11 +206,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             '[[Publication link]](' + str(link_to_publication) + ') ' +
                             '[[Zotero link]](' + str(zotero_link) + ')'
                         )
-                            
-                    sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation'))
-                    df_collections   
-                    
-                        
+                                                    
                     if sort_by == 'Publication date :arrow_down:' or df_collections['Citation'].sum() == 0:
                         count = 1
                         for index, row in df_collections.iterrows():
