68b33eb482d5d4cec98ae4fc696bc4ffef9d9a29
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-10 17:06:40 +0100

    sd

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -95,7 +95,7 @@ with col1:
                 'https://api.openalex.org/works?filter=primary_location.source.id:s86954274&sort=publication_year:desc&per_page=10', #International Journal
                 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s117224066&sort=publication_year:desc', #German Law Journal
                 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s160097506&sort=publication_year:desc', #American Journal of International Law
-                # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s175405714&sort=publication_year:desc', #European Journal of International Law
+                'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s175405714&sort=publication_year:desc', #European Journal of International Law
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s84944781&sort=publication_year:desc', #Human Rights Law Review
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s154337186&sort=publication_year:desc', #Leiden Journal of International Law
                 # 'https://api.openalex.org/works?page=1&filter=primary_location.source.id:s156235965&sort=publication_year:desc', #International & Comparative Law Quarterl
@@ -122,7 +122,7 @@ with col1:
                 'The English Historical Review', 'World Politics', 'Israel Affairs', 'Australian Journal of International Affairs', 'Contemporary British History',
                 'The Historian', 'The British Journal of Politics and International Relations', 'Terrorism and Political Violence', "Mariner's Mirror",
                 'Small Wars & Insurgencies', 'Journal of Cyber Policy', 'South Asia:Journal of South Asian Studies', 'International Journal', 'German Law Journal',
-                'American Journal of International Law', # 'European Journal of International Law', 'Human Rights Law Review', 'Leiden Journal of International Law',
+                'American Journal of International Law', 'European Journal of International Law', # 'Human Rights Law Review', 'Leiden Journal of International Law',
                 # 'International & Comparative Law Quarterly', 'Journal of Conflict and Security Law', 'Journal of International Dispute Settlement', 'Security and Human Rights',
                 # 'Modern Law Review', 'International Theory', 'Journal of Global Security Studies', 'Michigan Journal of International Law'
                 ]
