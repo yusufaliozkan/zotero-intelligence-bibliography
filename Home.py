1e801f4824f7a966d5da1df25c84852687c2bafb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-10 23:12:13 +0100

    sd

--- Home.py
+++ Home.py
@@ -424,15 +424,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         print(f"Search Tokens: {search_tokens}")  # Debugging: Print search tokens
                         df_csv = df_duplicated.copy()
 
-                        col112, col113, col114 = st.columns([2,2,2])
-                        with col112:
-                            display_abstracts = st.checkbox('Display abstracts')
-                        with col113:
-                            only_citation = st.checkbox('Show cited items only')
-                            if only_citation:
-                                df_csv = df_csv[(df_csv['Citation'].notna()) & (df_csv['Citation'] != 0)]
-                        with col114:
-                            table_view = st.checkbox('See results in table')
+
 
                         filtered_df = apply_boolean_search(df_csv, search_tokens, include_abstracts)
                         print(f"Filtered DataFrame (before dropping duplicates):\n{filtered_df}")  # Debugging: Print DataFrame before dropping duplicates
@@ -465,6 +457,16 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             types2 = st.multiselect('Publication types', types, key='original2')
                             collections = st.multiselect('Collection', collections, key='original_collection')
                             container_download_button = st.container()
+
+                            col112, col113, col114 = st.columns([2,2,2])
+                            with col112:
+                                display_abstracts = st.checkbox('Display abstracts')
+                            with col113:
+                                only_citation = st.checkbox('Show cited items only')
+                                if only_citation:
+                                    df_csv = df_csv[(df_csv['Citation'].notna()) & (df_csv['Citation'] != 0)]
+                            with col114:
+                                table_view = st.checkbox('See results in table')
 
                         if types2:
                             filtered_df = filtered_df[filtered_df['Publication type'].isin(types2)]                 
