4770898420ce4ccb3da79880edf67a358dc924cc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 10:55:48 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -95,8 +95,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
     collection_name = radio
     collection_key = collection_mapping[collection_name]
     # if collection_name:
-    
-    st.query_params.from_dict({"collection_id": collection_key})
+    if selected_collection_name != radio:
+        st.query_params.from_dict({"collection_id": collection_mapping[radio]})
     # st.experimental_set_query_params(collection_name=radio)
 
     df_collections = df_collections.loc[df_collections['Collection_Name']==collection_name]
