641fca695790adb98debc7f55a35a61e3002f7fa
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 10:25:44 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -84,16 +84,13 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
     selected_collection_name = reverse_collection_mapping.get(selected_collection_key, None)
 
-    if selected_collection_name in unique_collections:
-        selected_index = unique_collections.index(selected_collection_name)
-    else:
-        selected_index = 0
+    radio = container.radio('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
 
-    if selected_collection_name in unique_collections:
-        # Set the default value to the selected collection from the query params
-        radio = container.radio('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
-    else:
-        radio = container.radio('Select a collection', unique_collections)
+    # if selected_collection_name in unique_collections:
+    #     # Set the default value to the selected collection from the query params
+    #     radio = container.radio('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
+    # else:
+    #     radio = container.radio('Select a collection', unique_collections)
 
     # radio = container.radio('Select a collection', unique_collections)
     # collection_name = st.selectbox('Select a collection:', clist)
