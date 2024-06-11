df1c950c24b5504c293cf961d08753267d2fe85f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 10:37:01 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -75,23 +75,28 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-    # container = st.container()
-
+    # Retrieve query parameters
     query_params = st.query_params.to_dict()
-    selected_collection_key  = query_params.get("collection_id", None)
+    selected_collection_key = query_params.get("collection_id", None)
 
     unique_collections = list(df_collections['Collection_Name'].unique())
 
     selected_collection_name = reverse_collection_mapping.get(selected_collection_key, None)
 
+    # Determine the default index for the radio button
     if selected_collection_name in unique_collections:
-        # Set the default value to the selected collection from the query params
-        radio = st.radio('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
+        default_index = unique_collections.index(selected_collection_name)
     else:
-        radio = st.radio('Select a collection', unique_collections)
+        default_index = 0
 
-    # radio = container.radio('Select a collection', unique_collections)
-    # collection_name = st.selectbox('Select a collection:', clist)
+    # Create the radio button with the default index
+    radio = st.radio('Select a collection', unique_collections, index=default_index)
+
+    # Update query parameters only if there's a change in selection
+    if selected_collection_name != radio:
+        st.query_params.from_dict({"collection_id": collection_mapping[radio]})
+
+    # Further processing based on the selected collection
     collection_name = radio
     collection_key = collection_mapping[collection_name]
     # if collection_name:
