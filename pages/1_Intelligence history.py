ad40e9003566256f016b38f07bf853a7d83e5ba1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 13:38:33 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -94,7 +94,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     collection_name = radio
     collection_key = collection_mapping[collection_name]
     # if collection_name:
-    st.query_params.from_dict({"collection_id": collection_name})
+    st.query_params.from_dict({"collection_id": collection_key})
     df_collections = df_collections.loc[df_collections['Collection_Name']==collection_name]
     pd.set_option('display.max_colwidth', None)
 
