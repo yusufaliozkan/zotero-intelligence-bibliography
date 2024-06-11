62cb92086b8f0b028da85d6fba3f35081b0222cf
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-11 08:15:23 +0100

    sd

--- pages/7_Intelligence collection.py
+++ pages/7_Intelligence collection.py
@@ -195,7 +195,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
             sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation'))
             
             with st.expander('Click to expand', expanded=True):
-                if sort_by == 'Publication date :arrow_down:' or df_collections['Citation'].sum() == 0:
+
+                if sort_by == 'Publication date :arrow_down:': # or df_collections['Citation'].sum() == 0:
                     count = 1
                     for index, row in df_collections.iterrows():
                         formatted_entry = format_entry(row)
@@ -203,7 +204,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         count += 1
                         if display2:
                             st.caption(row['Abstract']) 
-                elif sort_by == 'Publication type' or df_collections['Citation'].sum() == 0:
+                elif sort_by == 'Publication type': # or df_collections['Citation'].sum() == 0:
                     df_collections = df_collections.sort_values(by=['Publication type'], ascending=True)
                     current_type = None
                     count_by_type = {}
@@ -218,14 +219,23 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         if display2:
                             st.caption(row['Abstract'])
                 else:
-                    df_collections = df_collections.sort_values(by=['Citation'], ascending=False)
-                    count = 1
-                    for index, row in df_collections.iterrows():
-                        formatted_entry = format_entry(row)
-                        st.write(f"{count}) {formatted_entry}")
-                        count += 1
-                        if display2:
-                            st.caption(row['Abstract']) 
+                    if df_collections['Citation'].sum() == 0:
+                        count = 1
+                        for index, row in df_collections.iterrows():
+                            formatted_entry = format_entry(row)
+                            st.write(f"{count}) {formatted_entry}")
+                            count += 1
+                            if display2:
+                                st.caption(row['Abstract'])
+                    else:
+                        df_collections = df_collections.sort_values(by=['Citation'], ascending=False)
+                        count = 1
+                        for index, row in df_collections.iterrows():
+                            formatted_entry = format_entry(row)
+                            st.write(f"{count}) {formatted_entry}")
+                            count += 1
+                            if display2:
+                                st.caption(row['Abstract']) 
 
 #UNTIL HERE
         with col2:
