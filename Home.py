8af7c8d6dae0e2af4ed32127af94b3e5e501b04b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-11 09:03:42 +0100

    ds

--- Home.py
+++ Home.py
@@ -804,11 +804,7 @@ with st.spinner('Retrieving data...'):
                                             display_bibliographies(filtered_df)
                                 else:
                                     st.write("No articles found with the given keyword/phrase.")
-                                status.update(
-                                    label=f"Search found **{num_items}** {'matching source' if num_items == 1 else 'matching sources'} in the database for '{search_term}'.", 
-                                    state="complete", 
-                                    expanded=True
-                                    )
+                                status.update(label="Search completed!", state="complete", expanded=True)
                         else:
                             st.write("Please enter a keyword or author name to search.")
                     search_keyword()
