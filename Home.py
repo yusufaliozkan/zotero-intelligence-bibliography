04f667a32c8c231842327186b2edcac1144827c0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-29 22:38:52 +0100

    sd

--- Home.py
+++ Home.py
@@ -3659,27 +3659,39 @@ with st.spinner('Retrieving data...'):
                         fig.update_layout(title={'text':'Top ' + str(number0) + ' collections in the library', 'y':0.95, 'x':0.4, 'yanchor':'top'})
                         st.plotly_chart(fig, use_container_width = True)
                     with col2:
-                        collection_line_legend_check = st.checkbox('Show legend', key='collection_line_legend_check')
-                        if collection_line_legend_check:
-                            collection_line_legend=True
-                        else:
-                            collection_line_legend=False
+                        colcum1, colcum2, colcum3 = st.columns(3)
+                        with colcum1:
+                            collection_line_legend_check = st.checkbox('Hide legend', key='collection_line_legend_check')
+                        with colcum2:
+                            last_10_year = st.checkbox('Limit to last 10 years', key='last10yearscollectioncummulative')
+                        with colcum3:
+                            top_5_collections = st.checkbox('Show top 5 collections', key='top5collections')
+                        
+                        collection_line_legend = not collection_line_legend_check
+
                         df_collections_22 = df_collections_2.copy()
+                        if last_10_year:
+                            df_collections_22 = df_collections_22[df_collections_22['Date year'] != 'No date']
+                            df_collections_22['Date year'] = df_collections_22['Date year'].astype(int)
+                            current_year = datetime.datetime.now().year
+                            df_collections_22 = df_collections_22[df_collections_22['Date year'] > (current_year - 10)]
+                        
                         collection_counts = df_collections_22.groupby(['Date year', 'Collection_Name']).size().unstack().fillna(0)
                         collection_counts = collection_counts.reset_index()
                         collection_counts.iloc[:, 1:] = collection_counts.iloc[:, 1:].cumsum()
 
-                        selected_collections = df_collections_21.head(number0 + 1)['Collection_Name'].tolist()
-                        collection_counts_filtered = collection_counts[['Date year'] + selected_collections]
-                        column_to_exclude = '01 Intelligence history'
-                        if column_to_exclude in selected_collections:
-                            selected_collections.remove(column_to_exclude)
-                        collection_counts_filtered = collection_counts[['Date year'] + selected_collections]
+                        # Determine the top 5 collections if the checkbox is checked
+                        if top_5_collections:
+                            top_5 = df_collections_22['Collection_Name'].value_counts().head(5).index.tolist()
+                        else:
+                            top_5 = df_collections_22['Collection_Name'].unique().tolist()
+                        
+                        collection_counts_filtered = collection_counts[['Date year'] + top_5]
                         collection_counts_filtered['Date year'] = pd.to_numeric(collection_counts_filtered['Date year'], errors='coerce')
-                        collection_counts_filtered = collection_counts_filtered.sort_values(by=['Date year'] + selected_collections, ascending=True)
+                        collection_counts_filtered = collection_counts_filtered.sort_values(by=['Date year'] + top_5, ascending=True)
 
                         # Plotting the line graph using Plotly Express
-                        fig = px.line(collection_counts_filtered, x='Date year', y=selected_collections, 
+                        fig = px.line(collection_counts_filtered, x='Date year', y=top_5, 
                                     markers=True, line_shape='linear', labels={'value': 'Cumulative Count'},
                                     title='Cumulative changes in collection over years')
                         fig.update_layout(showlegend=collection_line_legend)
