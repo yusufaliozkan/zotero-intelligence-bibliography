41af116ce48ca1b22494f7f0496dd1f663caaaff
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-10 01:21:08 +0100

    sd

--- pages/16_Item monitoring.py
+++ pages/16_Item monitoring.py
@@ -332,7 +332,7 @@ with col1:
 
 
             st.write('**Other resources**')
-            
+
             def fetch_rss_data(url, label):
                 response = requests.get(url)
                 rss_content = response.content
@@ -378,6 +378,7 @@ with col1:
 
             # Adding a column to df with the most similar title from df_titles
             df['Similar_Title'] = df['Title'].apply(lambda x: find_similar_title(x, df_titles['Title'], threshold=80))
+            df
 
             # Performing the merge based on the similar titles
             df_not = df.merge(df_titles[['Title']], left_on='Similar_Title', right_on='Title', how='left', indicator=True)
