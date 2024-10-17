5fd1ff3ae7a4598372f5519710805dfd4507e9cb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-17 14:13:34 +0100

    sd

--- Home_page.py
+++ Home_page.py
@@ -239,6 +239,7 @@ with st.spinner('Retrieving data...'):
 
     df_dedup = pd.read_csv('all_items.csv')
     df_duplicated = pd.read_csv('all_items_duplicated.csv')
+    df_authors = get_df_authors()
 
     col1, col2, col3 = st.columns([3,5,8])
     with col3:
